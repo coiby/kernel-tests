cb931b9b70ffc49b1e5403440b664e7ca4563110
Rachel Sibley <rasibley@redhat.com>
2020-10-07 17:11:29 -0400

    log to /dev/kmsg as well

--- watchdog/hw-generic/runtest.sh
+++ watchdog/hw-generic/runtest.sh
@@ -36,8 +36,12 @@ TEST="watchdog/hw-generic"
 # Status file to indicate where we are in the test (after reboot)
 TEST_STATUS=/var/tmp/watchdog.status
 touch "$TEST_STATUS"
+
 # File to backup/restore bootorder
 FILE=/tmp/watchdog_boot_order
+
+# /dev/kmsg file
+kmsg=/dev/kmsg
 
 efi_save()
 {
@@ -45,7 +49,7 @@ efi_save()
     order=$(efibootmgr | grep BootOrder | cut -d : -f 2)
     echo $order > $FILE
     curr=$(efibootmgr | grep BootCurrent | cut -d : -f 2)
-    echo -e "\nSAVE" | tee -a ${OUTPUTFILE}
+    echo -e "\nSAVE BootOrder" | tee -a ${OUTPUTFILE} ${kmsg}
     efibootmgr -o $curr
     echo
 }
@@ -54,7 +58,7 @@ efi_restore()
 {
     [ ! -e $FILE ] && echo "Error: save data is missing" && exit
     order=$(cat $FILE)
-    echo -e "\nRESTORE" | tee -a ${OUTPUTFILE}
+    echo -e "\nRESTORE BootOrder" | tee -a ${OUTPUTFILE} ${kmsg}
     efibootmgr -o $order
     echo
     rm $FILE
@@ -63,7 +67,7 @@ efi_restore()
 efi_set()
 {
     if [[ "$1" != "save" ]] && [[ "$1" != "restore" ]]; then
-		echo "Invalid command: $1" | tee -a ${OUTPUTFILE}
+		echo "Invalid command: $1" | tee -a ${OUTPUTFILE} ${kmsg}
 		rstrnt-abort --server $RSTRNT_RECIPE_URL/tasks/$RSTRNT_TASKID/status
     fi
 
@@ -76,30 +80,30 @@ chk_support() {
 		return 0
 	fi
 
-	echo -e "\n========== Checking Hardware Support ============" | tee -a ${OUTPUTFILE}
+	echo -e "\n========== Checking Hardware Support ============" | tee -a ${OUTPUTFILE} ${kmsg}
 
 	# Check that either iTCO or wdat is enabled in the logs
-	echo -e "\n== Related dmesg output:" | tee -a ${OUTPUTFILE}
-	egrep -i 'itco|wdat' /var/log/messages || echo "*** Warning, no related dmesg output! ***" | tee -a ${OUTPUTFILE}
+	echo -e "\n== Related dmesg output:" | tee -a ${OUTPUTFILE} ${kmsg}
+	egrep -i 'itco|wdat' /var/log/messages || echo "*** Warning, no related dmesg output! ***" | tee -a ${OUTPUTFILE} ${kmsg}
 
-	echo -e "\n== Checking that /dev/watchdog exists:" | tee -a ${OUTPUTFILE}
+	echo -e "\n== Checking that /dev/watchdog exists:" | tee -a ${OUTPUTFILE} ${kmsg}
 	if [ -c /dev/watchdog ]; then
-		echo "/dev/watchdog exists" | tee -a ${OUTPUTFILE}
+		echo "/dev/watchdog exists" | tee -a ${OUTPUTFILE} ${kmsg}
 		ls -l /dev/watchdog
 	else
-		echo "/dev/watchdog does not exist! Skipping test and existing !" | tee -a ${OUTPUTFILE}
+		echo "/dev/watchdog does not exist! Skipping test and existing !" | tee -a ${OUTPUTFILE} ${kmsg}
 		rstrnt-report-result $TEST SKIP
 		exit 0
 	fi
 
 	# Compile watchdog-simple test
-	echo -e "\n== Checking that watchdog-simple.c compiled successfully:" | tee -a ${OUTPUTFILE}
+	echo -e "\n== Checking that watchdog-simple.c compiled successfully:" | tee -a ${OUTPUTFILE} ${kmsg}
 	gcc -o watchdog-simple watchdog-simple.c
 	if [ ! -x watchdog-simple ] ; then
-		echo "Failed to build tests, exiting!" | tee -a ${OUTPUTFILE}
+		echo "Failed to build tests, exiting!" | tee -a ${OUTPUTFILE} ${kmsg}
 		rstrnt-abort --server $RSTRNT_RECIPE_URL/tasks/$RSTRNT_TASKID/status
 	else
-		echo "Compiled successfully." | tee -a ${OUTPUTFILE}
+		echo "Compiled successfully." | tee -a ${OUTPUTFILE} ${kmsg}
 	fi
 
 	echo chk_support >> "$TEST_STATUS"
@@ -107,20 +111,20 @@ chk_support() {
 }
 
 cleanup() {
-	echo -e "\n========== Clean up =============================" | tee -a ${OUTPUTFILE}
-	echo 'Remove status file' | tee -a ${OUTPUTFILE}
+	echo -e "\n========== Clean up =============================" | tee -a ${OUTPUTFILE} ${kmsg}
+	echo 'Remove status file' | tee -a ${OUTPUTFILE} ${kmsg}
 	rm -f "$TEST_STATUS"
 }
 
 disable_wdt_test() {
-	echo -e "\n========== Trigger watchdog reboot ============" | tee -a ${OUTPUTFILE}
+	echo -e "\n========== Trigger watchdog reboot ============" | tee -a ${OUTPUTFILE} ${kmsg}
 
 	# Check if we've already executed this function. If so, this indicates the
 	# the system rebooted (almost surely due to the watchdog). Report as PASS.
 	if grep -q disable_wdt_test "$TEST_STATUS" ; then
-		echo "== Disabling watchdog successfully triggered a reboot:" | tee -a ${OUTPUTFILE}
+		echo "== Disabling watchdog successfully triggered a reboot:" | tee -a ${OUTPUTFILE} ${kmsg}
 		rstrnt-report-result $TEST/disable_wdt_test PASS
-		return 0
+		return 0${kmsg}
 	fi
 	# Record we've run the test so on reboot we don't run it again
 	echo disable_wdt_test >> "$TEST_STATUS"
@@ -128,17 +132,16 @@ disable_wdt_test() {
 	# Start watchdog "daemon" in the background (which writes 1's into
 	# /dev/watchdog and keeps the box up). After several seconds, kill
 	# the program which should then cause the box to reboot.
-	echo "Disabling writes to /dev/watchdog... System should reboot in 30 - 60 seconds" | tee -a ${OUTPUTFILE}
+	echo "Disabling writes to /dev/watchdog... System should reboot in 30 - 60 seconds" | tee -a ${OUTPUTFILE} ${kmsg}
 	sync;sync
 	sleep 3
 	./watchdog-simple &
 	sleep 5
 	killall watchdog-simple
-	sync  # Write data to disk before the system does its hard reset
 
 	# If we get here it didn't reboot, so report as FAIL
 	sleep 65
-	echo "The system didn't reboot, reporting FAIL!" | tee -a ${OUTPUTFILE}
+	echo "The system didn't reboot, reporting FAIL!" | tee -a ${OUTPUTFILE} ${kmsg}
 	rstrnt-report-result $TEST/disable_wdt_test FAIL
 }
 
@@ -147,12 +150,12 @@ chk_support
 which efibootmgr &> /dev/null
 if [ $? -eq 0 ]; then
 	if [ "$RSTRNT_REBOOTCOUNT" -eq 0 ]; then
-		echo -e "\n========== Setting the BootOrder correcty for UEFI system ============" | tee -a ${OUTPUTFILE}
-		echo "== Original BootOrder:" | tee -a ${OUTPUTFILE}
+		echo -e "\n========== Setting the BootOrder correcty for UEFI system ============" | tee -a ${OUTPUTFILE} ${kmsg}
+		echo "== Original BootOrder:" | tee -a ${OUTPUTFILE} ${kmsg}
 		efibootmgr
 		efi_set save
 	else
-		echo -e "\n========== Restoring the BootOrder correcty for UEFI system ============" | tee -a ${OUTPUTFILE}
+		echo -e "\n========== Restoring the BootOrder correcty for UEFI system ============" | tee -a ${OUTPUTFILE} ${kmsg}
 		efi_set restore
 	fi
 fi
