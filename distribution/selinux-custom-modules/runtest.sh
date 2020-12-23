3385185ce4b783497405fdd478502046ddb5675c
Rachel Sibley <rasibley@redhat.com>
2020-12-23 11:27:24 -0500

    Mask known avc denials for RHEL9
    Mask known avc denials for both rhsmcertd-worke and rpcbind for
    RHEL9 kernels. See https://bugzilla.redhat.com/show_bug.cgi?id=1910373
    for more details.

--- distribution/selinux-custom-modules/runtest.sh
+++ distribution/selinux-custom-modules/runtest.sh
@@ -10,9 +10,15 @@ rlJournalStart
   rlPhaseStartTest
     # Bug 1896424 - [RHEL-8.4] selinux denies kexec write on aarch64
     # https://bugzilla.redhat.com/show_bug.cgi?id=1896424
-    if rlIsRHEL ">8.3" && [ $(uname -m) == "aarch64" ]; then
+    if rlIsRHEL 8.4 && [ $(uname -m) == "aarch64" ]; then
       rlRun "make -f /usr/share/selinux/devel/Makefile kexec.pp" 0 "Building kexec SELinux module"
       rlRun "semodule -i kexec.pp" 0 "Installing kexec SELinux module"
+    # Bug 1910373 - selinux avc denials for rhsmcertd-worke and rpcbind
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1910373
+    elif rlIsRHEL 9; then
+      rlRun "make -f /usr/share/selinux/devel/Makefile rpcbind-mod.pp" 0 "Building rpcbind-mod SELinux module"
+      rlRun "make -f /usr/share/selinux/devel/Makefile rhsmcertd-worke.pp" 0 "Building rhsmcertd-worke SELinux module"
+      rlRun "semodule -i rpcbind-mod.pp -i rhsmcertd-worke.pp" 0 "Installing rhsmcertd-worke and rpcbind-mod.pp SELinux modules"
     else
       rlLog "No custom SELinux modules required, skipping"
       rstrnt-report-result $TEST SKIP
