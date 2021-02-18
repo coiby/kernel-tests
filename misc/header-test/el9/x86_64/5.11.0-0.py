73d80d21d827161b8cc86b235c763b276fc81bd6
Jeffrey Bastian <jbastian@redhat.com>
2021-02-18 11:04:17 -0600

    header-test: add initial RHEL9 exc lists

---
+++ misc/header-test/el9/x86_64/5.11.0-0.py
@@ -0,0 +1,9 @@
+### x86_64 5.11.0-0.el9 ########################################################
+from flags import *
+
+def setup(exc):
+    PID_T = 'unistd.h'
+    SIZE_T = 'stddef.h'
+    SA_FAMILY_T = "sys/socket.h"
+    SOCKADDR = 'sys/socket.h'
+
