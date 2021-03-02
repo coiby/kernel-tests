67a94c65d2ad8041fb73d6702b2ed806dd6b5f39
Bruno Goncalves <bgoncalv@redhat.com>
2021-03-02 17:42:04 +0100

    fix serge-testsuite as runtest.sh is not beakerlib test

--- packages/selinux-policy/serge-testsuite/runtest.sh
+++ packages/selinux-policy/serge-testsuite/runtest.sh
@@ -35,7 +35,7 @@ git_path=${WRAPPER_GIT_PATH:-"kernel/selinux-testsuite"}
 TEST="packages/selinux-policy/serge-testsuite"
 # Test doesn't run without IPv6
 if grep "ipv6.disable=1" /proc/cmdline ; then
-    rlLog "Skip test as system doesn't have IPv6."
+    echo "Skip test as system doesn't have IPv6."
     rstrnt-report-result $TEST SKIP
     exit
 fi
