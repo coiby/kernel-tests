5ac712b3a3b859b2b4d1b5de916dd80d2534928f
Bruno Goncalves <bgoncalv@redhat.com>
2021-04-01 15:51:12 +0200

    mask again AVC issue from BZ1933680

--- distribution/selinux-custom-modules/runtest.sh
+++ distribution/selinux-custom-modules/runtest.sh
@@ -54,12 +54,11 @@ rlJournalStart
       rlRun "make -f /usr/share/selinux/devel/Makefile systemd-hostnam-mod.pp" 0 "Building systemd-hostname SELinux module"
       rules="${rules} sssd-mod systemd-hostnam-mod"
       # Fedora 33 doesn't have lockdown class
-# Temporarily enable avc for 1933680 to be able to provide more info in the BZ
-#      if seinfo --class lockdown -x  | grep -q confidentiality ; then
-#          # Bug 1933680 - avc: denied { confidentiality } for pid=814 comm="modprobe" lockdown_reason="use of tracefs"
-#          rlRun "make -f /usr/share/selinux/devel/Makefile modprobe-mod.pp" 0 "Building modprobe SELinux module"
-#          rules="${rules} modprobe-mod"
-#      fi
+      if seinfo --class lockdown -x  | grep -q confidentiality ; then
+          # Bug 1933680 - avc: denied { confidentiality } for pid=814 comm="modprobe" lockdown_reason="use of tracefs"
+          rlRun "make -f /usr/share/selinux/devel/Makefile modprobe-mod.pp" 0 "Building modprobe SELinux module"
+          rules="${rules} modprobe-mod"
+      fi
       for rule in $rules; do
            rlRun "semodule -i ${rule}.pp"  0 "Installing $rule SELinux modules"
       done
