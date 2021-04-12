7addf736ca1027b0a6134ba73b17930c4b5e46f1
Vector Li <huanli@redhat.com>
2021-04-12 10:27:48 +0800

    Update LTP to a recent commit
    Signed-off-by: Vector Li <huanli@redhat.com>

--- distribution/ltp-upstream/lite/runtest.sh
+++ distribution/ltp-upstream/lite/runtest.sh
@@ -29,7 +29,7 @@ function test_msg()
 #
 # NOTE: We use a tested commit to avoid that breakage in LTP's master branch
 #       which may make our test suites less stable
-LTP_REPO_COMMIT_ID=306adc9bed112ddee3c008b42f8bfd6dc4dadf7a
+LTP_REPO_COMMIT_ID=1b864ee0af9273b51a7f307fdeb4bb89885bbc6f
 function ltp_test_build()
 {
 	# workaround for the beaker issue when arch is ppc64:
