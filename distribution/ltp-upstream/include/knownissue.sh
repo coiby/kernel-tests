2e087bbd47b8459246f767f04f7e02d8eda11562
Li Wang <liwang@redhat.com>
2021-04-15 14:06:09 +0800

    ltp/include: add ioctl09 to knownisssue
    ioctl09 sporadically failed on race condition between parted and udev,
    even with 'udevadm trigger' still not perfectly fix this problem with
    s390x.
    
      ioctl09.c:47: TFAIL: access /sys/block/loop0/loop0p2 fails
      ioctl09.c:55: TFAIL: access /dev/loop0p2 fails
    
    Signed-off-by: Li Wang <liwang@redhat.com>

--- distribution/ltp-upstream/include/knownissue.sh
+++ distribution/ltp-upstream/include/knownissue.sh
@@ -137,6 +137,8 @@ function knownissue_filter()
 	tskip "ftrace-stress-test" fatal
 	# Issue TBD
 	tskip "sync_file_range02" unfix
+	# https://github.com/linux-test-project/ltp/issues/718
+	is_arch "s390x" && tskip "ioctl09" unfix
 	# Issue read_all_sys is triggering hard lockups on mustangs while reading /sys
 	# https://lore.kernel.org/linux-arm-kernel/1507592549.3785589.1570404050459.JavaMail.zimbra@redhat.com/
 	is_arch "aarch64" && tskip "read_all_sys" fatal
