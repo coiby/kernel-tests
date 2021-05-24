90c74d28d71f03bca813e1c81c984e2f49964afc
Yi Zhang <yi.zhang@redhat.com>
2021-05-24 11:35:48 +0800

    storage/blktests/srp: disable srp/013
    BZ1951961
    
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/srp/runtest.sh
+++ storage/blktests/srp/runtest.sh
@@ -121,7 +121,8 @@ function get_test_cases_srp
 	testcases+=" srp/010"
 	uname -ri | grep  -qE "el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/011"
 	# testcases+=" srp/012", need legacy device mapper support
-	testcases+=" srp/013"
+	# srp/013 on aarch64/ppc64le BZ1951961
+	uname -ri | grep -qE "4.18.0.*aarch64|4.18.0.*ppc64le" || testcases+=" srp/013"
 	uname -r | grep -q 4.18.0 || testcases+=" srp/014" #BZ1900153
 	uname -ri | grep -qE "el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/015"
 	echo $testcases
