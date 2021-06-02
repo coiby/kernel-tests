95b50d70c652fbb12d3c1ea3fd50f83025cb0a3c
Bruno Goncalves <bgoncalv@redhat.com>
2021-06-02 13:52:10 +0200

    run selinux with default audit rules
    this change in the rules didn't help us get more info about AVC, so let's run using the default configuration

--- distribution/selinux-custom-modules/runtest.sh
+++ distribution/selinux-custom-modules/runtest.sh
@@ -8,11 +8,6 @@ TEST="/kernel/distribution/selinux-custom-modules"
 
 rlJournalStart
 
-  rlPhaseStartTest "Modify to generate audit records"
-    rlRun "echo '-w /etc/shadow -p w' >> /etc/audit/rules.d/audit.rules" 0 "Adding extra rule task"
-    rlServiceStop auditd && rlServiceStart auditd
-  rlPhaseEnd
-
   rlPhaseStartTest
     # https://gitlab.com/cki-project/kernel-tests/-/issues/528
     # Bug 1932849 - avc: denied { module_request } kmod="net-pf-10"
