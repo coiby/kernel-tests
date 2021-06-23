19d21ed0c05e81274971292917301d860caf5f0a
Dennis Li <denli@redhat.com>
2021-06-23 16:32:50 -0400

    ltp: Add LICENSE info

--- distribution/ltp/lite/runtest.sh
+++ distribution/ltp/lite/runtest.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright Red Hat, Inc
+#
+#   SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 . ../../../cki_lib/libcki.sh || exit 1
 . ../include/runtest.sh      || exit 1
