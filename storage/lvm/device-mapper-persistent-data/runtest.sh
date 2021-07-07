9be39aabdc050ec165580ea913b9887e918fd8a6
Vector Li <huanli@redhat.com>
2021-07-07 09:42:44 +0800

    Add lvm/device-mapper-persistent-data test
    Signed-off-by: Vector Li <huanli@redhat.com>

---
+++ storage/lvm/device-mapper-persistent-data/runtest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing
+# to use, modify, copy, or redistribute it subject to the terms
+# and conditions of the GNU General Public License version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+#
+
+source ../../include/libstqe.sh
+
+function startup
+{
+    stqe_init_fwroot "master"
+}
+
+function cleanup
+{
+    stqe_fini_fwroot
+}
+
+function runtest
+{
+    cki_cd $(stqe_get_fwroot)
+    typeset -i rc=0
+    typeset tc_list=""
+    tc_list+=" lvm/device_mapper_persistent_data/thin"
+    tc_list+=" lvm/device_mapper_persistent_data/cache"
+    typeset tc=""
+    for tc in $tc_list; do
+        cki_run_cmd_pos "stqe-test run --fmf --path $tc"
+        (( rc += $? ))
+    done
+    cki_pd
+    (( rc != 0 )) && return $CKI_FAIL || return $CKI_PASS
+}
+
+cki_debug
+cki_main
+exit $?
