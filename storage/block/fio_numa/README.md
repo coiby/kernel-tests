9265844258eb520b26240cfbc091d18be5c225c6
Yi Zhang <yi.zhang@redhat.com>
2021-07-29 14:29:06 +0800

    storage: add fio random write/read test with different numa node
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

---
+++ storage/block/fio_numa/README.md
@@ -0,0 +1,17 @@
+# storage/block/fs_numa test suite
+
+Storage: Storage block fio random write/read with each NUMA CPU
+Test Maintainer: [Yi Zhang](mailto:yizhan@redhat.com)
+
+## How to run it
+Please refer to the top-level README.md for common dependencies.
+
+### Install dependencies
+```bash
+root# bash ../../cki_bin/pkgs_install.sh metadata
+```
+
+### Execute the test
+```bash
+bash ./runtest.sh
+```
---
+++ storage/block/fio_numa/runtest.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing
+# to use, modify, copy, or redistribute it subject to the terms
+# and conditions of the GNU General Public License version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+#
+FILE=$(readlink -f $BASH_SOURCE)
+NAME=$(basename $FILE)
+CDIR=$(dirname $FILE)
+NODES_RW=""
+NODES_RR=""
+
+declare -A FIO_TERSE_FIELDS
+FIO_TERSE_FIELDS=(
+        # Read status
+        ["read io"]=6
+        ["read bandwidth"]=7
+        ["read iops"]=8
+        # Write status
+        ["write io"]=47
+        ["write bandwidth"]=48
+        ["write iops"]=49
+)
+# Include enviroment and libraries
+source $CDIR/../../../cki_lib/libcki.sh || exit 1
+
+function randwrite_fio()
+{
+	local node=0 filed value
+	FIO_PERF_FIELDS=("write iops")
+	field="${FIO_TERSE_FIELDS["$FIO_PERF_FIELDS"]}"
+	while [ $node -lt $nodes_num ]; do
+		cpu=$(eval echo '$'NODE_${node}_CPU)
+		rlRun "taskset -c $cpu fio --output=/root/fio_perf_randwrite_$node --output-format=terse --terse-version=4 \
+		--bs=4K --size=1g --ioengine=libaio --iodepth=64 --iodepth_batch_submit=16 \
+		--iodepth_batch_complete_min=16 --filename=/root/fio.tmp \
+		--direct=1 --runtime=20 --numjobs=1 --size=1g --rw=randwrite --name=randwrite-test -group_reporting"
+		value="$(cut -d ';' -f "$field" "/root/fio_perf_randwrite_$node")"
+		rstrnt-report-log -l /root/fio_perf_randwrite_$node
+		NODES_RW+=" $value"
+		((node++))
+	done
+}
+
+function randread_fio()
+{
+	local node=0 filed value
+	TEST_DEV=$(lsblk | grep -oE "sda|vda|nvme0n1" | head -1)
+	FIO_PERF_FIELDS=("read iops")
+	field="${FIO_TERSE_FIELDS["$FIO_PERF_FIELDS"]}"
+	while [ $node -lt $nodes_num ]; do
+		cpu=$(eval echo '$'NODE_${node}_CPU)
+		rlRun "taskset -c $cpu fio --output=/root/fio_perf_randread_$node --output-format=terse --terse-version=4 \
+		--bs=4K --size=1g --ioengine=libaio --iodepth=64 --iodepth_batch_submit=16 \
+		--iodepth_batch_complete_min=16 --filename=/dev/${TEST_DEV} \
+		--direct=1 --runtime=20 --numjobs=1 --rw=randread --name=randread-test -group_reporting"
+		value="$(cut -d ';' -f "$field" "/root/fio_perf_randread_$node")"
+		rstrnt-report-log -l /root/fio_perf_randread_$node
+		NODES_RR+=" $value"
+		((node++))
+	done
+}
+
+function compare_min_max()
+{
+	local min=$1
+	local max=$1
+
+	for i in "$@"; do
+		((i > max)) && max=$i
+		((i < min)) && min=$i
+	done
+	rlLog "min: $min, max:$max"
+
+	if [ `echo "$min*1.1 > $max" |bc` -eq 1 ] ; then
+		rstrnt-report-result "Performance comparison: min:$min * 1.1 >  max:$max " PASS 0
+	else
+		rstrnt-report-result "Performance comparison: min:$min * 1.1 <  max:$max " FAIL 0
+	fi
+}
+
+function runtest
+{
+	local node=0
+	nodes_num=$(numactl -H | grep available | awk '{print $2}')
+	if [ $nodes_num -eq 1 ]; then
+		rstrnt-report-result "There is only one node on this server" SKIP 0
+		exit
+	fi
+	rlLog "There are $nodes_num nodes on this server"
+	while [ $node -lt $nodes_num ]; do
+		eval "NODE_${node}_CPU=$(numactl -H | grep "node $node cpus" | awk '{print $4}')"
+		((node++))
+	done
+	rlLog "Start  randwrite_fio tests"
+	randwrite_fio
+	rlLog "compare_min_max $NODES_RW"
+	compare_min_max $NODES_RW
+
+	rlLog "Start randread_fio tests"
+	randread_fio
+	rlLog "compare_min_max $NODES_RR"
+	compare_min_max $NODES_RR
+}
+
+cki_main
