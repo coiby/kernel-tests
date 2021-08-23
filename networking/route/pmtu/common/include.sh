29414e4b4733d5939aca64cbff92a827df01cd06
Bruno Goncalves <bgoncalv@redhat.com>
2021-08-23 14:11:58 +0200

    network tests should not install kernel packages
    during CKI run all kernel packages are already installed. Trying to
    install kernel rpms in the test can cause problems when testing upstream
    kernel as it might install different kernel.

--- networking/route/pmtu/common/include.sh
+++ networking/route/pmtu/common/include.sh
@@ -268,8 +268,6 @@ then
 	[ "$AVC_CHECK" = yes ] && enable_avc_check || disable_avc_check
 else
 	{
-	yum info kernel-modules-extra && yum install kernel-modules-extra -y --skip-broken
-
 	# ssh to switch would fail with error "no matching key exchange method found. Their offer: diffie-hellman-group1-sha1" on rhel8
 	# add extra configuration for ssh
 	yum install openssh-clients -y
