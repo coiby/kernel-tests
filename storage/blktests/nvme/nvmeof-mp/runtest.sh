f1ab7242c436833f75e676d13513a20b792f5ad3
Yi Zhang <yi.zhang@redhat.com>
2021-09-01 18:32:30 +0800

    storage/blktests/nvmeof-mp: disable nvmeof-mp/012 on x86_64/ppc64le
    BZ2000074
    
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/nvmeof-mp/runtest.sh
+++ storage/blktests/nvme/nvmeof-mp/runtest.sh
@@ -120,7 +120,7 @@ function get_test_cases
 	testcases+=" nvmeof-mp/009"
 	testcases+=" nvmeof-mp/010"
 	testcases+=" nvmeof-mp/011"
-	testcases+=" nvmeof-mp/012"
+	uname -ri | grep -qE "4.18.0.*x86_64|4.18.0.*ppc64le" || testcases+=" nvmeof-mp/012"  #BZ2000074
 	echo $testcases
 }
 
