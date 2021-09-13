6c55407ddcd91b16b1659bb3038849ab81ec99ed
Yi Zhang <yi.zhang@redhat.com>
2021-09-13 12:07:11 +0800

    storage/blktests/srp: disable srp/002 srp/011 srp/015 on all ppc64le
    The tests failed on 5.13.15 ppc64le
    
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/srp/runtest.sh
+++ storage/blktests/srp/runtest.sh
@@ -113,7 +113,7 @@ function get_test_cases_srp
 	testcases+=" srp/001"
 	# srp/002 srp/011 srp/015 failure on ppc64le|x86_64|aarch64, BZ1938508|BZ1963685|BZ1963696|BZ1999540
 	# srp/002 failure on aarch64 BZ2000815
-	uname -ri | grep -qE "el8.aarch64|el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/002"
+	uname -ri | grep -qE "ppc64le|el8.aarch64|el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/002"
 	# testcases+=" srp/003", need legacy device mapper support
 	# testcases+=" srp/004", need legacy device mapper support
 	testcases+=" srp/005"
@@ -122,12 +122,12 @@ function get_test_cases_srp
 	testcases+=" srp/008"
 	testcases+=" srp/009"
 	testcases+=" srp/010"
-	uname -ri | grep  -qE "el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/011"
+	uname -ri | grep  -qE "ppc64le|el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/011"
 	# testcases+=" srp/012", need legacy device mapper support
 	# srp/013 on aarch64/ppc64le BZ1951961
 	uname -ri | grep -qE "4.18.0.*aarch64|4.18.0.*ppc64le" || testcases+=" srp/013"
 	uname -r | grep -q 4.18.0 || testcases+=" srp/014" #BZ1900153
-	uname -ri | grep -qE "el8.aarch64|el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/015"
+	uname -ri | grep -qE "ppc64le|el8.aarch64|el8.x86_64|el8.ppc64le|el9.x86_64|el9.ppc64le" || testcases+=" srp/015"
 	echo $testcases
 }
 
