09617ee70e2edfb441a4b3e78540148dd427803a
Yi Zhang <yi.zhang@redhat.com>
2021-09-13 13:01:51 +0800

    storage/blktests/nvmeof-mp: disable 012 on upstream x86_64/s390x
    Test failed on 5.14.16, disable it
    
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/nvmeof-mp/runtest.sh
+++ storage/blktests/nvme/nvmeof-mp/runtest.sh
@@ -122,7 +122,7 @@ function get_test_cases
 	testcases+=" nvmeof-mp/009"
 	testcases+=" nvmeof-mp/010"
 	testcases+=" nvmeof-mp/011"
-	uname -ri | grep -qE "4.18.0.*x86_64|4.18.0.*ppc64le" || testcases+=" nvmeof-mp/012"  #BZ2000074
+	uname -ri | grep -qE "^5.*s390x|^5.*x86_64|4.18.0.*x86_64|4.18.0.*ppc64le" || testcases+=" nvmeof-mp/012"  #BZ2000074
 	echo $testcases
 }
 
