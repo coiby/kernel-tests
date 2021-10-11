00d468cc3477eec88e3592759873e4a885b2d59d
Nico Pache <npache@redhat.com>
2021-10-11 14:45:08 -0400

    Enable more kunit tests for upstream kernels
    Kernel-ark has recently enabled kunit. This commit will enable the
    following kunit tests to run using this script:
     -time_test
     -fat_test
     -rational-test
     -test_list_sort
     -slub_kunit
     -lib_test
    
    older kernels that do not contain these tests should not be affected as
    this script is backwards compatiable.
    
    Signed-off-by: Nico Pache <npache@redhat.com>

--- kunit/runtest.sh
+++ kunit/runtest.sh
@@ -60,21 +60,13 @@ PACKAGE="kernel"
 # When rebasing create a new array at the current release number (eg rhel8.5)
 # Then add a check for the proper tag number
 
-tag=$(uname -rm | grep -o "\-.*.el8")
-el8=$( echo "$tag" | grep -o ".el8" )
-version=$( echo "$tag" | sed 's/\-\(.*\).el8/\1/')
-
-#currently only supports rhel 8
-if [ "$el8" != ".el8" ]; then
-	rlLog "RHEL8 Varient not detected. currently only supports =>rhel8.4"
-	rstrnt-report-result $TEST SKIP
-	exit 0
-fi
+version=$(uname -rm | sed 's/\-*//' | sed 's/\..*//')
 
 test_arr=(kunit-test ext4-inode-test list-test sysctl-test mptcp_crypto_test \
 	mptcp_token_test bitfield_kunit cmdline_kunit property-entry-test \
 	qos-test resource_kunit soc-topology-test string-stream-test \
-	test_linear_ranges test_bits test_kasan)
+	test_linear_ranges test_bits test_kasan time_test fat_test lib_test\
+	rational-test test_list_sort slub_kunit)
 
 rlJournalStart
 #-------------------- Setup ---------------------
