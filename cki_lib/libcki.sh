c06baa608817f1714661e3f94548496c6261219a
Bruno Goncalves <bgoncalv@redhat.com>
2021-11-24 17:16:05 +0100

    cki_abort_task: report result as WARN
    related: https://gitlab.com/cki-project/kernel-tests/-/issues/847

--- cki_lib/libcki.sh
+++ cki_lib/libcki.sh
@@ -85,6 +85,7 @@ function cki_abort_task()
 {
     typeset reason="$*"
     [[ -z $reason ]] && reason="unknown reason"
+    rstrnt-report-result "${TEST}" WARN
     cki_log "Aborting current task: $reason"
     rstrnt-abort --server "$RSTRNT_RECIPE_URL/tasks/$RSTRNT_TASKID/status"
     exit $CKI_STATUS_ABORTED
