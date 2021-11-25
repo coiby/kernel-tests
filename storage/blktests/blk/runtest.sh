3bff1472fe2e2dc36fe2fd8583fcfd8f381cc1a7
Yi Zhang <yi.zhang@redhat.com>
2021-11-25 13:15:26 +0800

    storage/blktests: enable scsi tests from blktests
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/blk/runtest.sh
+++ storage/blktests/blk/runtest.sh
@@ -236,6 +236,17 @@ function get_test_cases_nvme
 	echo $testcases
 }
 
+function get_test_cases_scsi
+{
+	typeset testcases=""
+
+	testcases+=" scsi/004"
+	testcases+=" scsi/005"
+	testcases+=" scsi/007"
+
+	echo $testcases
+}
+
 bash $CDIR/build.sh
 if (( $? != 0 )); then
 	rstrnt-abort --server "$RSTRNT_RECIPE_URL/tasks/$RSTRNT_TASKID/status"
@@ -246,6 +257,7 @@ testcases_default=""
 testcases_default+=" $(get_test_cases_block)"
 testcases_default+=" $(get_test_cases_loop)"
 uname -ri | grep -qE "3.10.0-862.*s390x" || testcases_default+=" $(get_test_cases_nvme)"
+testcases_default+=" $(get_test_cases_scsi)"
 testcases=${_DEBUG_MODE_TESTCASES:-"$(echo $testcases_default)"}
 test_ws=$CDIR/blktests
 ret=0
