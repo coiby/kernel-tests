db42532c0c25b424899ad03766513ecc00e3ca5f
Waylon Cude <wcude@redhat.com>
2022-01-07 13:58:46 -0800

    Add pi_stress and rt_ltp tests
    Signed-off-by: Waylon Cude <wcude@redhat.com>

---
+++ rt-tests/pi_stress/runtest.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright Red Hat, Inc
+#
+#   SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Source rt common functions
+. ../include/runtest.sh || exit 1
+
+export TEST="rt-tests/pi_stress"
+
+rt_env_setup
+
+export rhel_major=$(grep -o '[0-9]*\.[0-9]*' /etc/redhat-release | awk -F '.' '{print $1}')
+
+echo "--- Test Start ---" | tee -a $OUTPUTFILE
+
+declare pkg_name="rt-tests" && [ $rhel_major -ge 9 ] && pkg_name="realtime-tests"
+which pi_stress || yum install -y $pkg_name
+
+if [ -z "$PARAM_SEC" ]; then
+    PARAM_SEC=300
+fi
+if [ -z "$PARAM_GROUPS" ]; then
+    PARAM_GROUPS=1
+fi
+
+echo "Running pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS" | tee -a $OUTPUTFILE
+pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS | tee -a $OUTPUTFILE
+if [ $? -eq 0 ]; then
+    rstrnt-report-result "pi_stress SCHED_FIFO" "PASS" 0
+else
+    rstrnt-report-result "pi_stress SCHED_FIFO" "FAIL" 1
+fi
+
+echo "Running pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS --rr" | tee -a $OUTPUTFILE
+pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS --rr | tee -a $OUTPUTFILE
+if [ $? -eq 0 ]; then
+    rstrnt-report-result "pi_stress SCHED_RR" "PASS" "0"
+else
+    rstrnt-report-result "pi_stress SCHED_RR" "FAIL" "1"
+fi
+
+echo "Running pip_stress" | tee -a $OUTPUTFILE
+pip_stress | tee -a $OUTPUTFILE
+if [ $? -eq 0 ]; then
+    rstrnt-report-result "pip_stress" "PASS" "0"
+else
+    rstrnt-report-result "pip_stress" "FAIL" "1"
+fi
+
+exit 0
---
+++ rt-tests/rt_ltp/runtest.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright Red Hat, Inc 
+#
+#   SPDX-License-Identifier: GPL-3.0-or-later  
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Source rt common functions
+. ../include/runtest.sh  || exit 1
+. ../include/ltp-make.sh || exit 1
+
+TEST="rt-tests/rt_ltp"
+
+# $TESTVERSION is set in ltp-make.sh
+ltp_version=${ltp_version:-$TESTVERSION}
+result_r="PASS"
+
+function check_status()
+{
+    if [ $? -eq 0 ]; then
+        echo ":: $* :: PASS ::" | tee -a "$OUTPUTFILE"
+    else
+        result_r="FAIL"
+        echo ":: $* :: FAIL ::" | tee -a "$OUTPUTFILE"
+    fi
+}
+
+function runtest()
+{
+    yum install -y wget gcc make automake || {
+        echo "dependent package install failed" | tee -a "$OUTPUTFILE"
+        rstrnt-report-result $TEST WARN 1
+        rlLog "Aborting test because dependent package install failed"
+        rstrnt-abort --server "$RSTRNT_RECIPE_URL/tasks/$RSTRNT_TASKID/status"
+        exit 1
+    }
+    download_ltp
+    # default test-arguments: func, stress, perf, list
+    pushd "ltp-full-$ltp_version/testcases/realtime"
+    func_list=$(./run.sh -t list | grep func)
+    while IFS= read -r case; do
+        echo "running $case"
+        ./run.sh -t "$case"
+        check_status "./run.sh -t $case"
+    done <<< "$func_list"
+    popd
+
+    if [ $result_r = "PASS" ]; then
+        echo "overall result: PASS" | tee -a "$OUTPUTFILE"
+        rstrnt-report-result $TEST "PASS" 0
+    else
+        echo "overall result: FAIL" | tee -a "$OUTPUTFILE"
+        rstrnt-report-result $TEST "FAIL" 1
+    fi
+}
+
+rt_env_setup
+runtest
+exit 0
