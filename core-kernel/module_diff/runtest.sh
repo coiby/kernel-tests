a7f4b2060ef5f25c55e2990a8bea02ebe561872f
Zhijun Wang <zhijwang@redhat.com>
2022-03-01 09:41:19 +0000

    core-kernel/module_diff: add ns.ko to known removed list on 8.6 x86_64,...

--- core-kernel/module_diff/runtest.sh
+++ core-kernel/module_diff/runtest.sh
@@ -510,6 +510,9 @@ rlJournalStart
                     if cki_kver_lt "4.18.0-366"; then
                         sed -i '/snd-sof-intel-ipc.ko/d' ${OS}/${Release}/HEAD-8.6-knownRemoved-x86_64.lst
 					fi
+                    if cki_kver_lt "4.18.0-371"; then
+                        sed -i '/^ns.ko/d' ${OS}/${Release}/HEAD-8.6-knownRemoved-x86_64.lst
+					fi
                     ;;
             esac
         elif [ "${K_VER}" = "5.14.0" ]; then
