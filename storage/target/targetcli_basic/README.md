3bbcbd17ea9f1a0fbd00a59adffd84b1519354a2
Martin Hoyer <mhoyer@redhat.com>
2022-04-27 14:46:46 +0200

    Adding storage/target/targetcli_basic test

---
+++ storage/target/targetcli_basic/README.md
@@ -0,0 +1,17 @@
+# storage/iscsi/params suite
+Testing various functions of targetcli including use of supported backstore types, attributes or CHAP authentication   
+The source code can be found at [python-stqe](https://gitlab.com/rh-kernel-stqe/python-stqe).  
+Test Maintainer: [Martin Hoyer](mailto:mhoyer@redhat.com)
+
+## How to run it
+Please refer to the top-level README.md for common dependencies.
+
+### Install dependencies
+```bash
+root# bash ../../../cki_bin/pkgs_install.sh metadata
+```
+
+### Execute the test
+```bash
+bash ./runtest.sh
+```
---
+++ storage/target/targetcli_basic/runtest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing
+# to use, modify, copy, or redistribute it subject to the terms
+# and conditions of the GNU General Public License version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+#
+
+source ../../include/libstqe.sh
+
+function runtest
+{
+    cki_run "stqe-test run --fmf -f tags:targetcli_basic"
+    typeset -i rc=$?
+    (( rc != 0 )) && return $CKI_FAIL || return $CKI_PASS
+}
+
+# stqe_init will abort the task if fails to run
+stqe_init
+
+runtest
+rc=$?
+
+exit $rc
