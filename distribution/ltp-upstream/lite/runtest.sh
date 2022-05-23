2cfaea7bf48a510bd37c439303f79c484c6720ed
Vector Li <huanli@redhat.com>
2022-05-23 14:54:14 +0800

    Update LTP to a recent commit
    Signed-off-by: Vector Li <huanli@redhat.com>

--- distribution/ltp-upstream/lite/runtest.sh
+++ distribution/ltp-upstream/lite/runtest.sh
@@ -40,7 +40,7 @@ function test_msg()
 #
 # NOTE: We use a tested commit to avoid that breakage in LTP's master branch
 #       which may make our test suites less stable
-LTP_REPO_COMMIT_ID=390cfc16309cf78931d8dc7d121a34ee68bfe2b4
+LTP_REPO_COMMIT_ID=6dd6876ff063ea77a6c626dc8b278609746aff5f
 function ltp_test_build()
 {
 	# NOTE: Skip to built and install ltp if it is done as we split a
