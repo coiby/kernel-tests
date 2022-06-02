799edb3d671093603b1683558c892d42b21a4369
Li Wang <liwang@redhat.com>
2022-06-02 10:47:44 +0800

    radix-tree: checking if dependencies install correctly
    Here only do a check on easily failed pkgs because of poor network.
    
    Signed-off-by: Li Wang <liwang@redhat.com>

--- core-kernel/Xarray-idr-radixtree-test/runtest.sh
+++ core-kernel/Xarray-idr-radixtree-test/runtest.sh
@@ -11,6 +11,8 @@ set -o pipefail
 
 function install_dependency()
 {
+	local RC=0
+
 	dnf="dnf -y install"
 
 	$dnf \
@@ -33,6 +35,18 @@ function install_dependency()
 		libubsan \
 		userspace-rcu \
 		userspace-rcu-devel
+
+	rpm -q dwarves               --quiet	|| RC=1
+	rpm -q libasan               --quiet	|| RC=1
+	rpm -q libubsan              --quiet	|| RC=1
+	rpm -q userspace-rcu         --quiet	|| RC=1
+	rpm -q userspace-rcu-devel   --quiet	|| RC=1
+
+	if [ $RC -eq 1 ]; then
+		echo "Failed to install dependecy packages"
+		exit 1
+	fi
+
 }
 
 function get_running_kernel_src()
