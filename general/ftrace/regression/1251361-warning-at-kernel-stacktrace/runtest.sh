51d8b2044e2538c5b4aa2bb48e4b181d4aa94bb2
Luigi Pellecchia <lpellecc@redhat.com>
2022-08-18 08:52:34 +0000

    ftrace 1251361 migration to tmt

---
+++ general/ftrace/regression/1251361-warning-at-kernel-stacktrace/runtest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Include rhts environment
+. /usr/share/beakerlib/beakerlib.sh
+
+rlJournalStart
+    rlPhaseStartSetup
+        if stat /run/ostree-booted > /dev/null 2>&1; then
+            rlRun "rpm-ostree install -A --idempotent --allow-inactive trace-cmd" 
+        else
+            rlRun "yum install -y trace-cmd"
+        fi
+    rlPhaseEnd
+    rlPhaseStartTest
+        rlRun "echo stacktrace > /sys/kernel/debug/tracing/trace_options"
+        rlRun "echo \"p kfree\" >> /sys/kernel/debug/tracing/kprobe_events"
+        rlRun "echo \"r exit_mmap\" >> /sys/kernel/debug/tracing/kprobe_events"
+        rlRun "echo 1 > /sys/kernel/debug/tracing/events/kprobes/enable"
+        rlRun "echo 1 >/sys/kernel/debug/tracing/tracing_on"
+        rlRun "dmesg | egrep 'WARN|save_stack_trace_regs'" 1
+    rlPhaseEnd
+    rlPhaseStartCleanup
+        trace-cmd reset
+    rlPhaseEnd
+rlJournalEnd
