7cb8462d3c5ddc583008d9ac603e20c9fe581d6b
Shizhao Chen <shichen@redhat.com>
2022-08-24 00:04:23 +0800

    [rt] sync smidetect with the private version
    1) enable tmt;
    
    2) add LATCHECK parameter and keep the previous behavior as default
       (LATCHECK=0) - it's default to be enabled in the private case;
    
    3) add HARLIMIT parameter and keep the previous value (200us) as default;
    
    4) keep 10m DURATION - default to be 1h in the private case, needs to update
       taskfiles accordingly;
    
    4) now the public case is pretty much identical to the private case.

--- rt-tests/smidetect/runtest.sh
+++ rt-tests/smidetect/runtest.sh
@@ -1,57 +1,76 @@
 #!/bin/bash
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-#   Copyright Red Hat, Inc 
+#   Copyright Red Hat, Inc
 #
-#   SPDX-License-Identifier: GPL-3.0-or-later  
+#   SPDX-License-Identifier: GPL-3.0-or-later
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Enable TMT testing for RHIVOS
+. ../../automotive/include/include.sh || exit 1
+: ${OUTPUTFILE:=runtest.log}
 
 # Source rt common functions
 . ../include/runtest.sh || exit 1
 
-TEST="rt-tests/smidetect"
-
-function RprtRslt ()
-{
-    result=$1
-
-    # File the results in the database
-    if [ $result = "PASS" ]; then
-        rstrnt-report-result $TEST $result 0
-    else
-        rstrnt-report-result $TEST $result 1
-    fi
-}
-
-DURATION=${DURATION:-10m}
+export TEST="rt-tests/smidetect"
+export DURATION=${DURATION:-10m}
+export LATCHECK=${LATCHECK:-0}
+export HARDLIMIT=${HARDLIMIT:-200us}
 
 function RunTest ()
 {
-    # Default result to Fail
-    export result_r="FAIL"
-
-    echo Test Start Time: `date` | tee -a $OUTPUTFILE
+    echo "Test Start Time: $(date)" | tee -a $OUTPUTFILE
 
     hwlatdetect \
         --duration=$DURATION \
-        --window=1s --width=500ms \
-        --threshold=10us --hardlimit=200us \
-        --debug | tee -a $OUTPUTFILE
+        --window=1s \
+        --width=500ms \
+        --threshold=10us \
+        --hardlimit=$HARDLIMIT \
+        --debug \
+        2>&1 | tee -a $OUTPUTFILE
+    RET_CODE=${PIPESTATUS[0]}
 
-    if [ $? -ne 0 ] ; then
-        echo "smidetect Failed: " | tee -a $OUTPUTFILE
-        result_r="FAIL"
+    # The numeric values below assume we are dealing with user seconds.
+    VAL=$(grep 'Max Latency' $OUTPUTFILE | awk -F: '{print $2}')
+    MAXLAT=$(grep 'Max Latency' $OUTPUTFILE | awk -F: '{gsub(/ /,"");gsub(/[a-z]+/,"");print $2}')
+    LIMIT_NUMERIC=$(echo $HARDLIMIT | awk -F[a-z] '{print $1}')
+
+    if [ $LATCHECK -eq 0 ]; then
+        # User chooses not to review max latency, so set pass/fail based on exit
+        # status of smidetect
+        if [ $RET_CODE -eq 0 ]; then
+            echo "smidetect(hwlatdetect) Passed - functional verification: " | tee -a $OUTPUTFILE
+            rstrnt-report-result "$TEST" "PASS" 0
+        else
+            echo "smidetect(hwlatdetect) Failed - functional verification: " | tee -a $OUTPUTFILE
+            rstrnt-report-result "$TEST" "FAIL" 1
+        fi
     else
-        echo "smidetect Passed: " | tee -a $OUTPUTFILE
-        result_r="PASS"
+        # Set result to pass only if max lat remained under HARDLIMIT
+        if [[ $VAL == ' Below threshold' ]] || (($MAXLAT <= $LIMIT_NUMERIC)); then
+            echo "smidetect(hwlatdetect) Passed - latency verification: " | tee -a $OUTPUTFILE
+            rstrnt-report-result "$TEST" "PASS" 0
+        else
+            echo "smidetect(hwlatdetect) Failed - latency verification: " | tee -a $OUTPUTFILE
+            rstrnt-report-result "$TEST" "FAIL" 1
+        fi
     fi
 
-    echo Test End Time: `date` | tee -a $OUTPUTFILE
-    RprtRslt $result_r
+    echo "Test End Time: $(date)" | tee -a $OUTPUTFILE
 }
 
 # ---------- Start Test -------------
-rt_env_setup
-RunTest
+if [ "$RSTRNT_REBOOTCOUNT" -eq 0 ]; then
+    if ! kernel_automotive; then
+        rt_env_setup
+    fi
+    RunTest
+    # reboot after test run because smidetect can leave residual SMI/SMM
+    # mods that impact future test cases such as stalld
+    rstrnt-reboot
+fi
+
 exit 0
