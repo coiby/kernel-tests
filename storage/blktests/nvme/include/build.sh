ec8fce8716f85dbd8fdff9126dd2136b40f698d0
Yi Zhang <yi.zhang@redhat.com>
2022-09-01 08:57:35 +0800

    storage/blktests: fix RHEL7 kernel version check
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/include/build.sh
+++ storage/blktests/nvme/include/build.sh
@@ -4,7 +4,7 @@
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 LOOKASIDE=https://github.com/yizhanglinux/blktests.git
-uname -r | grep -q 3.10 && BR=rhel7 || BR=nvme-rdma-tcp
+rlIsRHEL 7 && BR=rhel7 || BR=nvme-rdma-tcp
 rm -rf blktests
 git clone -b $BR $LOOKASIDE
 pushd blktests
