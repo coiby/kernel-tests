3fea8f6d70aa38aaea9b6e57aac666b237c626b1
Luigi Pellecchia <lpellecc@redhat.com>
2022-09-06 09:34:16 +0000

    general memory test jvm rlimit migrated to tmt

---
+++ general/memory/regression/1464290-1463241-stackguard-jvm-rlimit/runtest.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Description:
+#   Author: Wang Shu <shuwang@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2015 Red Hat, Inc.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+
+rlJournalStart
+    rlPhaseStartSetup
+        useradd test
+    rlPhaseEnd
+
+    rlPhaseStartTest Test
+        rlRun "bash -c 'ulimit -s 1024; /bin/true'"
+        rlRun "su test -c 'ulimit -s 1024; /bin/true'"
+
+        rlRun "gcc map_growsdown.c -o map_growsdown"
+        rlRun "./map_growsdown"
+
+        java_version="1.7.0"
+
+        if rlIsRHEL "9" || rlIsCentOS "9"; then
+            java_version="1.8.0"
+        fi
+
+        if stat /run/ostree-booted > /dev/null 2>&1; then
+            rpm-ostree install -A --idempotent --allow-inactive java-${java_version}-openjdk java-${java_version}-openjdk-devel
+        else
+            yum install -y java-${java_version}-openjdk java-${java_version}-openjdk-devel 
+        fi
+
+        if [ $? != "0" ]; then
+            rstrnt-report-result JVM_Test_Skipped PASS 99
+
+            rlPhaseEnd
+            rlJournalEnd
+            rlJournalPrintText
+            exit 0;
+        fi
+
+        LIBPATH=$(find /usr/lib/jvm/java-${java_version}/jre/lib -name libjvm.so | grep server)
+        LIBPATH=$(dirname $LIBPATH)
+        rlRun "gcc -fPIC -I/usr/lib/jvm/java-${java_version}/include/ -I/usr/lib/jvm/java-${java_version}/include/linux/  -L $LIBPATH -ljvm jni-sanity.c -o jni-sanity"
+        rlRun "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBPATH} ./jni-sanity"
+
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+    rlPhaseEnd
+rlJournalEnd
+rlJournalPrintText
+
