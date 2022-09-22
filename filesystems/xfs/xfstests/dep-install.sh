8d27bd5ccbb7b0de6401904046f57ce153afc847
Brian Grech <bgrech@redhat.com>
2022-09-22 18:19:08 +0000

    XFS support for rpm-ostree booted environments

--- filesystems/xfs/xfstests/dep-install.sh
+++ filesystems/xfs/xfstests/dep-install.sh
@@ -8,7 +8,12 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 YUM_PROG=$(type -P yum)
-if type -P dnf >/dev/null; then
+YUM_OPTS="-y --skip-broken install"
+if [[ -e /run/ostree-booted ]];then
+	YUM_PROG="$(type -P rpm-ostree)"
+	YUM_OPTS="--apply-live --idempotent --allow-inactive install"
+fi
+if type -P dnf >/dev/null && ! [[ -e /run/ostree-booted ]]; then
 	YUM_PROG="$(type -P dnf) --setopt=strict=0"
 fi
-$YUM_PROG -y --skip-broken install xfs-kmod xfsprogs xfsdump perl quota acl attr  bind-utils bc indent rpm-build autoconf libtool popt-devel libblkid-devel readline-devel gettext policycoreutils-python shadow-utils libuuid-devel e4fsprogs e2fsprogs e2fsprogs-devel gdbm-devel libaio-devel libattr-devel libacl-devel xfsprogs-devel btrfs-progs gfs2-utils gcc ncurses-devel pyOpenSSL git mdadm libcap git vim openssl-devel samba samba-client cifs-utils nfs-utils rpcbind lvm2 librbd1-devel librdmacm-devel vdo kmod-kvdo fio beakerlib libtirpc libtirpc-devel rpcgen blockdev
+$YUM_PROG $YUM_OPTS xfs-kmod xfsprogs xfsdump perl quota acl attr  bind-utils bc indent rpm-build autoconf libtool popt-devel libblkid-devel readline-devel gettext policycoreutils-python shadow-utils libuuid-devel e4fsprogs e2fsprogs e2fsprogs-devel gdbm-devel libaio-devel libattr-devel libacl-devel xfsprogs-devel btrfs-progs gfs2-utils gcc ncurses-devel pyOpenSSL git mdadm libcap git vim openssl-devel samba samba-client cifs-utils nfs-utils rpcbind lvm2 librbd1-devel librdmacm-devel vdo kmod-kvdo fio beakerlib libtirpc libtirpc-devel rpcgen blockdev
