299fc2590985ee3a5ed0214eac251c4a21dcacde
Bruno Goncalves <bgoncalv@redhat.com>
2022-10-04 15:49:01 +0200

    LTP lite: don't override LTP_TIMEOUT_MUL and LTP_RUNTIME_MUL if passed as test parameter when runing on VM or kernel debug

--- distribution/ltp/lite/runtest.sh
+++ distribution/ltp/lite/runtest.sh
@@ -16,15 +16,15 @@
 #export RHTS_OPTION_STRONGER_AVC=
 
 # VMs can have slow performance, therefore increase LTP_TIMEOUT_MUL
-if  cki_is_vm; then
-	export LTP_TIMEOUT_MUL=2
-	export LTP_RUNTIME_MUL=10
+if cki_is_vm; then
+	export LTP_TIMEOUT_MUL=${LTP_TIMEOUT_MUL:-2}
+	export LTP_RUNTIME_MUL=${LTP_RUNTIME_MUL:-10}
 fi
 
 # debug kernel is slower increase LTP_TIMEOUT_MUL
-if  cki_is_kernel_debug; then
-	export LTP_TIMEOUT_MUL=2
-	export LTP_RUNTIME_MUL=10
+if cki_is_kernel_debug; then
+	export LTP_TIMEOUT_MUL=${LTP_TIMEOUT_MUL:-2}
+	export LTP_RUNTIME_MUL=${LTP_RUNTIME_MUL:-10}
 fi
 
 core_pattern="$(cat /proc/sys/kernel/core_pattern)"
