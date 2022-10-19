17592f60cdf98a327ca2e69888f3e698035c9263
Bruno Goncalves <bgoncalv@redhat.com>
2022-10-19 13:32:48 +0200

    add cki restraint plugins

---
+++ cki/restraint/runtest.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+set -xe
+
+if [[ -f /usr/lib/systemd/system/restraintd.service ]]; then
+    if grep -qE "^OOMPolicy=continue$" /usr/lib/systemd/system/restraintd.service; then
+        echo "restraintd already contains OMPolicy=continue"
+    else
+        # Patch restraintd service to continue after OOM, some tests generate OOM and restraintd should continue to run
+        sed -i '/OOMScoreAdjust=.*/a OOMPolicy=continue' /usr/lib/systemd/system/restraintd.service
+    fi
+fi
+
+mkdir -p /usr/share/rhts/
+
+# Silence dmesg warnings for known issues
+cat >/usr/share/rhts/falsestrings <<EOF
+BIOS BUG
+DEBUG
+mapping multiple BARs.*IBM System X3250 M4
+MAX_LOCKDEP_CHAIN_HLOCKS
+BUG: KASAN: use-after-free in string_nocheck
+watchdog: BUG: soft lockup - CPU
+EOF
+echo "/usr/share/rhts/falsestrings is"
+cat /usr/share/rhts/falsestrings
+
+# workaround for https://github.com/restraint-harness/restraint/issues/273
+cat >/usr/share/rhts/failurestrings <<EOF
+\bOops\b
+\bBUG\b
+NMI appears to be stuck
+Badness at
+EOF
+echo "/usr/share/rhts/failurestrings is"
+cat /usr/share/rhts/failurestrings
+
+# Patch restraint's localwatchdog plugin to abort the recipe instead of warning,
+# when a task goes over KILLTIMEOVERRIDE.
+# The ABORT task result isn't accepted as a valid result, we should instead change it to:
+# 'sed -i 's|rstrnt-reboot|rstrnt-abort --server $RSTRNT_RECIPE_URL/tasks/$TASKID/status\nrstrnt-reboot|' /usr/share/restraint/plugins/localwatchdog.d/99_reboot'
+# Otherwise you may see "Failed to submit result, status: 400 Message: BAD REQUEST"
+# see: https://bugzilla.redhat.com/show_bug.cgi?id=1716997
+sed -i 's|rstrnt-reboot|rstrnt-abort --server $RSTRNT_RECIPE_URL/tasks/$TASKID/status\nrstrnt-reboot|' /usr/share/restraint/plugins/localwatchdog.d/99_reboot
+
+# Show information the test aborted due to localwatchdog
+if ! grep -q "\${RSTRNT_TASKNAME} hit test timeout" /usr/share/restraint/plugins/localwatchdog.d/10_localwatchdog; then
+   echo 'echo "${RSTRNT_TASKNAME} hit test timeout, aborting it..." >> /dev/kmsg' >> /usr/share/restraint/plugins/localwatchdog.d/10_localwatchdog
+fi
+
+cp -r plugins /usr/share/restraint/
+# Make sure the plugins have exec permission
+chmod -R +x /usr/share/restraint/plugins
