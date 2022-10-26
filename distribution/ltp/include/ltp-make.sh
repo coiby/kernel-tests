d4bf392af37e8a9231297d06e72a54b760163b83
Ping Fang <pifang@redhat.com>
2022-10-26 20:24:19 +0800

    ltp/include: Backport ptrace07 fix, update knownissues
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include/ltp-make.sh
+++ distribution/ltp/include/ltp-make.sh
@@ -106,6 +106,11 @@ patch-generic()
         ${PATCH} < ${ABS_DIR}/INTERNAL/0001-shmat03-ignore-EACCES.patch
         ${PATCH} < ${ABS_DIR}/INTERNAL/0001-Disable-btrfs-as-we-don-t-support-it-anymore.patch
         ${PATCH} < ${ABS_DIR}/INTERNAL/0001-rhel9-support-futex_waitv.patch
+        ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-ptrace07-Fix-compilation-by-avoiding-aligned_alloc.patch
+        ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-ptrace07-Fix-compilation-when-cpuid.h-is-missing.patch
+        ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-ptrace07-Fix-compilation-when-not-on-x86.patch
+        ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-ptrace07-fix-the-broken-case-caused-by-hardcoded-xst.patch
+
     fi
 
     if [ "$TESTVERSION" == "20220527" ]; then
