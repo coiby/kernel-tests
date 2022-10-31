3223bb1e7898d39b6e3debb08421bb56f15485d8
Qiao Zhao <qzhao@redhat.com>
2022-10-31 09:49:56 +0800

    update the tuna to match new CLI future change since 9.2
    Signed-off-by: Qiao Zhao <qzhao@redhat.com>

--- rt-tests/us/unittests/pkg/tuna/test.py
+++ rt-tests/us/unittests/pkg/tuna/test.py
@@ -5,6 +5,7 @@ Unittest for package tuna
 import os
 import subprocess
 import rtut
+import re
 
 class TunaTest(rtut.RTUnitTest):
 
@@ -14,6 +15,9 @@ class TunaTest(rtut.RTUnitTest):
         self.thrdplay = subprocess.Popen(["vmstat", "1", "5000"], stdout=self.fnull)
         self.pidplay = self.thrdplay.pid
         self.tmp_file = f"{os.getcwd()}/output.txt"
+        f = open("/etc/redhat-release", "r")
+        self.rhel_version = float(re.findall(r"\d.\d", f.read().rstrip())[0])
+        f.close()
 
     def tearDown(self):
         self.thrdplay.terminate()
@@ -23,58 +27,107 @@ class TunaTest(rtut.RTUnitTest):
             os.remove(self.tmp_file)
 
     def test_threads_prio_fifo(self):
-        self.run_cmd(f'tuna --threads={self.pidplay} --priority=FIFO:40')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna priority FIFO:40 --threads={self.pidplay}')
+        else:
+            self.run_cmd(f'tuna --threads={self.pidplay} --priority=FIFO:40')
 
     def test_threads_prio_rr(self):
-        self.run_cmd('tuna --threads=vmstat --priority=RR:50 --show_threads')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna priority RR:50 --threads=vmstat')
+        else:
+            self.run_cmd('tuna --threads=vmstat --priority=RR:50 --show_threads')
 
     def test_version(self):
         self.run_cmd('tuna -v')
 
     def test_show_threads(self):
-        self.run_cmd('tuna --threads=vmstat --show_threads')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_threads --threads=vmstat')
+        else:
+            self.run_cmd('tuna --threads=vmstat --show_threads')
 
     def test_what_is(self):
-        self.run_cmd('tuna --threads=vmstat -W')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna what_is vmstat')
+        else:
+            self.run_cmd('tuna --threads=vmstat -W')
 
     def test_cgroup(self):
-        self.run_cmd('tuna -G -P')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_threads -G')
+        else:
+            self.run_cmd('tuna -G -P')
 
     def test_show_irqs(self):
-        self.run_cmd('tuna --show_irqs')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_irqs')
+        else:
+            self.run_cmd('tuna --show_irqs')
 
     def test_show_irqs_by_user(self):
-        self.run_cmd('tuna --irqs=timer --show_irqs')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_irqs --irqs=rtc0')
+        else:
+            self.run_cmd('tuna --irqs=rtc0 --show_irqs')
 
     def test_save(self):
-        self.run_cmd(f'tuna --save={self.tmp_file}')
+        if self.rhel_version >= 9.2:
+            self.run_cmd(f'tuna save {self.tmp_file}')
+        else:
+            self.run_cmd(f'tuna --save={self.tmp_file}')
 
-    def test_isoate(self):
-        self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0 --isolate')
+    def test_isolate(self):
+        if self.rhel_version >= 9.2:
+            self.run_cmd(f'tuna isolate --cpus 0')
+        else:
+            self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0 --isolate')
 
     def test_spread(self):
-        self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0,1 --spread')
+        if self.rhel_version >= 9.2:
+            self.run_cmd(f'tuna spread --threads={self.pidplay} --cpus=0,1')
+        else:
+            self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0,1 --spread')
 
     def test_include(self):
-        self.run_cmd('tuna --cpus=0,1 --include --run="ps -ef"')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna include --cpus 0,1')
+        else:
+            self.run_cmd('tuna --cpus=0,1 --include --run="ps -ef"')
 
     def test_affect_children(self):
-        self.run_cmd('tuna --cpus=0,1 --affect_children --include --run="ps -ef"')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_threads --cpus 0,1 --affect_children')
+        else:
+            self.run_cmd('tuna --cpus=0,1 --affect_children --include --run="ps -ef"')
 
     def test_filter(self):
-        self.run_cmd('tuna --threads=vmstat --show_threads --filter -c 0')
+        if self.rhel_version < 9.2:
+            self.run_cmd('tuna --threads=vmstat --show_threads --filter -c 0')
 
     def test_no_kthreads(self):
-        self.run_cmd('tuna --cpus=0,1 --no_kthreads --include --run="ps -ef"')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_threads --threads vmstat --cpus 0,1 --no_kthreads')
+        else:
+            self.run_cmd('tuna --cpus=0,1 --no_kthreads --include --run="ps -ef"')
 
     def test_move(self):
-        self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0 --move')
+        if self.rhel_version >= 9.2:
+            self.run_cmd(f'tuna move --threads={self.pidplay} --cpus=0')
+        else:
+            self.run_cmd(f'tuna --threads={self.pidplay} --cpus=0 --move')
 
     def test_run(self):
-        self.run_cmd('tuna --cpus=0,1 --run="ps -ef"')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna run "ps -ef" --cpus 0,1')
+        else:
+            self.run_cmd('tuna --cpus=0,1 --run="ps -ef"')
 
     def test_sockets(self):
-        self.run_cmd('tuna --sockets=0')
+        if self.rhel_version >= 9.2:
+            self.run_cmd('tuna show_threads --sockets 0')
+        else:
+            self.run_cmd('tuna --sockets=0')
 
 if __name__ == '__main__':
     TunaTest.run_unittests()
