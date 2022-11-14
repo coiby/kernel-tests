8f3c58ae1f292afb3ab23fdfb542788f68c90529
Precambrian_horror <ehamera@redhat.com>
2022-11-14 11:45:38 -0500

    Initial commit for the /kernel/power-management/powertop/sanity test,

---
+++ power-management/powertop/sanity/01_set_filename.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+filename="foo.csv"
+
+rm "$filename"
+powertop --csv="$filename"
+retval="$?"
+
+#if [ "$retval" != 0 ]; then
+#        echo "Warning: powertop returned $retval"
+#        generate warning
+#        exit 0
+#fi
+
+if [ -f "$filename" ]; then
+        echo "O.K."
+        exit 0
+else
+        echo "powertop hasn't generated output file."
+        exit 1
+fi
