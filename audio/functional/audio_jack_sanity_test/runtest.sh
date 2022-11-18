109b988235f1d7518e98c2c5f74acf02508ce567
Dorinda <dbassey@redhat.com>
2022-11-18 17:07:41 +0100

    audio/functional: tmt migration
    Signed-off-by: Dorinda <dbassey@redhat.com>

--- audio/functional/audio_jack_sanity_test/runtest.sh
+++ audio/functional/audio_jack_sanity_test/runtest.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 export PYTHONPATH=../../../test-framework:../../../audio_tests:$PYTHONPATH
-. /tmp/test-params.sh
+OUTPUTFILE=test.log
+[ -f /tmp/test-params.sh ] && . /tmp/test-params.sh
 command_arguments=""
 if [ -n "${SEED+1}" ]
 then
-	command_arguments="$command_arguments --randomSeed $SEED"
+        command_arguments="$command_arguments --randomSeed $SEED"
 fi
 if [ -n "${SKIP_UNSUPPORTED+1}" ]
 then
-	command_arguments="$command_arguments --skip_unsupported"
+        command_arguments="$command_arguments --skip_unsupported"
 fi
-sh ../../../test-framework/test_launcher.sh $TEST "./test.py $command_arguments $*"
-rhts-submit-log -l ./test.log
+sh ../test_launcher.sh $TEST "./test.py $command_arguments $*"
+touch ${OUTPUTFILE}
+rstrnt-report-log -l ${OUTPUTFILE}
