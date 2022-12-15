e98bf301cf70025ff25354777aea1bc5867f5173
Laura Trivelloni <ltrivell@redhat.com>
2022-12-15 19:50:10 +0000

    Deleted from MR already merged files.

--- networking/wireless/tests/bug/bz1044005/wireless_connection_to_unencrypted_AP/runtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=../../../../test-framework:../../../../wireless_tests:$PYTHONPATH
-. /tmp/test-params.sh
-rhts-run-simple-test "$TEST" "./test.py $*"
-rhts-submit-log -l /var/log/wpa_supplicant.log
-rhts-submit-log -l ./test.log
-rhts-submit-log -l /var/log/messages
-
