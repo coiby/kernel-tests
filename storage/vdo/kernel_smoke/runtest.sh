b7065f06f6be14f290449425f5388ea918833ee9
Filip Suba <fsuba@redhat.com>
2022-12-21 09:46:19 +0100

    [storage/vdo] Add vdo tests

---
+++ storage/vdo/kernel_smoke/runtest.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright (c) 2022 Red Hat, Inc. All rights reserved. This copyrighted
+# material is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General
+# Public License v.2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+# USA.
+#
+# Author: Filip Suba <fsuba@redhat.com>
+
+# need to get OS version for later filtering. This works only for RHEL though, as there are no Fedora filters...
+#OS=`cat /etc/*release | grep VERSION_ID | cut -c13`
+#compose_filter="--filter compose:RHEL-$OS"
+
+filter1="--filter=component:vdo|component:kmod-kvdo"
+filter2="--filter=tags:kernel_smoke"
+setup1="--setup=iscsi"
+setup2="--setup=vdo/setup/storage/cleanup_multipath"
+
+command="stqe-test run --fmf $filter1 $filter2 $setup1 $setup2 $ENV_PARAMS"
+echo "$command"
+
+$command
