c9d716310e48589358c3c820caaa339ce7e00024
Shizhao Chen <shichen@redhat.com>
2022-12-26 20:33:51 +0800

    oslat: add test_bucket_width()
    To test the newly added --bucket-width/-W option:
    
    - https://bugzilla.redhat.com/show_bug.cgi?id=2041637
    - https://bugzilla.redhat.com/show_bug.cgi?id=2122374

--- rt-tests/us/unittests/pkg/realtime-tests/oslat/test.py
+++ rt-tests/us/unittests/pkg/realtime-tests/oslat/test.py
@@ -4,6 +4,7 @@ Unittest for oslat of realtime-tests
 """
 import os
 import rtut
+import subprocess
 
 class OslatTest(rtut.RTUnitTest):
 
@@ -25,6 +26,16 @@ class OslatTest(rtut.RTUnitTest):
                      f'--duration 10s --json={self.tmp_file} --rtprio 50 --workload-mem 4K '
                      '--quiet --single-preheat --trace-threshold 10000000 '
                      '--workload no --zero-omit')
+
+    def test_bucket_width(self):
+        if subprocess.run("oslat --help | grep bucket-width",
+                          stdout=subprocess.DEVNULL,
+                          shell=True).returncode == 0:
+            self.run_cmd('oslat -z -D 10s -b 32 --bucket-width 1000')
+            self.run_cmd('oslat -z -D 10s -b 32 --bucket-width 100')
+            self.run_cmd('oslat -z -D 10s -b 32 --bucket-width 10')
+            self.run_cmd('oslat -z -D 10s -b 32 --bucket-width 1')
+            self.run_cmd('oslat -z -D 10s -b 32 -W 10')
 
     def test_short(self):
         self.run_cmd('oslat -b 64 -B -c 1,2 -C -D 10s -f 50 -m 4K -q -s -T 10000000 -w no -z')
