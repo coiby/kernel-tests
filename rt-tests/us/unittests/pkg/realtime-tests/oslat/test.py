89d20d69789d151fb3b3f0492a8d432334a0941b
Shizhao Chen <shichen@redhat.com>
2022-12-26 21:17:14 +0800

    oslat: separate --rtprio/-f from test_long/short
    1. Thread or even system may hang when --rtprio/-f and --cpu-main-thread are
    used together;
    
    2. Running SCHED_FIFO workload on CPU 0 may hang the thread, so --cpu-list/-c is
    necessary in test_rtprio.

--- rt-tests/us/unittests/pkg/realtime-tests/oslat/test.py
+++ rt-tests/us/unittests/pkg/realtime-tests/oslat/test.py
@@ -23,8 +23,12 @@ class OslatTest(rtut.RTUnitTest):
 
     def test_long(self):
         self.run_cmd('oslat --bucket-size 64 --bias --cpu-list 1,2 --cpu-main-thread 1 '
-                     f'--duration 10s --json={self.tmp_file} --rtprio 50 --workload-mem 4K '
+                     f'--duration 10s --json={self.tmp_file} --workload-mem 4K '
                      '--single-preheat --trace-threshold 10000000 --workload no --zero-omit')
+
+    def test_rtprio(self):
+        self.run_cmd('oslat -z -D 10s -b 32 -c 1,2 --rtprio 1')
+        self.run_cmd('oslat -z -D 10s -b 32 -c 1,2 -f 1')
 
     def test_bucket_width(self):
         if subprocess.run("oslat --help | grep bucket-width",
@@ -37,7 +41,7 @@ class OslatTest(rtut.RTUnitTest):
             self.run_cmd('oslat -z -D 10s -b 32 -W 10')
 
     def test_short(self):
-        self.run_cmd('oslat -b 64 -B -c 1,2 -C 1 -D 10s -f 50 -m 4K -s -T 10000000 -w no -z')
+        self.run_cmd('oslat -b 64 -B -c 1,2 -C 1 -D 10s -m 4K -s -T 10000000 -w no -z')
 
 if __name__ == '__main__':
     OslatTest.run_unittests()
