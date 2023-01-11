2c45aef50386ab0f590bec3b3984230f17f54354
Bruno Goncalves <bgoncalv@redhat.com>
2023-01-11 14:55:17 +0100

    kselftests: don't try to install kernel modules-extra packages when using CKI_SELFTESTS_URL
    CKI_SELFTESTS_URL are used for upstream kernel built by CKI, they are in
    tarball format, so won't be able to find any kernel rpm package for it.
    
    this was wrongly introduced by:
    https://gitlab.com/redhat/centos-stream/tests/kernel/kernel-tests/-/commit/5100106e0bb44f1ad053d7c54b248fee34c247ef#c15ec54f628aa5b664c4b6612b34e5a7a9676b84_114_135

--- kselftests/runtest.sh
+++ kselftests/runtest.sh
@@ -133,8 +133,6 @@ install_packages()
 
 install_kselftests()
 {
-    # Install debug-modules-extra
-    rlRun "$pkg_mgr $pkg_mgr_inst_string ${name}-modules-extra-${version}-${release}"
     # Install the selftests-internal, modules-internal packages by default
     if [ "${CKI_SELFTESTS_URL}" ] ; then
         pushd ${EXEC_DIR}
@@ -144,6 +142,8 @@ install_kselftests()
         rlLog "Upstream ${TEST} installed..."
         popd
     elif [ "${BUILD_FROM_SRC}" ] ; then
+        # Install debug-modules-extra
+        rlRun "$pkg_mgr $pkg_mgr_inst_string ${name}-modules-extra-${version}-${release}"
         if [ "${UPSTREAM_SOURCE_URL}" ]; then
             pushd $TMPDIR/linux-kselftest-*/
         else
@@ -158,6 +158,8 @@ install_kselftests()
         popd
         [ -f $TMPDIR/selftests/run_kselftest.sh ] && return 0 || return 1
     else
+        # Install debug-modules-extra
+        rlRun "$pkg_mgr $pkg_mgr_inst_string ${name}-modules-extra-${version}-${release}"
         if ! rpm -q ${name}-modules-internal > /dev/null 2>&1; then
             rlRun "dnf download --resolve ${name}-modules-internal-${version}-${release}.${arch}"
             rlRun "$pkg_mgr $pkg_mgr_inst_string ./${name}-modules-internal-${version}-${release}.${arch}.rpm"
