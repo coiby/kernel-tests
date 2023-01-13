8e55772934a763916585b4b02a24a39d3bf3ebdc
Bruno Goncalves <bgoncalv@redhat.com>
2023-01-13 09:34:21 +0100

    multipath_duplicate_UUID_BZ_2028835: no need to include cki_lib as the test doesn't use any cki function

--- storage/multipath/multipath_duplicate_UUID_BZ_2028835/runtest.sh
+++ storage/multipath/multipath_duplicate_UUID_BZ_2028835/runtest.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-FILE=$(readlink -f "$BASH_SOURCE")
-CDIR=$(dirname "$FILE")
-
-# Include enviroment and libraries
-source "$CDIR"/../../../cki_lib/libcki.sh         || exit 1
 . /usr/share/beakerlib/beakerlib.sh             || exit 1
 
 function cleanup()
