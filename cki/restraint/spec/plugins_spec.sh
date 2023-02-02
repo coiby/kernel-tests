a9694111540fd446c6a26532f3792e405386e4ba
Bruno Goncalves <bgoncalv@redhat.com>
2023-02-02 10:46:43 +0100

    cki/restraint add spec test for 26_cki_environment abort if wrong task tries to run

--- cki/restraint/spec/plugins_spec.sh
+++ cki/restraint/spec/plugins_spec.sh
@@ -40,25 +40,25 @@ Describe 'cki-restraint: plugins'
     It "can run 26_cki_environment"
         When run script cki/restraint/plugins/task_run.d/26_cki_environment
         The first line should equal "rstrnt_info *** Running Plugin: cki/restraint/plugins/task_run.d/26_cki_environment"
-        The file "${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/bootinfo" should be exist
+        The file "${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/bootinfo" should be exist
         The status should be success
     End
 
     It "can run 30_init_test_console_log"
         When run script cki/restraint/plugins/task_run.d/30_init_test_console_log
         The first line should equal "dmesg -C"
-        The stdout should include "curl --silent --show-error --retry 5 http://${LAB_CONTROLLER}:8000/recipes/${RSTRNT_RECIPEID}/logs/console.log -o ${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/init_console.log"
+        The stdout should include "curl --silent --show-error --retry 5 http://${LAB_CONTROLLER}:8000/recipes/${RSTRNT_RECIPEID}/logs/console.log -o ${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/init_console.log"
         The status should be success
     End
 
     It "can run 80_finish_console_log"
-        mkdir -p "${CURRENT_TASK_PATH}/${RSTRNT_TASKID}"
-        touch "${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/init_console.log"
-        touch "${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/end_console.log"
+        mkdir -p "${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}"
+        touch "${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/init_console.log"
+        touch "${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/end_console.log"
         When run script cki/restraint/plugins/completed.d/80_finish_console_log
         The first line should equal "sleep 15"
-        The line 2 should equal "curl --silent --show-error --retry 5 http://${LAB_CONTROLLER}:8000/recipes/${RSTRNT_RECIPEID}/logs/console.log -o ${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/end_console.log"
-        The line 3 should equal "rstrnt-report-log -l ${CURRENT_TASK_PATH}/${RSTRNT_TASKID}/test_console.log"
+        The line 2 should equal "curl --silent --show-error --retry 5 http://${LAB_CONTROLLER}:8000/recipes/${RSTRNT_RECIPEID}/logs/console.log -o ${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/end_console.log"
+        The line 3 should equal "rstrnt-report-log -l ${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/test_console.log"
         The status should be success
     End
 
@@ -66,5 +66,33 @@ Describe 'cki-restraint: plugins'
         When run script cki/restraint/plugins/completed.d/90_cleanup_cki
         The first line should equal "rstrnt_info *** Running Plugin: cki/restraint/plugins/completed.d/90_cleanup_cki"
         The status should be success
+    End
+End
+
+Describe 'cki-restraint: plugins concurrent task'
+    cleanup(){
+        rm -rf "${CURRENT_TASK_PATH}"
+    }
+    BeforeAll 'cleanup'
+    AfterAll 'cleanup'
+
+    It "can run 26_cki_environment first task"
+        When run script cki/restraint/plugins/task_run.d/26_cki_environment
+        The first line should equal "rstrnt_info *** Running Plugin: cki/restraint/plugins/task_run.d/26_cki_environment"
+        The file "${CURRENT_TASK_PATH}/${RSTRNT_RECIPEID}/${RSTRNT_TASKID}/bootinfo" should be exist
+        The status should be success
+    End
+
+    It "abort run 26_cki_environment with concurrent task"
+        export RSTRNT_RECIPEID=54322
+        export RSTRNT_TASKID=12346
+        export RSTRNT_TASKNAME=concurrent-test-task
+        export RSTRNT_RECIPE_URL="http://${LAB_CONTROLLER}:8000/recipes/${RSTRNT_RECIPEID}"
+        When run script cki/restraint/plugins/task_run.d/26_cki_environment
+        The first line should equal "rstrnt_info *** Running Plugin: cki/restraint/plugins/task_run.d/26_cki_environment"
+        The line 2 should equal "rstrnt-report-result concurrent-test-task FAIL"
+        The line 3 should equal "Aborting task ${RSTRNT_TASKID} from recipe ${RSTRNT_RECIPEID} as system expects to run tests for recipe 54321"
+        The line 4 should equal "rstrnt-abort --server http://beaker.test.com:8000/recipes/${RSTRNT_RECIPEID}/tasks/${RSTRNT_TASKID}/status"
+        The status should be failure
     End
 End
