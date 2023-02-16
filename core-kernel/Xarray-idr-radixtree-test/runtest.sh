8c911fec614ed6fa3471286b4fda02c3873e16bc
Bruno Goncalves <bgoncalv@redhat.com>
2023-02-16 11:31:30 +0100

    Xarray-idr-radixtree-test: disableexclude packages to be able to download the src.rpm
    CKI Boot test exclude all kernel packages except the ones related to the
    kernel being tested to avoid wrong kernel being installed.
    
    This can cause problem when downloading src.rpm.
    
    For example when testing kernel-debug, kernel related packages are
    exclude, but the src.rpm is still kernel and not kernel-debug, so we
    need to remove the exclude from the repo.
    
    fix: https://gitlab.com/redhat/centos-stream/tests/kernel/kernel-tests/-/issues/1575

--- core-kernel/Xarray-idr-radixtree-test/runtest.sh
+++ core-kernel/Xarray-idr-radixtree-test/runtest.sh
@@ -66,9 +66,12 @@ function get_running_kernel_src()
 	else
 		kernelpkg="kernel"
 		# check if it is running kernel-rt
-		echo $running_kernel | grep -q -v '\.rt'
-		if [ $? -eq 1 ]; then
+		if  cki_is_kernel_rt; then
 			kernelpkg="kernel-rt"
+		fi
+		dnf_download_options=""
+		if  cki_is_kernel_debug; then
+			dnf_download_options="--disableexcludes kernel-cki"
 		fi
 		rlLog "detected rhel/fedora/ark kernel..."
 		echo $running_kernel | grep -q -v 'fc'
@@ -79,7 +82,7 @@ function get_running_kernel_src()
 			running_kernel=$(dnf -q --disablerepo="*" --enablerepo="kernel-cki" list --all "${kernelpkg}.src" --showduplicates \
 				| awk '{print$2}' | tail -1)
 		fi
-		rlRun -l "dnf download --source ${kernelpkg}-${running_kernel}"
+		rlRun -l "dnf download ${dnf_download_options} --source ${kernelpkg}-${running_kernel}"
 		rpm -ivh kernel-*.src.rpm
 		tar xf /root/rpmbuild/SOURCES/linux-*.tar.xz -C .
 	fi
