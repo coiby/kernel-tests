5d751b334ec6df88f1c82d55645fd4fbd3f1babf
zguo <zguo@redhat.com>
2023-04-20 20:55:45 +0800

    rdma: add case 2 for rdma-ndd
    Signed-off-by: zguo <zguo@redhat.com>

--- rdma/rdma-ndd/main.py
+++ rdma/rdma-ndd/main.py
@@ -10,8 +10,9 @@ from rdmaqe.common.tc import Test
 
 import libsan.host.linux as linux
 from libsan.host.cmdline import run
-
 from stqe.host.atomic_run import atomic_run
+
+from setup import change_nd_format
 
 import sys
 
@@ -55,13 +56,10 @@ def test(tc):
     else:         
         tc.tfail("Service operation failed with following errors: \n\t'" + "\n\t ".join([str(i) for i in errors_service]))
 
-     # case 2, change the node description format for all the RDMA devices
-    _, nd_before = run('cat /sys/class/infiniband/*/node_desc | grep $(hostname -s) | wc -l', return_output=True)
-    new_hostname = "mynewhostname"
-    retcode, _ = run('echo "[Service]\nEnvironment=RDMA_NDD_ND_FORMAT=%%h" >> /usr/lib/systemd/system/rdma-ndd.service',return_output=True, verbose=True)
-    if retcode == 0:
-        run('systemctl daemon-reload')
-        run('systemctl restart rdma-ndd.service')
+        # case 2, change the node description format for all the RDMA devices
+        _, nd_before = run('cat /sys/class/infiniband/*/node_desc | grep $(hostname -s) | wc -l', return_output=True)
+        change_nd_format("%%h")
+        new_hostname = "mynewhostname"
         _cmd = 'hostname ' + new_hostname
         run(_cmd)
         run('cat /sys/class/infiniband/*/node_desc', return_output=True, verbose=True)
@@ -72,15 +70,17 @@ def test(tc):
         else:
             tc.tfail("Changing node_desc format failed")
 
-    # post-test
-    # Change the node_desc format to the default one
+        # post-test
+        # Change the node_desc format to the default one
+        print("Changing the node_desc format to the default one.")
+        change_nd_format("%h %d")
 
 
 def main():
     test_class = Test()
 
-    ret = test(test_class)
-    print("Test return code: %s" % ret)
+    # ret = test(test_class)
+    # print("Test return code: %s" % ret)
 
     if not test_class.tend():
         print("FAIL: test failed")
