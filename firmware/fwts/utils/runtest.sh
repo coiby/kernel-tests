a1e8fe685541078521bec70073068712e9a74791
Yaning Wang <yaniwang@redhat.com>
2023-04-29 17:56:43 +0800

    add missing include

--- firmware/fwts/utils/runtest.sh
+++ firmware/fwts/utils/runtest.sh
@@ -5,6 +5,15 @@ auto_include=../../../automotive/include/include.sh
 [ -f $auto_include ] && . $auto_include
 declare -F kernel_automotive && kernel_automotive && is_rhivos=1 || is_rhivos=0
 
+# Include rhts environment
+if ! (($is_rhivos)); then
+    . /usr/bin/rhts-environment.sh || exit 1
+fi
+
+# Include Beaker environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+
 . ../include/runtest.sh || exit 1
 
 rlJournalStart
