e0736aa36ee803284f16c5c124bf9fed8db366bb
weiwang <weiwang@redhat.com>
2023-05-26 10:58:23 +0800

    Remove the white space of the line end

--- misc/record-proc-interrupts/runtest.sh
+++ misc/record-proc-interrupts/runtest.sh
@@ -3,7 +3,7 @@
 # Enable TMT testing for RHIVOS
 auto_include=../../automotive/include/include.sh
 [ -f $auto_include ] && . $auto_include
-declare -F kernel_automotive && kernel_automotive 
+declare -F kernel_automotive && kernel_automotive
 
 # Include Beaker environment
 . /usr/share/beakerlib/beakerlib.sh || exit 1
