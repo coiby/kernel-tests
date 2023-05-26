ad22c25b3734cccdd4237fc16348e5af6e45f39c
Mike Stowell <mstowell@redhat.com>
2023-05-26 11:18:42 -0400

    [rt] stalld: fix wrong PID assignments and clean up zombie busyloops

--- rt-tests/us/stalld/runtest.sh
+++ rt-tests/us/stalld/runtest.sh
@@ -71,7 +71,7 @@ function install_and_start_stalld() {
         # Use a higher runtime ns for boosting, equal to 0.1s
         # With the default boost timing it will take multiple boosts
         # to finish the test and will take much longer
-        stalld -v -A -t 30 -r 1000000 | tee -a "$OUTPUTFILE" &
+        stalld -v -A -t 30 -r 1000000 > >(tee -a "$OUTPUTFILE") &
         export STALLD_PID=$!
     }
 }
@@ -87,7 +87,8 @@ function run_test() {
 
         # Run a busy loop to stall cpu 1
         timeout "${MAX_RUNTIME}s" chrt -f 1 taskset -c 1 ./rt_busyloop &
-        export BUSYLOOP_PID=$!
+        BUSYLOOP_PID=$(pgrep rt_busyloop)
+        export BUSYLOOP_PID
 
         # Print process info so we can see PIDs and tell if the right process
         # is getting boosted
@@ -116,11 +117,14 @@ function run_test() {
 }
 
 function stop_stalld() {
+    # first ensure any potential zombie busyloops have been terminated
+    killall -s 9 rt_busyloop
+
     echo "Stoping stalld." | tee -a "$OUTPUTFILE"
-    kill "$STALLD_PID" &
+    kill "$STALLD_PID"
     sleep 10
     if ps -p $STALLD_PID >/dev/null; then
-        echo "stalld did not respond to SIGTERM - using SIGKILL"
+        echo "stalld did not respond gracefully to SIGTERM - using SIGKILL instead"
         kill -9 $STALLD_PID
     fi
 }
