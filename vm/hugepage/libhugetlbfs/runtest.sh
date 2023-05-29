5caacc35f1de5e715e6c43c86b74fb72d04d6c4f
Li Wang <liwang@redhat.com>
2023-05-29 17:37:16 +0800

    Revert "libhugetlbfs: avoid building the package again after rebooting"
    This reverts commit c45c375d72902fc71d10d123dbcfa6d97a97f08d.

--- vm/hugepage/libhugetlbfs/runtest.sh
+++ vm/hugepage/libhugetlbfs/runtest.sh
@@ -40,13 +40,11 @@ PACKAGE_VERSION="2.21"
 grep -q "release [5-7].*" /etc/redhat-release && PACKAGE_VERSION="2.18"
 TARGET=${PACKAGE_NAME}-${PACKAGE_VERSION}
 PACKAGE_URL="$LOOKASIDE/$PACKAGE_VERSION/$TARGET.tar.gz"
-if [ ! -e "REBOOTED" ]; then
-    bash $CDIR/utils/build.sh $PACKAGE_NAME $PACKAGE_VERSION $PACKAGE_URL
-    if [ $? -ne 0 ]; then
-        echo "Oops, failed to build $TARGET"
-        rstrnt-report-result Build_${TARGET}_failed FAIL 1
-        exit 0
-    fi
+bash $CDIR/utils/build.sh $PACKAGE_NAME $PACKAGE_VERSION $PACKAGE_URL
+if [ $? -ne 0 ]; then
+    echo "Oops, failed to build $TARGET"
+    rstrnt-report-result Build_${TARGET}_failed FAIL 1
+    exit 0
 fi
 
 TESTAREA=/mnt/testarea
@@ -236,7 +234,6 @@ EOF
        if [[ x"${HPSIZE}" == "x512M" && ${free_hugepages} -lt $HPCOUNT && ( -z "${RSTRNT_REBOOTCOUNT}" || ${RSTRNT_REBOOTCOUNT} -eq 0 ) ]]; then
       rlLog "Have ${free_hugepages} free hugepages of ${HPCOUNT} needed.  Rebooting with 2M hugepages"
       grubby --args="default_hugepagesz=2M" --update-kernel /boot/vmlinuz-$(uname -r)
-      touch REBOOTED
       rstrnt-reboot
       # Make sure the script doesn't continue if rstrnt-reboot get's killed
       # https://github.com/beaker-project/restraint/issues/219
@@ -271,6 +268,5 @@ EOF
         rlRun "umount -a -t hugetlbfs"
         rlRun "hugeadm --pool-pages-max ${HPSIZE}:0"
         rlRun "mv /etc/sysctl.conf.backup /etc/sysctl.conf"
-        rlRun "rm -fr REBOOTED"
     rlPhaseEnd
 rlJournalEnd
