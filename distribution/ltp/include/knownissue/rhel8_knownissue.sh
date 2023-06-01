8784501125454cf8a56341c640455a06444ec822
Bruno Goncalves <bgoncalv@redhat.com>
2023-06-01 10:41:11 +0200

    ltp: disable dynamic_debug01 on kernel-rt-debug on rhel8 < 8.5
    related https://gitlab.com/redhat/centos-stream/tests/kernel/kernel-tests/-/issues/1613

--- distribution/ltp/include/knownissue/rhel8_knownissue.sh
+++ distribution/ltp/include/knownissue/rhel8_knownissue.sh
@@ -20,6 +20,8 @@ function rhel8_fatal_issues()
 	osver_in_range "800" "810" && cki_is_kernel_debug && tskip "read_all_sys" fatal
 	# Bug 2115120 - RHEL8: kernel-rt: WARNING: possible circular locking dependency detected (raw_v6_hashinfo.lock->(softirq_ctrl.lock).lock->raw_v6_hashinfo.lock
 	is_rt && cki_is_kernel_debug && osver_in_range "806" "807" && tskip "read_all_proc" fatal
+	# hang with kmemleak: kmemleak_alloc: https://gitlab.com/redhat/centos-stream/tests/kernel/kernel-tests/-/issues/1613
+	is_rt && cki_is_kernel_debug && osver_in_range "800" "805" && tskip "dynamic_debug01" fatal
 }
 
 function rhel8_unfix_issues()
