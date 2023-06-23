3fdc0cfe77dccc197cab181dbc3d497d0cc4b17e
Julia Graham <jugraham@redhat.com>
2023-06-23 14:42:03 -0400

    checking only current dmesg logs

--- general/ftrace/regression/1251361-warning-at-kernel-stacktrace/runtest.sh
+++ general/ftrace/regression/1251361-warning-at-kernel-stacktrace/runtest.sh
@@ -14,12 +14,13 @@ rlJournalStart
         fi
     rlPhaseEnd
     rlPhaseStartTest
+        curTime=$(dmesg | tail -n 1 | awk -F"[][]" '{print $2}')
         rlRun "echo stacktrace > /sys/kernel/debug/tracing/trace_options"
         rlRun "echo \"p kfree\" >> /sys/kernel/debug/tracing/kprobe_events"
         rlRun "echo \"r exit_mmap\" >> /sys/kernel/debug/tracing/kprobe_events"
         rlRun "echo 1 > /sys/kernel/debug/tracing/events/kprobes/enable"
         rlRun "echo 1 >/sys/kernel/debug/tracing/tracing_on"
-        rlRun "dmesg | egrep 'WARN|save_stack_trace_regs'" 1
+        rlRun "dmesg | sed -ne \"/$curTime/,$ p\" | egrep 'WARN|save_stack_trace_regs'" 1
     rlPhaseEnd
     rlPhaseStartCleanup
         trace-cmd reset
