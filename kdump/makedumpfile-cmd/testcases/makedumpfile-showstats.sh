facf3ad761eb54c214b4aac7e7428319719e8de8
Jie Li <jieli@redhat.com>
2023-06-30 14:17:07 +0800

    kdump: add testcase for makedumpfile showstats(v4)
    Signed-off-by: Jie Li <jieli@redhat.com>

--- kdump/makedumpfile-cmd/testcases/makedumpfile-showstats.sh
+++ kdump/makedumpfile-cmd/testcases/makedumpfile-showstats.sh
@@ -3,6 +3,17 @@
 # Source Kdump tests common functions.
 . ../include/runtest.sh
 
+if [ "${RELEASE}" -lt 8 ]; then
+    Skip "This feature is not supported."
+    Report
+fi
+
+if $IS_RHEL9;then
+    CheckSkipTest kexec-tools 2.0.23-5 && Report
+elif $IS_RHEL8;then
+    CheckSkipTest kexec-tools 2.0.20-66 && Report
+fi
+
 MakedumpfileShowstatsTest()
 {
     # 1-compression methods:
@@ -25,49 +36,53 @@ MakedumpfileShowstatsTest()
 
     # 1: compression_methods
     local params_lst=("-z" "-c" "-l" "-p" "-E")
+    local logfile="1-compression_methods.log"
+    rm -f "${logfile}"
     MKPARAM1="-d 31"
-    rm -f "1-compression_methods.log"
     for compression in "${params_lst[@]}"; do
         cmdstr="${MKCMD} ${compression} ${MKPARAM1} ${MKPARAM2}"
         Log "CMD: ${cmdstr}"
-        ${cmdstr} | tee -a "1-compression_methods.log"
-        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}"
+        ${cmdstr} | tee -a "${logfile}"
+        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}, please check log:${logfile}"
     done
-    RhtsSubmit "$(pwd)/1-compression_methods.log"
+    RhtsSubmit "$(pwd)/${logfile}"
 
     # 2: dump level with -lzo compression
-    rm -f "2-dump_lvl.log"
+    logfile="2-dump_lvl.log"
+    rm -f "${logfile}"
     params_lst=("-d 7" "-d 9" "-d 16")
     MKPARAM1="-l"
     for dump_lvl in "${params_lst[@]}"; do
         cmdstr="${MKCMD} ${dump_lvl} ${MKPARAM1} ${MKPARAM2}"
         Log "CMD: ${cmdstr}"
-        ${cmdstr} | tee -a "2-dump_lvl.log"
-        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}"
+        ${cmdstr} | tee -a "${logfile}"
+        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}, please check log:${logfile}"
     done
-    RhtsSubmit "$(pwd)/2-dump_lvl.log"
+    RhtsSubmit "$(pwd)/${logfile}"
 
     # 3: message level 1/7 with lzo compression
-    rm -f "3-msg_lvl.log"
+    logfile="3-msg_lvl.log"
+    rm -f "${logfile}"
     params_lst=("--message-level 1" "--message-level 7")
     MKPARAM1="-l -d 31"
     for msg_lvl in "${params_lst[@]}"; do
         cmdstr="${MKCMD} ${msg_lvl} ${MKPARAM1} ${MKPARAM2}"
         Log "CMD: ${cmdstr}"
-        ${cmdstr} | tee -a "3-msg_lvl.log"
-        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}"
+        ${cmdstr} | tee -a "${logfile}"
+        [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}, please check log:${logfile}"
     done
-    RhtsSubmit "$(pwd)/3-msg_lvl.log"
+    RhtsSubmit "$(pwd)/${logfile}"
 
     # 4: " -F -l -d 31"
-    rm -f "4-F-l-d_31.log"
+    logfile="4-F-l-d_31.log"
+    rm -f "${logfile}"
     MKPARAM1="-F -l -d 31"
     MKPARAM2="/proc/kcore --dry-run --show-stats"
     cmdstr="${MKCMD} ${MKPARAM1} ${MKPARAM2} 2>&1"
     Log "CMD: ${cmdstr}"
-    ${cmdstr} | tee -a "4-F-l-d_31.log"
-    [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}"
-    RhtsSubmit "$(pwd)/4-F-l-d_31.log"
+    ${cmdstr} | tee -a "${logfile}"
+    [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}, please check log:${logfile}"
+    RhtsSubmit "$(pwd)/${logfile}"
 
 }
 
