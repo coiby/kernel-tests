8d2a4eb185e06d3253f8cac45d18b4a2bbe81ad8
Jie Li <jieli@redhat.com>
2023-07-03 10:29:02 +0800

    kdump: add testcase for initrd squash rootfs support(+bugfix)
    Signed-off-by: Jie Li <jieli@redhat.com>

--- kdump/makedumpfile-cmd/testcases/makedumpfile-showstats.sh
+++ kdump/makedumpfile-cmd/testcases/makedumpfile-showstats.sh
@@ -78,9 +78,9 @@ MakedumpfileShowstatsTest()
     rm -f "${logfile}"
     MKPARAM1="-F -l -d 31"
     MKPARAM2="/proc/kcore --dry-run --show-stats"
-    cmdstr="${MKCMD} ${MKPARAM1} ${MKPARAM2} 2>&1"
-    Log "CMD: ${cmdstr}"
-    ${cmdstr} | tee -a "${logfile}"
+    cmdstr="${MKCMD} ${MKPARAM1} ${MKPARAM2}"
+    Log "CMD: ${cmdstr} 2>&1"
+    ${cmdstr} 2>&1 | tee -a "${logfile}"
     [ "${PIPESTATUS[0]}" -ne 0 ] && Error "Failed: ${cmdstr}, please check log:${logfile}"
     RhtsSubmit "$(pwd)/${logfile}"
 
