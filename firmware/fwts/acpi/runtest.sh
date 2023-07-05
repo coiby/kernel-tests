a9ff2cfe8fb8175ce6f11a9c0569d7c2bd738b34
PengshanYu <peyu@redhat.com>
2023-07-05 15:23:28 +0800

    add tmt and rhivos support

--- firmware/fwts/acpi/runtest.sh
+++ firmware/fwts/acpi/runtest.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Enable TMT testing for RHIVOS
+auto_include=../../../automotive/include/rhivos.sh
+[ -f $auto_include ] && . $auto_include
+declare -F kernel_automotive && kernel_automotive && is_rhivos=1 || is_rhivos=0
+
+# Include rhts environment
+if ! (($is_rhivos)); then
+    . /usr/bin/rhts-environment.sh || exit 1
+fi
+
 # Include Beaker environment
-. /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 # source fwts include/library
