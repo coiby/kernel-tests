081878fed5cd83bd173538cefe5a709fdc42a812
George Jacob <gjacob@redhat.com>
2023-07-07 12:23:46 -0500

    Modified runtest.sh to look for libcki.sh instead of rhivos.sh. Modified lines to use dnf or yum whichever is present insead of defaulting to yum

--- security/crypto/1600394/runtest.sh
+++ security/crypto/1600394/runtest.sh
@@ -27,15 +27,20 @@
 
 # Include Beaker environment
 . /usr/share/beakerlib/beakerlib.sh || exit 1
-. ../../include/rhivos.sh
+#. ../../include/rhivos.sh
+. ../../../cki_lib/libcki.sh
 
 rlJournalStart
     rlPhaseStartSetup
         if [ "$(rlIsRHEL 7)" ]; then
             rlRun "yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
         fi
-        if ! kernel_automotive; then
-            rlRun "yum install -y python3"
+        if ! cki_is_kernel_automotive; then
+	    if [ -x /usr/bin/dnf ]; then
+                rlRun "dnf install -y python3"
+            elif [ -x /usr/bin/yum ]; then
+                rlRun "yum install -y python3"
+            fi
         fi
         rlRun "ls -l /usr/bin/python3*"
     rlPhaseEnd
