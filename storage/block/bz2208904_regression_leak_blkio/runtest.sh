49a79bde576f49e2a72162ed227cdbade88a743e
Changhui Zhong <czhong@redhat.com>
2023-07-17 15:42:12 +0800

    Add new test case for bz2208904
    Signed-off-by: Changhui Zhong <czhong@redhat.com>

---
+++ storage/block/bz2208904_regression_leak_blkio/runtest.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing
+# to use, modify, copy, or redistribute it subject to the terms
+# and conditions of the GNU General Public License version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+#
+
+# Include enviroment and libraries
+. /usr/share/beakerlib/beakerlib.sh     || exit 1
+
+function run_test()
+{
+    rlRun "rpm -q podman || yum install -y podman"
+    rlRun "rpm -q container-tools || yum module install -y container-tools"
+    sleep 5
+
+    rlLog "The original num_cgroups value:"
+    rlRun "cat /proc/cgroups | grep -e subsys -e blkio | column -t"
+    num_cgroupsV1=$(cat /proc/cgroups | grep -e subsys -e blkio | column -t | awk 'NR==2 { print $3 }')
+
+    for i in $(seq 1000); do podman run --name=test --replace centos /bin/echo 'running'; done >/dev/null &
+    pid=$!
+
+    for ((i=1; i<=10; i++));do
+        rlRun "cat /proc/cgroups | grep -e subsys -e blkio | column -t >> num_cgroups.txt"
+        sleep 30
+    done
+
+    wait $pid
+
+    rlRun "cat num_cgroups.txt"
+    my_array=()
+    for i in $(cat num_cgroups.txt | awk 'NR%2==0 { print $3 }');do
+        my_array+=("$i")
+    done
+
+    for item in "${my_array[@]}";do
+        if [ "$item" -gt $(("$num_cgroupsV1" + 20)) ];then
+            rlFail "Test failed: num_cgroups increase"
+        else
+            rlPass "Test passed: num_cgroups does not increase"
+        fi
+    done
+}
+
+function check_log()
+{
+    rlRun "dmesg | grep -i 'Call Trace:'" 1 "check the errors"
+    rlRun "dmesg | grep -i 'kernel BUG at'" 1 "check the errors"
+    rlRun "dmesg | grep 'BUG:'" 1 "check the errors"
+    rlRun "dmesg | grep -i 'WARNING:'" 1 "check the errors"
+}
+
+rlJournalStart
+    rlPhaseStartTest
+        rlRun "dmesg -C"
+        rlRun "uname -a"
+        rlLog "$0"
+        run_test
+        check_log
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd
