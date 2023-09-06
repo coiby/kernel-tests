5f80890d0a516ce5c560d4c26905a6d3df26372a
Changhui Zhong <czhong@redhat.com>
2023-09-06 09:25:20 +0000

    update block test case

--- storage/block/bz2167245_blkg_leak_regression/runtest.sh
+++ storage/block/bz2167245_blkg_leak_regression/runtest.sh
@@ -22,12 +22,47 @@
 
 function run_test()
 {
+    if rlIsRHEL 8; then
+        run4rhel8
+    elif rlIsRHEL '>=9.0'; then
+        run4rhel9
+    else
+        rlLog "skip this case"
+        return 0
+    fi
+    rlRun "rm -rf trace.log"
+}
+
+function run4rhel8()
+{
+    rlRun "chmod a+x rhel8_blkg.bt"
+    rlRun "./rhel8_blkg.bt"
+    rlRun "./rhel8_blkg.bt | tee trace.log"
+    rlRun "cat trace.log |  grep 'blkg_alloc'"
+    a=$(cat trace.log |  grep 'blkg_alloc' | head -n 1 |  awk -F 'blkg_alloc' '{print $2}')
+    rlRun "cat trace.log | grep \"${a}\""
+    b=$(cat trace.log | grep "${a}" | wc -l)
+    if [ ${b} == 2 ];then
+        rlPass "Testing Pass"
+    else
+        rlFail "Testing Failed,request queue won't be freed"
+    fi
+}
+
+function run4rhel9()
+{
     rlRun "chmod a+x blkg.bt"
     rlRun "./blkg.bt"
     rlRun "./blkg.bt | tee trace.log"
     rlRun "cat trace.log |  grep 'blk_mq_release'"
-    rlRun "cat trace.log |  grep 'blk_release_queue'"
-    rlRun "rm -rf trace.log"
+    a=$(cat trace.log |  grep 'blk_mq_release' | head -n 1 |  awk -F 'blk_mq_release' '{print $2}')
+    rlRun "cat trace.log | grep \"${a}\""
+    b=$(cat trace.log | grep "${a}" | wc -l)
+    if [ ${b} == 2 ];then
+        rlPass "Testing Pass"
+    else
+        rlFail "Testing Failed,request queue won't be freed"
+    fi
 }
 
 function check_log()
