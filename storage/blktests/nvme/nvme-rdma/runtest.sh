bae4f42b7f86eb73d9d3e5413ad62e17b44f5803
Yi Zhang <yi.zhang@redhat.com>
2023-10-16 17:58:41 +0800

    storage/blktests: nvme-rdma nvme/031 fixed in 8.8
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/nvme-rdma/runtest.sh
+++ storage/blktests/nvme/nvme-rdma/runtest.sh
@@ -84,7 +84,7 @@ function get_test_cases_rdma
 		testcases+=" nvme/028"
 		testcases+=" nvme/029"
 		uname -ri | grep -q "4.18.0-147.*s390x" || testcases+=" nvme/030" # BZ1753057, skip on 8.1.z fixed on 8.2
-		uname -ri | grep "4.18.0-147" | grep -Eq "s390x|ppc64le|aarch64" || testcases+=" nvme/031"
+		uname -ri | grep -qE "el8_1|el8_6|el8_7" || testcases+=" nvme/031"
 	elif rlIsRHEL 9 || rlIsFedora || rlIsCentOS 9; then
 		testcases+=" nvme/003"
 		testcases+=" nvme/004"
