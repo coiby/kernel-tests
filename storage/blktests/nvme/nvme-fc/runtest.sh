876cb35bc5476da9689819848027a104973ea504
Bruno Goncalves <bgoncalv@redhat.com>
2023-10-20 16:52:52 +0200

    blktests/nvme/nvme-fc: add shellspec test

--- storage/blktests/nvme/nvme-fc/runtest.sh
+++ storage/blktests/nvme/nvme-fc/runtest.sh
@@ -3,7 +3,9 @@
 TNAME="storage/blktests/nvme/nvme-fc"
 TRTYPE=${TRTYPE:-"fc"}
 
-. ../../include/include.sh || exit 1
+FILE=$(readlink -f "${BASH_SOURCE[0]}")
+CDIR=$(dirname "$FILE")
+. "$CDIR"/../../include/include.sh || exit 1
 
 function get_test_cases_fc
 {
@@ -50,25 +52,34 @@ function get_test_cases_fc
 	echo "$testcases"
 }
 
-. ../include/build.sh
+function main
+{
+	enable_nvme_core_multipath
 
-enable_nvme_core_multipath
+	test_ws=./blktests
+	ret=0
+	trtype=$TRTYPE
+	testcases_default=""
+	testcases_default+=" $(get_test_cases_"$trtype")"
+	testcases=${_DEBUG_MODE_TESTCASES:-"$testcases_default"}
+	for testcase in $testcases; do
+		nvme_trtype="$trtype" do_test "$test_ws" "$testcase"
+		result=$(get_test_result "$test_ws" "$testcase")
+		report_test_result "$result" "nvme-$trtype: $TNAME/tests/$testcase"
+		((ret += $?))
+	done
 
-test_ws=./blktests
-ret=0
-trtype=$TRTYPE
-testcases_default=""
-testcases_default+=" $(get_test_cases_"$trtype")"
-testcases=${_DEBUG_MODE_TESTCASES:-"$testcases_default"}
-for testcase in $testcases; do
-	nvme_trtype="$trtype" do_test "$test_ws" "$testcase"
-	result=$(get_test_result "$test_ws" "$test_case")
-	report_test_result "$result" "nvme-$trtype: $TNAME/tests/$test_case"
-	((ret += $?))
-done
+	if (( ret != 0 )); then
+		echo ">> There are failing tests, pls check it"
+	fi
 
-if (( ret != 0 )); then
-	echo ">> There are failing tests, pls check it"
+	return
+}
+
+# don't run it if running as part of shellspec
+# https://github.com/shellspec/shellspec#__sourced__
+if [ ! "${__SOURCED__:+x}" ]; then
+	. "$CDIR"/../include/build.sh
+
+	main
 fi
-
-exit 0
