91a2c737a8149ead5b4bec62899e71e48d3412f9
Samuel Dobron <sdobron@redhat.com>
2023-11-16 15:02:01 +0100

    distribution/kernelinstall: allow eln kernels to install extra modules
    Allow ELN kernels to install `kernel-modules-{extra,internal}`
    when `KERNELARGEXTRAMODULES` resp. `KERNELARGINTERNALMODULES`
    are provided.

--- distribution/kernelinstall/runtest.sh
+++ distribution/kernelinstall/runtest.sh
@@ -825,7 +825,7 @@ function YumInstallKernel ()
             DeBug "Exit YumInstallPackage FAIL 5 (YUM exited with a failure)"
         fi
         # Install kernel-modules-extra if running on RHEL8+
-        if echo "$KERNELARGVERSION" | grep -Eq '\.(el8|elrdy|el9)'; then
+        if echo "$KERNELARGVERSION" | grep -Eq '\.(el8|elrdy|el9|eln)'; then
             if [ "$KERNELARGEXTRAMODULES" == "1" ]; then
                 echo "***** Install kernel-modules-extra package via yum ${testkername}-modules-extra-${KERNELARGVERSION}.$kernarch *****" | tee -a $OUTPUTFILE
                 $yumcmd -y install ${testkername}-modules-extra-${KERNELARGVERSION}.$kernarch
@@ -868,7 +868,7 @@ function BrewInstallKernel ()
           popd
           # RHEL8 kernels are now provided by the meta package kernel which
           # requires kernel-core and kernel-modules packages
-          if echo "$KERNELARGVERSION" | grep -Eq '\.(el8|elrdy|el9)'; then
+          if echo "$KERNELARGVERSION" | grep -Eq '\.(el8|elrdy|el9|eln)'; then
              testkerncore=$testkername-core-$KERNELARGVERSION
              testkernmodules=$testkername-modules-$KERNELARGVERSION
              testkernmodules_core=$testkername-modules-core-$KERNELARGVERSION
