a8a8b24f2b77f745350b1642c99fa996ca52e510
Bruno Goncalves <bgoncalv@redhat.com>
2023-11-16 16:04:27 +0100

    ltp/include/runtest.sh: revert attempt to fix SC2010

--- distribution/ltp/include/runtest.sh
+++ distribution/ltp/include/runtest.sh
@@ -189,7 +189,9 @@ RprtRslt ()
     # Always upload parsed test log for those failed test cases
     GetFailureLog $logfile_run "None" > $logfile_fail
     [ -s $logfile_fail ] && SubmitLog $logfile_fail
-    for failed_test in *.fail.log; do
+    # shellcheck disable=SC2010
+    failed_tests=$(ls *.fail.log)
+    for failed_test in $failed_tests; do
         # skip logfile_fail as it is not a test case fail log
         if [ "$failed_test" == "$logfile_fail" ]; then
             continue
