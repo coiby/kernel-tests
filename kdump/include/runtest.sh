90f5394824d3dc0e77cb4477930bfd9f68e7f2f9
Xiaoying Yan <yiyan@redhat.com>
2023-11-20 16:34:10 +0800

    Kdump: turn off the POSIX to avoid syntax errors.
    On the system with enabled POSIX,it may cause some syntax errors,like below:
    posix          	on
    -bash: ../../automotive/include/rhivos.sh: line 147: syntax error near unexpected token `>'
    -bash: ../../automotive/include/rhivos.sh: line 147: `    eval "$cmd" > >(tee -a $OUTPUTFILE)'
    
    if turn off the POSIX,it will avoid the syntax error:
    posix          	off
    
    Signed-off-by: Xiaoying Yan <yiyan@redhat.com>

--- kdump/include/runtest.sh
+++ kdump/include/runtest.sh
@@ -14,6 +14,11 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Turn off the POSIX to avoid syntax errors
+set +o posix
+
+. ../../automotive/include/rhivos.sh
+
 . ../include/lib.sh
 . ../include/kdump.sh
 . ../include/kdump-multi.sh
