ab6d65975cbed935442cac3fe33683c5d0552ecf
Yi Zhang <yi.zhang@redhat.com>
2023-12-29 15:12:38 +0800

    storage/blktests: update nvme/nvme-tcp/spec/runtest_spec.sh
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/nvme-tcp/spec/runtest_spec.sh
+++ storage/blktests/nvme/nvme-tcp/spec/runtest_spec.sh
@@ -8,8 +8,8 @@ Describe 'blktests - nvme-tcp - main'
         echo "enable_nvme_core_multipath"
     End
 
-    Mock get_test_cases_tcp
-        echo "nvme/001"
+    Mock Mock get_test_cases_list NVME_TCP
+        echo "nvme/003"
     End
 
     Mock do_test
@@ -26,9 +26,9 @@ Describe 'blktests - nvme-tcp - main'
         When call main
 
         The line 1 should equal "enable_nvme_core_multipath"
-        The line 2 should equal "do_test ${CDIR}/blktests nvme/001"
+        The line 2 should equal "do_test ${CDIR}/blktests nvme/003"
         The line 3 should equal "nvme_trtype = tcp"
-        The line 4 should equal "rstrnt-report-result nvme-tcp: storage/blktests/nvme/nvme-tcp/tests/nvme/001 PASS 0"
+        The line 4 should equal "rstrnt-report-result nvme-tcp: storage/blktests/nvme/nvme-tcp/tests/nvme/003 PASS 0"
         The status should be success
     End
 End
