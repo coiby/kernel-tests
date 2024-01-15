879ce0462fa3c66c62d1d7e25036395fd4c604b2
Yi Zhang <yi.zhang@redhat.com>
2024-01-15 14:16:15 +0800

    storage/blktests: update case_type
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/blktests/nvme/nvme-tcp/runtest.sh
+++ storage/blktests/nvme/nvme-tcp/runtest.sh
@@ -18,7 +18,7 @@ function main
 	testcases_default="$(get_test_cases_list $CASE_TYPE)"
 	testcases=${_DEBUG_MODE_TESTCASES:-"$testcases_default"}
 	if [ -z "$testcases" ]; then
-		cki_abort_task "Abort test because $case_type case list is empty"
+		cki_abort_task "Abort test because $CASE_TYPE case list is empty"
 	fi
 	for testcase in $testcases; do
 		nvme_trtype="$trtype" do_test "$test_ws" "$testcase"
