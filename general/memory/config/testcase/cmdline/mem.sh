f94185fe533f9391f6a714570eb73bbb7dd0eee6
Ian Wienand <iwienand@redhat.com>
2024-01-18 09:54:03 +1100

    general/memory/.../mem.sh : align mem= for PPC64
    As discussed inline, there are a range of ppc64 kernels that do not
    auto-align incoming mem= arguments to the required 16mib boundary.  To
    make this test work consistently, align it here.
    
    Partial-Issue: https://issues.redhat.com/browse/RHEL-8591

--- general/memory/config/testcase/cmdline/mem.sh
+++ general/memory/config/testcase/cmdline/mem.sh
@@ -24,6 +24,13 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+function align()
+{
+    local mem=$1
+    local alignment=$2
+    local mask=$(( alignment - 1 ))
+    echo $(( ((mem + mask)&~(mask)) ))
+}
 
 function mem()
 {
@@ -39,6 +46,18 @@ function mem()
     memtotal=$(cat ${DIR_DEBUG}/MEMTOTAL)
     mem_half=$((memtotal/2))
     mem_double=$((memtotal*2))
+
+    # ppc64 requires 16MB aligned mem= so it can allocate large pages
+    # correctly, or it fails to boot [1].  [2] discusses how setting
+    # the alignment of the mem= command has been inadvertently dropped
+    # from various kernels, which leads to boot hangs.  Align it here
+    # to make sure this consistently works.
+    #  [1] https://kernel.googlesource.com/pub/scm/linux/kernel/git/tglx/history/+/34f96943a4eb7e3074a91ad847d8ca6ad4a2e97c
+    #  [2] https://issues.redhat.com/browse/RHEL-8591
+    if uname -r | grep -q ppc64; then
+        mem_half=$(align $mem_half 16384)
+        mem_double=$(align $mem_double 16384)
+    fi
 
     [ $mem_half -gt 1048576 ] && setup_cmdline_args "mem=${mem_half}K" HALF && rlAssertGreaterOrEqual "Assert current memory:$mem_curr" $mem_half $mem_curr
     setup_cmdline_args "mem=${mem_double}K" DOUB
