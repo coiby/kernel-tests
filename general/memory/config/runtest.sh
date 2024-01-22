e501f37ed42d88bce6d68da4dbeca1e8b0135d13
Chunyu Hu <chuhu@redhat.com>
2024-01-22 17:43:18 +0800

    general/config: fix shellcheck SC2034
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- general/memory/config/runtest.sh
+++ general/memory/config/runtest.sh
@@ -64,6 +64,7 @@ function run_cases()
         subfunc=$(basename ${subcase%.sh})
         pname=$subfunc
         ptype=FAIL
+		# shellcheck disable=SC1090
         source $subcase
 
         check_knownissues $subfunc
