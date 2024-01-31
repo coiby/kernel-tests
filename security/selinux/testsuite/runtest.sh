a817a030401990a5d10ab20c755d66bf2aa5f32e
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2024-01-31 11:58:00 -0600

    Removing failing selinux tests on automotive

--- security/selinux/testsuite/runtest.sh
+++ security/selinux/testsuite/runtest.sh
@@ -349,9 +349,11 @@ rlJournalStart
                     "Apply workaround for missing XFS quota checks"
             fi
             #Excluding tests for automotive
-            if (uname -r | grep -w -q el[0-9]*iv); then
+            if cki_is_kernel_automotive; then
+                #Excluded as automotive doesn't support Quota related functionality VROOM-17160
+                #Functionality removed by disabling Configuration Parameter : CONFIG_QUOTA_TREE
                 exclude_tests+=" filesystem/ext4 fs_filesystem/ext4"
-            fi
+
         fi
 
         # CKI mainline kernels don't ship with module build infrastructure
