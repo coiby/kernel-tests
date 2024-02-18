93cf712d6686e1341da934ef1ca96ffa91a53448
Xiaoying Yan <yiyan@redhat.com>
2024-02-18 18:18:44 +0800

    kdump: update the crashkernel value to the latest
    Signed-off-by: Xiaoying Yan <yiyan@redhat.com>

--- kdump-cki/include/runtest.sh
+++ kdump-cki/include/runtest.sh
@@ -595,19 +595,19 @@ DefKdumpMem()
         fi
 
     elif $IS_RHEL8 || $IS_CentOS8; then
-        if   [[ "${K_ARCH}"  = "x86_64" ]]; then args="crashkernel=0G-4G:160M,4G-64G:192M,64G-1T:256M,1T-:512M"
-        elif [[ "${K_ARCH}"  = "s390x"  ]]; then args="crashkernel=0G-4G:160M,4G-64G:192M,64G-1T:256M,1T-:512M"
+        if   [[ "${K_ARCH}"  = "x86_64" ]]; then args="crashkernel=0G-4G:192M,4G-64G:256M,64G-:512M"
+        elif [[ "${K_ARCH}"  = "s390x"  ]]; then args="crashkernel=0G-4G:192M,4G-64G:256M,64G-:512M"
         elif [[ "${K_ARCH}"  = ppc64*  ]]; then
-            args="crashkernel=0M-4G:384M,4G-16G:512M,16G-64G:1G,64G-128G:2G,128G-:4G"
-        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=512M"
+            args="crashkernel=0G-4G:384M,4G-16G:512M,16G-64G:1G,64G-128G:2G,128G-:4G"
+        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=480M"
         fi
 
     elif $IS_RHEL9 || $IS_CentOS9; then
-        if   [[ "${K_ARCH}"  = "x86_64" ]]; then args="crashkernel=0G-4G:192M,4G-64G:192M,64G-1T:256M,1T-:512M"
-        elif [[ "${K_ARCH}"  = "s390x"  ]]; then args="crashkernel=0G-4G:192M,4G-64G:192M,64G-1T:256M,1T-:512M"
+        if   [[ "${K_ARCH}"  = "x86_64" ]]; then args="crashkernel=0G-4G:192M,4G-64G:256M,64G-:512M"
+        elif [[ "${K_ARCH}"  = "s390x"  ]]; then args="crashkernel=0G-4G:192M,4G-64G:256M,64G-:512M"
         elif [[ "${K_ARCH}"  = ppc64*  ]]; then
             args="crashkernel=0M-4G:384M,4G-16G:512M,16G-64G:1G,64G-128G:2G,128G-:4G"
-        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=512M"
+        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=0G-4G:256M,4G-64G:320M,64G-:576M"
         fi
 
     elif $IS_FC; then
@@ -615,7 +615,7 @@ DefKdumpMem()
         elif [[ "${K_ARCH}"  = "s390x"  ]]; then args="crashkernel=0G-4G:192M,4G-64G:192M,64G-1T:256M,1T-:512M"
         elif [[ "${K_ARCH}"  = ppc64*  ]]; then
             args="crashkernel=0M-4G:384M,4G-16G:512M,16G-64G:1G,64G-128G:2G,128G-:4G"
-        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=512M"
+        elif [[ "${K_ARCH}"  = "aarch64"  ]]; then args="crashkernel=1G-4G:256M,4G-64G:766M,64G-:1G"
         fi
 
     elif $IS_RHEL5; then
