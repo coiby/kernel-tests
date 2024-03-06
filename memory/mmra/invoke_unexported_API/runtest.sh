c43b2bcc3ffc3ba388817006263b9f8e839f0375
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2024-03-06 11:54:18 -0600

    Modified rlRun steps in runtest

--- memory/mmra/invoke_unexported_API/runtest.sh
+++ memory/mmra/invoke_unexported_API/runtest.sh
@@ -9,21 +9,26 @@ rlJournalStart
     rlPhaseStartSetup
         rlShowRunningKernel
     rlPhaseEnd
-    rlPhaseStart
-        # rlRun "make" 2 "-p 'File doesn't link as expected'"
-        rlRun "make"
-    rlPhaseEnd
+    # rlPhaseStart
+    #     # rlRun "make" 2 "-p 'File doesn't link as expected'"
+    #     rlRun "make"
+    # rlPhaseEnd
 
-    rlPhaseStart
-        rlRun "make" 0-255
-        if [ -f $MODFILE ]; then
-            rlRun "insmod $MODFILE" 1 "Loading the module should fail."
-            rlRun "dmesg | grep -i 'unresolved symbol'" 0 "Checking dmesg for unresolved symbol error"
-        else
-            rlLogInfo "Module file does not exist. Linking step failed as expected."
-        fi
+    # rlPhaseStart
+    #     rlRun "make" 0-255
+    #     if [ -f $MODFILE ]; then
+    #         rlRun "insmod $MODFILE" 1 "Loading the module should fail."
+    #         rlRun "dmesg | grep -i 'unresolved symbol'" 0 "Checking dmesg for unresolved symbol error"
+    #     else
+    #         rlLogInfo "Module file does not exist. Linking step failed as expected."
+    #     fi
+    # rlPhaseEnd
+    rlPhaseStart "Compilation"
+    if ! make; then
+        rlPass "Compilation failed as expected due to unresolved symbols"
+    rlRun "insmod $MODFILE" 1 "Loading the module should fail."
+    rlRun "dmesg | grep -i 'unresolved symbol'" 0 "Checking dmesg for unresolved symbol error"
     rlPhaseEnd
-
 
 rlPhaseStartCleanup
         rlRun "make clean" 0 "Cleaning up"
