1d682d1b76658b2b4e0ac1687a789e987511e7b4
Yi Zhang <yi.zhang@redhat.com>
2024-03-07 08:37:12 +0800

    storage/ssd: update nvme telemetry-log for rhel8
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/ssd/nvme_cli_command_test/main.sh
+++ storage/ssd/nvme_cli_command_test/main.sh
@@ -54,9 +54,9 @@ for DISK in $DISKS; do
 	tok "nvme get-ns-id ${NVME_DISK}"
 	tok "nvme get-log --log-id=2 --log-len=512 ${NVME_DISK}"
 
-	if [[ $MODEL =~ "SAMSUNG MZ1L21T9HCLS-00A07"|"SAMSUNG MZQL21T9HCJR-00A07"|"SAMSUNG MZQL2960HCJR-00A07"|"Dell Ent NVMe v2 AGN RI U.2"|"Dell Ent NVMe CM6 RI"|"Dell Ent NVMe P5500 RI U.2"|"Dell Express Flash NVMe PM1725 1.6TB"|"SAMSUNG MZPLJ1T6HBJR-00007"|"Dell Express Flash PM1725b"|"Dell Express Flash NVMe P4800X"|"Dell Ent NVMe v2 AGN FIPS MU U.2"|"Dell Express Flash NVMe P4500"|"Samsung SSD 983 DCT" ]]; then
+	if [[ $MODEL =~ "SAMSUNG MZ1L21T9HCLS-00A07"|"SAMSUNG MZQL21T9HCJR-00A07"|"SAMSUNG MZQL2960HCJR-00A07"|"Dell Ent NVMe v2 AGN RI U.2"|"Dell Ent NVMe CM6 RI"|"Dell Ent NVMe P5500 RI U.2"|"Dell Express Flash NVMe PM1725 1.6TB"|"SAMSUNG MZPLJ1T6HBJR-00007"|"Dell Express Flash NVMe P4800X"|"Dell Ent NVMe v2 AGN FIPS MU U.2"|"Samsung SSD 983 DCT" ]]; then
 		tok "nvme telemetry-log ${NVME_CHAR} --output-file=telemetry_log.bin"
-	elif [[ $MODEL =~ "Micron_9300_MTFDHAL3T8TDP"|"Dell Express Flash PM1725a 1.6TB AIC"|"INTEL SSDPEDMD016T4"|"Dell Express Flash NVMe P4600 2.0TB AIC" ]]; then
+	elif [[ $MODEL =~ "Micron_9300_MTFDHAL3T8TDP"|"Dell Express Flash PM1725a 1.6TB AIC"|"INTEL SSDPEDMD016T4"|"Dell Express Flash NVMe P4600 2.0TB AIC"|"Dell Express Flash PM1725b"|"Dell Express Flash NVMe P4500" ]]; then
 		if rlIsRHEL 9; then
 			tok "nvme telemetry-log ${NVME_CHAR} --output-file=telemetry_log.bin"
 		elif rlIsRHEL 8; then
