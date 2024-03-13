c243a03865fc98d48515634eed45d9b485b8dc8c
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2024-03-13 15:11:46 -0500

    Added code to install devel package in rutnest

--- memory/mmra/invoke_unexported_API/runtest.sh
+++ memory/mmra/invoke_unexported_API/runtest.sh
@@ -8,6 +8,16 @@ MODFILE="${MODULE}.ko"
 rlJournalStart
     rlPhaseStartSetup
         rlShowRunningKernel
+        devel_pkg=$(K_GetRunningKernelRpmSubPackageNVR devel)
+        pkg_mgr=$(K_GetPkgMgr)
+        rlLog "pkg_mgr = ${pkg_mgr}"
+        if [[ $pkg_mgr == "rpm-ostree" ]]; then
+            export pkg_mgr_inst_string="-A -y --idempotent --allow-inactive install"
+        else
+            export pkg_mgr_inst_string="-y install"
+        fi
+        # shellcheck disable=SC2086
+        ${pkg_mgr} ${pkg_mgr_inst_string} ${devel_pkg}
     rlPhaseEnd
     rlPhaseStartTest
     rlRun "make test 2>&1" 2
