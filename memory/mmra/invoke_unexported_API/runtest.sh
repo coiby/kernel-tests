897d89c6a93db8f2b64599e0eb87f53d54ac9fa9
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2024-03-13 13:59:41 -0500

    Modified Makefile and runtest.sh to move dmesg from runtest to Makfile. Wrapped make in rlRun

--- memory/mmra/invoke_unexported_API/runtest.sh
+++ memory/mmra/invoke_unexported_API/runtest.sh
@@ -9,26 +9,11 @@ rlJournalStart
     rlPhaseStartSetup
         rlShowRunningKernel
     rlPhaseEnd
-    # rlPhaseStart
-    #     # rlRun "make" 2 "-p 'File doesn't link as expected'"
-    #     rlRun "make"
-    # rlPhaseEnd
-
-    # rlPhaseStart
-    #     rlRun "make" 0-255
-    #     if [ -f $MODFILE ]; then
-    #         rlRun "insmod $MODFILE" 1 "Loading the module should fail."
-    #         rlRun "dmesg | grep -i 'unresolved symbol'" 0 "Checking dmesg for unresolved symbol error"
-    #     else
-    #         rlLogInfo "Module file does not exist. Linking step failed as expected."
-    #     fi
-    # rlPhaseEnd
-    rlPhaseStart "Compilation"
-    if ! make; then
-        rlPass "Compilation failed as expected due to unresolved symbols"
-    fi
-    rlRun "insmod $MODFILE" 1 "Loading the module should fail."
-    rlRun "dmesg | grep -i 'unresolved symbol'" 0 "Checking dmesg for unresolved symbol error"
+    rlPhaseStartTest
+    rlRun "make test 2>&1" 
+    rlRun "dmesg > dmesg-test.log"
+    rlAssertGrep "Unexported symbol" dmesg-test.log
+    rlFileSubmit dmesg-test.log
     rlPhaseEnd
 
 rlPhaseStartCleanup
