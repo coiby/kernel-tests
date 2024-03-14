eef80a071b13a313387a639b88162d218ab7577c
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2024-03-14 12:35:16 -0500

    Removed logic for test.log

--- memory/mmra/invoke_unexported_API/runtest.sh
+++ memory/mmra/invoke_unexported_API/runtest.sh
@@ -20,11 +20,12 @@ rlJournalStart
         ${pkg_mgr} ${pkg_mgr_inst_string} ${devel_pkg}
     rlPhaseEnd
     rlPhaseStartTest
-    rlRun "make test 2>test.log" 2
-    rlAssertGrep "ERROR: modpost: "unexported_kernel_symbol"" test.log 
+    rlRun "make test 2>&1" 2
+    # rlAssertGrep "ERROR: modpost: "unexported_kernel_symbol"" test.log 
     rlRun "dmesg > dmesg-test.log"
     rlAssertGrep "Unexported symbol" dmesg-test.log
     rlFileSubmit dmesg-test.log
+    # rlFileSubmit test.log
     rlPhaseEnd
 
 rlPhaseStartCleanup
