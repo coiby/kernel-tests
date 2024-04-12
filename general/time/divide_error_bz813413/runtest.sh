d45e91e25f32f27eb85e6938700b79cb2f55d5d2
Stephen Bertram <sbertram@redhat.com>
2024-04-12 15:31:14 -0400

    divide_error_bz813413: changes to enable working with automotive env.

--- general/time/divide_error_bz813413/runtest.sh
+++ general/time/divide_error_bz813413/runtest.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-. ../../../cki_lib/libcki.sh           || exit 1
+. ../../../cki_lib/libcki.sh         || exit 1
 
 TEST="general/time/divide_error_bz813413"
 
 function runtest ()
 {
     # guestfish command be included in libguestfs-tools-c package.
-    yum -y install libguestfs-tools-c
-    which guestfish
-    if [ $? -ne 0 ]; then
+    if ! which guestfish; then
         echo "no guestfish command, installation failed" | tee -a $OUTPUTFILE
         rstrnt-report-result $TEST "SKIP"
         exit 0
@@ -17,8 +15,7 @@ function runtest ()
 
     for i in `seq 1 100`; do
         guestfish -a /dev/null run -v > /tmp/fish.out 2>&1 ;
-        grep "divide error" /tmp/fish.out
-        if [ $? -eq 0 ]; then
+        if grep -q "divide error" /tmp/fish.out ; then
             echo "divide error in /tmp/fish.out, happens for ${i}th time" | tee -a $OUTPUTFILE
             rstrnt-report-log -l /tmp/fish.out
             rstrnt-report-result $TEST "FAIL" 1
