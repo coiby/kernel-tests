e0e6a513eee69058aed3cc49734329c1acbfa3fc
Jerry Snitselaar <jsnitsel@redhat.com>
2024-04-22 23:47:37 -0700

    hw-enablement/dmaengine: Add tests for the idxd, ptdma, and ioatdma drivers
    This adds two tests, idxd and dmatest, targeted at
    the idxd, ptdma, and ioatdma drivers.
    
    dmatest needs the following params:
    
    DMA_MODNAME:
    	"idxd"
    	"ioatdma"
    	"ptdma"
    DMA_IOMMU_CONF
    	"sm-on-lazy" (idxd only)
    	"sm-on-strict" (idxd only)
    	"sm-on-pt" (idxd only)
    	"lazy"
    	"strict"
    	"pt"
    
    It tests the dma channel based drivers ioatdma and ptdma,
    along with the dmaengine workqueue driver of idxd with
    the dmatest module from the dmaengine subsystem.
    
    idxd needs the following params:
    
    DMA_IOMMU_CONF:
    	"sm-on-lazy"
    	"sm-on-strict"
    	"sm-on-pt"
    IDXD_ACCFG_TEST:
    	"dsa_config_test_runner"
    	"dsa_user_test_runner"
    	"iaa_user_test_runner"
    
    The idxd user workqueues, and iax devices on systems
    that have the capability using tests from the accel-config-test
    package.
    
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

---
+++ hw-enablement/dmaengine/dmatest/runtest.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Copyright (c) 2024 Red Hat, Inc
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Summary: Test idxd dmaengine workqueue with dmatest module
+#
+# Author: Jerry Snitselaar <jsnitsel@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+FILE=$(readlink -f ${BASH_SOURCE[0]})
+CDIR=$(dirname $FILE)
+
+if test -z "${DMA_MODNAME}"; then
+	test_complete "SKIP" "${RSTRNT_TASKNAME} The parameter DMA_MODNAME must be set"
+fi
+
+if test -z "${DMA_IOMMU_CONF}"; then
+	test_complete "SKIP" "${RSTRNT_TASKNAME} The parameter DMA_IOMMU_CONF must be set"
+fi
+
+DMA_TESTNAME="${DMA_MODNAME}-${DMA_IOMMU_CONF}-$(basename ${CDIR})"
+
+. "/usr/share/beakerlib/beakerlib.sh" || exit 1
+. "${CDIR}/../../../iommu/include/iommu-helper.sh" || exit 1
+. "${CDIR}/../include/dmaengine-helper.sh" || exit 1
+. "${CDIR}/../include/dsa-helper.sh" || exit 1
+
+if test -z "${DMATEST_MULTITHREADS}"; then
+	if test "${DMA_MODNAME}" = "idxd" -o "${DMA_MODNAME}" = "ioatdma"; then
+		DMATEST_MULTITHREADS="10"
+	fi
+elif test $((DMATEST_MULTITHREADS)) -lt 2; then
+	DMATEST_MULTITHREADS="2"
+fi
+
+if ! test -z "${DMAENG_DEBUG}"; then
+	set -x
+fi
+
+rlJournalStart
+{
+	if ! test -d "${DMA_STATEDIR}"; then
+		if is_vendor "intel"; then
+			intel_iommu_supported
+		fi
+		init_state
+		init_test_pkgs
+		set_config "${DMA_IOMMU_CONF}"
+		set_phase "config"
+	fi
+
+	if check_phase "config"; then
+	   grub_setup "$(get_config)"
+	fi
+
+	case "${DMA_MODNAME}" in
+		"idxd")
+			idxd_check
+			;;
+		"ptdma" | "ioatdma")
+			;;
+		*)
+			test_complete "SKIP" "${DMA_TESTNAME}: DMA_MODNAME ${DMA_MODNAME} is not recognized"
+			;;
+	esac
+
+	# IOMMU enabled check
+	if is_vendor "intel" && ! intel_iommu_enabled; then
+		test_complete "FAIL" "${DMA_TESTNAME} IOMMU didn't initialize"
+	elif is_vendor "amd" && ! amd_iommu_enabled; then
+		test_complete "FAIL" "${DMA_TESTNAME} IOMMU didn't initialize"
+	fi
+
+	# IOMMU state check
+	iommu_state_check
+
+	if check_phase "reboot"; then
+	   set_phase "run"
+	fi
+
+	rlPhaseStart FAIL "${DMA_TESTNAME} pre-test reboot and dma faults check"
+	{
+		if ! check_rebootcount "1"; then
+			fail_abort "${DMA_TESTNAME} Unexpected reboot"
+		fi
+		check_dma_faults "${DMA_TESTNAME} pre-test dma faults check"
+	}
+	rlPhaseEnd
+
+	setup_sys
+
+	# Test using dma channels and dmatest module
+	case "${DMA_MODNAME}" in
+		"idxd")
+			rlPhaseStartTest "${DMA_TESTNAME} memcpy-1-thread"
+			{
+				dmaengine_enable "dsa0" "wq0.0" "engine0.0"
+				dmaengine_test "dsa0" "wq0.0" 3000 1000 0 1 "${DMA_TESTNAME} $(get_config) dmatest memcpy-1-thread"
+				dmaengine_disable "dsa0" "wq0.0"
+			}
+			rlPhaseEnd
+
+			if ! test -z "${DMATEST_MULTITHREADS}"; then
+				cleanup_sys
+				setup_sys
+
+				rlPhaseStartTest "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-thread"
+				{
+					dmaengine_enable "dsa0" "wq0.0" "engine0.0"
+					dmaengine_test "dsa0" "wq0.0" 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-threads"
+					dmaengine_disable "dsa0" "wq0.0"
+				}
+				rlPhaseEnd
+			fi
+			;;
+		"ioatdma" | "ptdma")
+			driverCheck "${DMA_MODNAME}"
+			rlPhaseStartTest "${DMA_TESTNAME} memcpy testing"
+			{
+				dmatest_run 3000 1000 0 1 "${DMA_TESTNAME} memcpy-1-thread"
+				if ! test -z "${DMATEST_MULTITHREADS}"; then
+					echo "DMA_MULTITHREADS = ${DMATEST_MULTITHREADS}"
+					dmatest_run 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-threads"
+				fi
+			}
+			rlPhaseEnd
+			;;
+		*)
+			test_complete "SKIP" "${DMA_TESTNAME}: DMA_MODNAME ${DMA_MODNAME} is not recognized"
+			;;
+	esac
+
+	cleanup_sys
+	grub_cleanup
+
+	rlPhaseStartCleanup "${DMA_TESTNAME} cleanup"
+	{
+		# did we reboot during the test?
+		if ! check_rebootcount "1"; then
+			fail_abort "${DMA_TESTNAME} Unexpected reboot"
+		fi
+
+		grub_exit
+		cleanup_state
+	}
+	rlPhaseEnd
+
+	rstrnt-report-result "${DMA_TESTNAME} dmatest test complete" "PASS"
+}
+
+rlJournalEnd
