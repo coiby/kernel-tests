2886e14a844683e943e50345e63c3899b49cf105
Jerry Snitselaar <jsnitsel@redhat.com>
2024-05-03 02:46:37 -0700

    iommu: improve enabled iommu detection
    Look at the IORT table on arm systems to verify there is
    an SMMU node type, and use the /sys/class/iommu entries
    to determine if an iommu has been initialized, instead of
    looking at the kernel's console output.
    
    It also re-works result reporting, fixing a problem
    with how it was previously calling rstrnt-report-result.
    
    It addition it removes the beakerlib rlPhase* bits since
    they aren't really needed.
    
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- hw-enablement/dmaengine/dmatest/runtest.sh
+++ hw-enablement/dmaengine/dmatest/runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# -*- mode: Shell-script; sh-shell: bash; sh-basic-offset: 4; sh-indentation: 4; coding: utf-8-unix; indent-tabs-mode: t; ruler-mode-show-tab-stops: t; tab-width: 4 -*-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Copyright (c) 2024 Red Hat, Inc
@@ -16,11 +16,11 @@ FILE=$(readlink -f ${BASH_SOURCE[0]})
 CDIR=$(dirname $FILE)
 
 if test -z "${DMA_MODNAME}"; then
-	test_complete "SKIP" "${RSTRNT_TASKNAME} The parameter DMA_MODNAME must be set"
+	test_complete "${RSTRNT_TASKNAME}" "SKIP" "The parameter DMA_MODNAME must be set"
 fi
 
 if test -z "${DMA_IOMMU_CONF}"; then
-	test_complete "SKIP" "${RSTRNT_TASKNAME} The parameter DMA_IOMMU_CONF must be set"
+	test_complete "${RSTRNT_TASKNAME}" "SKIP" "The parameter DMA_IOMMU_CONF must be set"
 fi
 
 DMA_TESTNAME="${DMA_MODNAME}-${DMA_IOMMU_CONF}-$(basename ${CDIR})"
@@ -42,115 +42,89 @@ if ! test -z "${DMAENG_DEBUG}"; then
 	set -x
 fi
 
-rlJournalStart
-{
-	if ! test -d "${DMA_STATEDIR}"; then
-		if is_vendor "intel"; then
-			intel_iommu_supported
+if ! test -d "${DMA_STATEDIR}"; then
+	if is_vendor "intel"; then
+		intel_iommu_supported
+	fi
+	init_state
+	init_test_pkgs
+	set_config "${DMA_IOMMU_CONF}"
+	set_phase "config"
+fi
+
+if check_phase "config"; then
+	grub_setup "$(get_config)"
+fi
+
+case "${DMA_MODNAME}" in
+	"idxd")
+		idxd_check
+		;;
+	"ptdma" | "ioatdma")
+		;;
+	*)
+		test_complete "${DMA_TESTNAME}" "SKIP" "DMA_MODNAME ${DMA_MODNAME} is not recognized"
+		;;
+esac
+
+# IOMMU enabled check
+if is_vendor "intel" && ! intel_iommu_enabled; then
+	test_complete "${DMA_TESTNAME}" "FAIL" "IOMMU didn't initialize"
+elif is_vendor "amd" && ! amd_iommu_enabled; then
+	test_complete "${DMA_TESTNAME}" "FAIL" "IOMMU didn't initialize"
+fi
+
+# IOMMU state check
+iommu_state_check
+
+if check_phase "reboot"; then
+	set_phase "run"
+fi
+
+if ! check_rebootcount "1"; then
+	fail_abort "${DMA_TESTNAME}-pre-test-reboot-check" "Unexpected reboot"
+fi
+check_dma_faults "${DMA_TESTNAME}-pre-test"
+
+setup_sys
+
+# Test using dma channels and dmatest module
+case "${DMA_MODNAME}" in
+	"idxd")
+		dmaengine_enable "dsa0" "wq0.0" "engine0.0"
+		dmaengine_test "dsa0" "wq0.0" 3000 1000 0 1 "${DMA_TESTNAME}-memcpy-1-thread"
+		dmaengine_disable "dsa0" "wq0.0"
+
+		if ! test -z "${DMATEST_MULTITHREADS}"; then
+			cleanup_sys
+			setup_sys
+
+			dmaengine_enable "dsa0" "wq0.0" "engine0.0"
+			dmaengine_test "dsa0" "wq0.0" 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME}-memcpy-${DMATEST_MULTITHREADS}-threads"
+			dmaengine_disable "dsa0" "wq0.0"
 		fi
-		init_state
-		init_test_pkgs
-		set_config "${DMA_IOMMU_CONF}"
-		set_phase "config"
-	fi
-
-	if check_phase "config"; then
-	   grub_setup "$(get_config)"
-	fi
-
-	case "${DMA_MODNAME}" in
-		"idxd")
-			idxd_check
-			;;
-		"ptdma" | "ioatdma")
-			;;
-		*)
-			test_complete "SKIP" "${DMA_TESTNAME}: DMA_MODNAME ${DMA_MODNAME} is not recognized"
-			;;
-	esac
-
-	# IOMMU enabled check
-	if is_vendor "intel" && ! intel_iommu_enabled; then
-		test_complete "FAIL" "${DMA_TESTNAME} IOMMU didn't initialize"
-	elif is_vendor "amd" && ! amd_iommu_enabled; then
-		test_complete "FAIL" "${DMA_TESTNAME} IOMMU didn't initialize"
-	fi
-
-	# IOMMU state check
-	iommu_state_check
-
-	if check_phase "reboot"; then
-	   set_phase "run"
-	fi
-
-	rlPhaseStart FAIL "${DMA_TESTNAME} pre-test reboot and dma faults check"
-	{
-		if ! check_rebootcount "1"; then
-			fail_abort "${DMA_TESTNAME} Unexpected reboot"
+		;;
+	"ioatdma" | "ptdma")
+		driverCheck "${DMA_MODNAME}"
+		dmatest_run 3000 1000 0 1 "${DMA_TESTNAME}-memcpy-1-thread"
+		if ! test -z "${DMATEST_MULTITHREADS}"; then
+			dmatest_run 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME}-memcpy-${DMATEST_MULTITHREADS}-threads"
 		fi
-		check_dma_faults "${DMA_TESTNAME} pre-test dma faults check"
-	}
-	rlPhaseEnd
+		;;
+	*)
+		test_complete "${DMA_TESTNAME}" "SKIP" "DMA_MODNAME ${DMA_MODNAME} is not recognized"
+		;;
+esac
 
-	setup_sys
+cleanup_sys
+grub_cleanup
 
-	# Test using dma channels and dmatest module
-	case "${DMA_MODNAME}" in
-		"idxd")
-			rlPhaseStartTest "${DMA_TESTNAME} memcpy-1-thread"
-			{
-				dmaengine_enable "dsa0" "wq0.0" "engine0.0"
-				dmaengine_test "dsa0" "wq0.0" 3000 1000 0 1 "${DMA_TESTNAME} $(get_config) dmatest memcpy-1-thread"
-				dmaengine_disable "dsa0" "wq0.0"
-			}
-			rlPhaseEnd
+# did we reboot during the test?
+if ! check_rebootcount "1"; then
+	fail_abort "${DMA_TESTNAME}-post-test-reboot-check" "Unexpected reboot"
+fi
 
-			if ! test -z "${DMATEST_MULTITHREADS}"; then
-				cleanup_sys
-				setup_sys
+grub_exit
+cleanup_state
 
-				rlPhaseStartTest "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-thread"
-				{
-					dmaengine_enable "dsa0" "wq0.0" "engine0.0"
-					dmaengine_test "dsa0" "wq0.0" 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-threads"
-					dmaengine_disable "dsa0" "wq0.0"
-				}
-				rlPhaseEnd
-			fi
-			;;
-		"ioatdma" | "ptdma")
-			driverCheck "${DMA_MODNAME}"
-			rlPhaseStartTest "${DMA_TESTNAME} memcpy testing"
-			{
-				dmatest_run 3000 1000 0 1 "${DMA_TESTNAME} memcpy-1-thread"
-				if ! test -z "${DMATEST_MULTITHREADS}"; then
-					echo "DMA_MULTITHREADS = ${DMATEST_MULTITHREADS}"
-					dmatest_run 3000 1000 0 "${DMATEST_MULTITHREADS}" "${DMA_TESTNAME} memcpy-${DMATEST_MULTITHREADS}-threads"
-				fi
-			}
-			rlPhaseEnd
-			;;
-		*)
-			test_complete "SKIP" "${DMA_TESTNAME}: DMA_MODNAME ${DMA_MODNAME} is not recognized"
-			;;
-	esac
-
-	cleanup_sys
-	grub_cleanup
-
-	rlPhaseStartCleanup "${DMA_TESTNAME} cleanup"
-	{
-		# did we reboot during the test?
-		if ! check_rebootcount "1"; then
-			fail_abort "${DMA_TESTNAME} Unexpected reboot"
-		fi
-
-		grub_exit
-		cleanup_state
-	}
-	rlPhaseEnd
-
-	rstrnt-report-result "${DMA_TESTNAME} dmatest test complete" "PASS"
-}
-
-rlJournalEnd
+rstrnt-report-result "${DMA_TESTNAME}" "PASS"
--- iommu/new-boot/runtest.sh
+++ iommu/new-boot/runtest.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#
+# -*- mode: Shell-script; sh-shell: bash; sh-basic-offset: 4; sh-indentation: 4; coding: utf-8-unix; indent-tabs-mode: t; ruler-mode-show-tab-stops: t; tab-width: 4 -*-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Copyright (c) 2024 Red Hat, Inc
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# Summary: Test intel_iommu boot with Scalable Mode and batched
-#		   iotlb invalidation.
+# Summary: Test iommu boot with various configurations
 #
 # Author: Jerry Snitselaar <jsnitsel@redhat.com>
 #
@@ -17,11 +16,11 @@ FILE=$(readlink -f ${BASH_SOURCE[0]})
 CDIR=$(dirname $FILE)
 
 if test -z "${DMA_MODNAME}"; then
-	test_complete "SKIP" "${RSTRNT_TASKNAME} requires DMA_MODNAME to be set"
+	test_complete "${RSTRNT_TASKNAME}" "SKIP" "requires DMA_MODNAME to be set"
 fi
 
 if test -z "${DMA_IOMMU_CONF}"; then
-	test_complete "SKIP" "${RSTRNT_TASKNAME} requires DMA_IOMMU_CONF to be set"
+	test_complete "${RSTRNT_TASKNAME}" "SKIP" "requires DMA_IOMMU_CONF to be set"
 fi
 
 DMA_TESTNAME="${DMA_MODNAME}-new-boot-${DMA_IOMMU_CONF}"
@@ -33,45 +32,33 @@ if ! test -z "${IOMMU_DEBUG}"; then
 	set -x
 fi
 
-rlJournalStart
-{
-	if ! test -d "${DMA_STATEDIR}"; then
-		iommu_supported_check
-		init_state
-		set_config "${DMA_IOMMU_CONF}"
-		set_phase "config"
-	fi
+if ! test -d "${DMA_STATEDIR}"; then
+	iommu_supported_check
+	init_state
+	set_config "${DMA_IOMMU_CONF}"
+	set_phase "config"
+fi
 
-	if check_phase "config"; then
-	   grub_setup "$(get_config)"
-	fi
+if check_phase "config"; then
+	grub_setup "$(get_config)"
+fi
 
-	iommu_enabled_check
+iommu_enabled_check
 
-	iommu_state_check
+iommu_state_check
 
-	if check_phase "reboot"; then
-	   set_phase "run"
-	fi
+if check_phase "reboot"; then
+	set_phase "run"
+fi
 
-	rlPhaseStart FAIL "${DMA_TESTNAME} reboot and dma faults check"
-	{
-		if ! check_rebootcount "1"; then
-			fail_abort "${DMA_TESTNAME} Unexpected reboot"
-		fi
-		check_dma_faults "${DMA_TESTNAME} dma faults check"
-	}
-	rlPhaseEnd
+if ! check_rebootcount "1"; then
+	fail_abort "${DMA_TESTNAME}-reboot-check" "Unexpected reboot"
+fi
+check_dma_faults "${DMA_TESTNAME}-post-boot"
 
-	grub_cleanup
+grub_cleanup
 
-	rlPhaseStartCleanup "${DMA_TESTNAME} cleanup"
-	{
-		grub_exit
-		cleanup_state
-	}
-	rlPhaseEnd
+grub_exit
+cleanup_state
 
-	rstrnt-report-result "${DMA_TESTNAME} test complete" "PASS"
-}
-rlJournalEnd
+rstrnt-report-result "${DMA_TESTNAME}" "PASS"
