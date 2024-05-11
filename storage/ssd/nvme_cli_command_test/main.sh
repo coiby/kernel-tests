8689a2022361cee823af16f0ad0f2d2f4fdfcbcb
Yi Zhang <yi.zhang@redhat.com>
2024-05-11 01:32:20 -0400

    storage/ssd: update nvme_cli_command_test for rhel10
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/ssd/nvme_cli_command_test/main.sh
+++ storage/ssd/nvme_cli_command_test/main.sh
@@ -57,7 +57,7 @@ for DISK in $DISKS; do
 	if [[ $MODEL =~ "SAMSUNG MZ1L21T9HCLS-00A07"|"SAMSUNG MZQL21T9HCJR-00A07"|"SAMSUNG MZQL2960HCJR-00A07"|"Dell Ent NVMe v2 AGN RI U.2"|"Dell Ent NVMe CM6 RI"|"Dell Ent NVMe P5500 RI U.2"|"Dell Express Flash NVMe PM1725 1.6TB"|"SAMSUNG MZPLJ1T6HBJR-00007"|"Dell Express Flash NVMe P4800X"|"Dell Ent NVMe v2 AGN FIPS MU U.2"|"Samsung SSD 983 DCT" ]]; then
 		tok "nvme telemetry-log ${NVME_CHAR} --output-file=telemetry_log.bin"
 	elif [[ $MODEL =~ "Micron_9300_MTFDHAL3T8TDP"|"Dell Express Flash PM1725a 1.6TB AIC"|"INTEL SSDPEDMD016T4"|"Dell Express Flash NVMe P4600 2.0TB AIC"|"Dell Express Flash PM1725b"|"Dell Express Flash NVMe P4500" ]]; then
-		if rlIsRHEL 9; then
+		if rlIsRHEL 9 || rlIsRHEL 10; then
 			tok "nvme telemetry-log ${NVME_CHAR} --output-file=telemetry_log.bin"
 		elif rlIsRHEL 8; then
 			tnot "nvme telemetry-log ${NVME_CHAR} --output-file=telemetry_log.bin"
