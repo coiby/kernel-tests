5af90be9d6db58eaaa900a336f9273671ff9ce21
Yi Zhang <yi.zhang@redhat.com>
2024-05-13 10:54:46 -0400

    storage/ssd: update get_nvme_pci_id for rhel-10
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/ssd/include/include.sh
+++ storage/ssd/include/include.sh
@@ -81,7 +81,7 @@ function get_nvme_pci_id() {
 	NVME_DISK=$1
 	NVME_CHAR=${NVME_DISK:0:5}
 	TEST_DEV_SYSFS=/sys/block/$NVME_DISK/device
-	uname -r | grep -qE "5.[0-9]" && TEST_DEV_SYSFS="$TEST_DEV_SYSFS/$NVME_CHAR"
+	uname -r | grep -qE "el9|el10" && TEST_DEV_SYSFS="$TEST_DEV_SYSFS/$NVME_CHAR"
 	readlink -f "$TEST_DEV_SYSFS" | \
 		grep -Eo '[0-9a-f]{4,5}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]' | \
 		tail -1
