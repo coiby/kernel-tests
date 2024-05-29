41e3c56e5ceb8565bcc8a55044fb0a2d7a2b2550
Ping Fang <pifang@redhat.com>
2024-05-30 00:02:38 +0800

    ltp: fix ltp issue on autosd
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include/ltp-make.sh
+++ distribution/ltp/include/ltp-make.sh
@@ -23,7 +23,9 @@ if [ -z ${TESTVERSION} ]; then
         TESTVERSION="20240129"
     fi
 fi
-
+if rlIsOS 'autosd'; then
+    export CREATE_ENTRIES=1
+fi
 # the task path may be different under the restraint harness if the task
 # is fetched directly from git, so use a relative path to the include task
 ABS_DIR=$(dirname ${BASH_SOURCE[0]})"/patches"
