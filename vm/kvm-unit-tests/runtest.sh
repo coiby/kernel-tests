22254e2f45e7c98a06edc54ac523cee277687c82
Jan Richter <jarichte@redhat.com>
2024-06-06 13:15:47 +0200

    Increase timeout for vmx kvm-unit test
    A few kvm-unit tests are functioning properly, but encountering a
    timeout issue on debug kernel. To address this, let's extend the
    timeout duration for those test to allow it enough time to complete.
    
    Signed-off-by: Jan Richter <jarichte@redhat.com>

--- vm/kvm-unit-tests/runtest.sh
+++ vm/kvm-unit-tests/runtest.sh
@@ -185,10 +185,11 @@ function configureTests
     hwpf=$(uname -m)
 
     #Increasing timeout for some test to give them more time in CKI environment
-    sed -i '/^\[vmx_vmcs_shadow_test\]$/,/^\[/ s/^timeout = 180/timeout = 300/' x86/unittests.cfg
+    sed -i '/^\[vmx_vmcs_shadow_test\]$/,/^\[/ s/^timeout = 180/timeout = 400/' x86/unittests.cfg
     sed -i '/^\[vmx_pf_vpid_test\]$/,/^\[/ s/^timeout = 240/timeout = 1000/' x86/unittests.cfg
     sed -i '/^\[vmx_pf_invvpid_test\]$/,/^\[/ s/^timeout = 240/timeout = 1000/' x86/unittests.cfg
     sed -i '/^\[vmx_pf_no_vpid_test\]$/,/^\[/ s/^timeout = 240/timeout = 1000/' x86/unittests.cfg
+    sed -i '/^\[vmx_apicv_test\]$/,/^\[/ s/^timeout = 10/timeout = 20/' x86/unittests.cfg
     sed -i '/^\[access_fep\]$/,/^\[/ s/^timeout = 240/timeout = 500/' x86/unittests.cfg
 }
 
