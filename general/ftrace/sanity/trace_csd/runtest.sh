1a168930a5af135c2a866401d0e90ebc95341632
Qiao Zhao <qzhao@redhat.com>
2024-06-20 16:23:32 +0800

    [New Case] enabled csd tracepoint check
    Issue: https://issues.redhat.com/browse/RHEL-13876
    RHEL-9.5
    
    Signed-off-by: Qiao Zhao <qzhao@redhat.com>

---
+++ general/ftrace/sanity/trace_csd/runtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. /usr/share/beakerlib/beakerlib.sh
+
+rlJournalStart
+    if [ -f /sys/kernel/tracing/events/csd/enable ]; then
+    rlPhaseStartTest "csd tracepoint test"
+        rlRun "ls /sys/kernel/debug/tracing/events/csd/"
+        rlRun "trace-cmd record -e csd_function_entry -e csd_function_exit -e csd_queue_cpu sleep 10"
+        rlRun "trace-cmd report  -i trace.dat | tee csd.log"
+        rlAssertGreater "Should have a few instance of csd_* trace" $(grep 'csd_function' csd.log | wc -l) 1
+        rlFileSubmit csd.log
+    rlPhaseEnd
+    else
+    rlPhaseStartTest "No CSD tracepoint on current system"
+        rlRun "SKIP: CSD tracepoints backport since RHEL9.5"
+    rlPhaseEnd
+    fi
+rlJournalEnd
