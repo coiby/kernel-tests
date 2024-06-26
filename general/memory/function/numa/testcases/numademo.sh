5b7009c97f0cb356390f176ccc125e4dbbc7e2a6
Ping Fang <pifang@redhat.com>
2024-06-26 11:23:54 +0800

    memory/numa: Skip numademo on single node system
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- general/memory/function/numa/testcases/numademo.sh
+++ general/memory/function/numa/testcases/numademo.sh
@@ -4,5 +4,8 @@ numastat || exit 1
 numastat -c || exit 1
 numastat -m || exit 1
 numastat -n || exit 1
+if [ $(numactl -H | grep available | awk '{print $2}') -lt 2 ]; then
+	exit 0
+fi
 numademo -t 32M memset memcpy forward backward stream random2 ptrchase || exit 1
 exit 0
