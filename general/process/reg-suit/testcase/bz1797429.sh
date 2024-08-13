e30a0b01fee6579482bd232045ccff39a08561c2
Shizhao Chen <shichen@redhat.com>
2024-08-13 16:07:14 +0800

    process/reg-suite: fix typo in bz1797429.sh
    Resolves: #1985

--- general/process/reg-suit/testcase/bz1797429.sh
+++ general/process/reg-suit/testcase/bz1797429.sh
@@ -4,7 +4,7 @@ function bz1797429()
 	which stress-ng &>/dev/null || { pushd ../../../include/scripts/; sh stress.sh; popd; }
 	which stress-ng &>/dev/null || { report_result "stress-ng-install" SKIP; return; }
 	rpm -q perf || yum -y install perf &>/dev/null
-	perf trace -e clone --duraton 100 stress-ng --fork 1 -t 6 &
+	perf trace -e clone --duration 100 stress-ng --fork 1 -t 6 &
 	# Fix me, this should be in serial
 	timeout 5 agetty /dev/ttyS0 115200
 	echo t > /proc/sysrq-trigger
