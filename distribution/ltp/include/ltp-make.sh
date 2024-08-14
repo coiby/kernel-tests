e4d5c68aa0bdb16f65fc6c5dfdf1c344a30e60da
Li Wang <liwang@redhat.com>
2024-08-14 14:49:08 +0800

    ltp/include: backport patch for creat07
    Signed-off-by: Li Wang <liwang@redhat.com>

--- distribution/ltp/include/ltp-make.sh
+++ distribution/ltp/include/ltp-make.sh
@@ -130,6 +130,7 @@ patch-generic()
         ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0006-Add-cachestat04-test.patch
         ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-cachestat-remove-.min_kver-from-cachestat-tests.patch
         ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-cachestat01-Reduce-required-space-on-64kb-page-size.patch
+        ${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-syscalls-execve04-creat07-skip-for-kernels-6.11-and-.patch
     fi
     if [ "$TESTVERSION" == "20240129" ]; then
         ${PATCH} < ${ABS_DIR}/INTERNAL/0001-fix-broken-failure-detection-with-dmesg.patch
