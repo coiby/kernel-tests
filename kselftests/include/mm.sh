37d6ea03e6168fcdf49f9e0cc2aae0ae2bceec52
Stephen Bertram <sbertram@redhat.com>
2024-08-14 22:03:16 -0400

    Adding a baseline and threshold for gup benchmark tests.

--- kselftests/include/mm.sh
+++ kselftests/include/mm.sh
@@ -56,14 +56,90 @@ do_mm_run()
             if cki_is_kernel_automotive ; then
                 sed -i "s/mm:run_vmtests.sh/mm:run_mm_tests.sh/" kselftest-list.txt
                 cat >> mm/run_mm_tests.sh << EOF
-echo "=== Running gup_test -u ==="
-./gup_test -u
+#!/bin/bash
+
+exitcode=0
+factor=20 #20%
+# Baseline values
+pin_get_baseline=\${pin_get_baseline}
+pin_put_baseline=\${pin_put_baseline}
+gup_get_baseline=\${gup_get_baseline}
+gup_put_baseline=\${gup_put_baseline}
+
+# Setting the thresholds
+pin_get_threshold=\$((\$((pin_get_baseline * factor / 100)) + pin_get_baseline))
+pin_put_threshold=\$((\$((pin_put_baseline * factor / 100)) + pin_put_baseline))
+gup_get_threshold=\$((\$((gup_get_baseline * factor / 100)) + gup_get_baseline))
+gup_put_threshold=\$((\$((gup_put_baseline * factor / 100)) + gup_put_baseline))
+
 echo "=== Running gup_test -a ==="
-./gup_test -a
+gup_test_pin_fast_benchmark(){
+    # Collect pin benchmark
+    pin_fast_benchmark=\$(./gup_test -a)
+    if [[ "\$pin_fast_benchmark" == "check if CONFIG_GUP_TEST is enabled in kernel config" ]]; then
+        echo "\$pin_fast_benchmark"
+    else
+        # Capture and compare the get and put times from pin fast benchmark
+        pin_get_microsecs=\$(echo \$pin_fast_benchmark | awk -F ":" '{print \$4}' | awk '{print \$1}')
+        pin_put_microsecs=\$(echo \$pin_fast_benchmark | awk -F ":" '{print \$5}' | awk '{print \$1}')
+        if [[ \$pin_get_microsecs -gt \$pin_get_threshold ]]; then
+            echo "FAIL: PIN_FAST_BENCHMARK: Time: get:\$pin_get_microsecs exceeds threshold:\$pin_get_threshold."
+            exitcode=1
+        else
+            echo "PASS: PIN_FAST_BENCHMARK: Time: get:\$pin_get_microsecs within threshold:\$pin_get_threshold."
+        fi
+        if [[ \$pin_put_microsecs -gt \$pin_put_threshold ]]; then
+            echo "FAIL: PIN_FAST_BENCHMARK: Time: put:\$pin_put_microsecs exceeds threshold:\$pin_put_threshold."
+            exitcode=1
+        else
+            echo "PASS: PIN_FAST_BENCHMARK: Time: put:\$pin_put_microsecs within threshold:\$pin_put_threshold."
+        fi
+    fi
+}
+
+if [[ -n \${pin_get_baseline} ]] && [[ -n \${pin_put_baseline} ]]; then
+    gup_test_pin_fast_benchmark
+else
+    echo "SKIP: pin fast benchmarks, no baselines set."
+fi
+
+echo "=== Running gup_test -u ==="
+gup_test_gup_fast_benchmark(){
+    # Collect gup benchmark
+    gup_fast_benchmark=\$(./gup_test -u)
+    if [[ "\$gup_fast_benchmark" == "check if CONFIG_GUP_TEST is enabled in kernel config" ]]; then
+        echo "\$gup_fast_benchmark"
+    else
+        # Capture and compare the get and put times from gup fast benchmark
+        gup_get_microsecs=\$(echo \$gup_fast_benchmark | awk -F ":" '{print \$4}' | awk '{print \$1}')
+        gup_put_microsecs=\$(echo \$gup_fast_benchmark | awk -F ":" '{print \$5}' | awk '{print \$1}')
+        if [[ \$gup_get_microsecs -gt \$gup_get_threshold ]]; then
+            echo "FAIL: GUP_FAST_BENCHMARK: Time: get:\$gup_get_microsecs exceeds threshold:\$gup_get_threshold."
+            exitcode=1
+        else
+            echo "PASS: GUP_FAST_BENCHMARK: Time: get:\$gup_get_microsecs within threshold:\$gup_get_threshold."
+        fi
+        if [[ \$gup_put_microsecs -gt \$gup_put_threshold ]]; then
+            echo "FAIL: GUP_FAST_BENCHMARK: Time: put:\$gup_put_microsecs exceeds threshold:\$gup_put_threshold."
+            exitcode=1
+        else
+            echo "PASS: GUP_FAST_BENCHMARK: Time: put:\$gup_put_microsecs within threshold:\$gup_put_threshold."
+        fi
+    fi
+}
+
+if [[ -n \${gup_get_baseline} ]] && [[ -n \${gup_put_baseline} ]]; then
+    gup_test_gup_fast_benchmark
+else
+    echo "SKIP: gup fast benchmarks, no baselines set."
+fi
+
 echo "=== Running gup_test -ct -F 0x1 0 19 0x1000 ==="
 ./gup_test -ct -F 0x1 0 19 0x1000
 echo "=== Running mrelease_test ==="
 ./mrelease_test
+
+exit \$exitcode
 EOF
                 chmod 755 mm/run_mm_tests.sh
                 test="mm:run_mm_tests.sh"
