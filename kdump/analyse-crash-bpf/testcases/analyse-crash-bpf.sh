0962b929d136d905d3918b22f420cb0f2263a0a1
Xiaoying Yan <yiyan@redhat.com>
2024-08-26 15:05:05 +0800

    kdump:update the crash bpf test command.
    Signed-off-by: Xiaoying Yan <yiyan@redhat.com>

--- kdump/analyse-crash-bpf/testcases/analyse-crash-bpf.sh
+++ kdump/analyse-crash-bpf/testcases/analyse-crash-bpf.sh
@@ -40,8 +40,8 @@ EOF
 
     prog_id=$(grep BPF_PROG -A 1 "$crash_output" | awk '{w=$1} END{print w}')
     map_id=$(grep BPF_MAP -A 1 "$crash_output" | awk '{w=$1} END{print w}')
-    if [[ ! $prog_id =~ ^[0-9]+$ ]] || [[ ! $map_id =~ ^[0-9]+$ ]]; then
-        Error "Failed to get program id or map id. Please read $(basename $crash_output) for details."
+    if [[ ! $prog_id =~ ^[0-9]+$ ]]; then
+        Error "Failed to get program id. Please read $(basename $crash_output) for details."
         return
     fi
 
@@ -50,15 +50,23 @@ EOF
 help bpf
 bpf
 bpf -p $prog_id
-bpf -m $map_id
 bpf -p $prog_id -j
 bpf -p $prog_id -t
 bpf -p $prog_id -T
-bpf -m $map_id -s
 bpf -p $prog_id -s
 bpf -P
 bpf -M
 bpf -PM -jTs
+EOF
+    # The USED_MAPS field will be displayed only when it has actual values
+    if [[ $map_id =~ ^[0-9]+$ ]]; then
+    cat <<EOF >> "$crash_cmd"
+bpf -m $map_id
+bpf -m $map_id -s
+EOF
+    fi
+
+    cat <<EOF >> "$crash_cmd"
 exit
 EOF
 
@@ -68,4 +76,3 @@ EOF
 #+---------------------------+
 # $1 is the test phase name
 MultihostStage "$(basename "${0%.*}")" "analyse"
-
