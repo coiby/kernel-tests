3b81ee642c5dfc3efe90de021122dc4db94e66d2
Charles Shi <shichen120@gmail.com>
2024-10-07 23:14:22 -0400

    Fix log analysing issues and enhance the outputs
    Fixed the following issues:
    1. Found no log files leads to 'readlink: missing operand' for readlink
    2. Found multiple log files lead to 'No such file or directory' for grep
    3. Unable to handle 'perf/' test results.
    
    More enhancement to make the output log informative.

--- rt-tests/rt_ltp/runtest.sh
+++ rt-tests/rt_ltp/runtest.sh
@@ -25,29 +25,83 @@ ltp_version=${ltp_version:-$TESTVERSION}
 result_r="PASS"
 
 function check_status() {
-    if [ $? -eq 0 ]; then
-        pushd "logs/" || exit 1
-        casename=$(echo "$1" | awk -F'func/' '{print $2}')
-        log_file=$(readlink -f $(find ./ -name "*$casename*"))
+    # $1: the ./run.sh command line
+    # $2: return code from ./run.sh
 
-        if grep "Result: FAIL" "$log_file"; then
-            result_r="FAIL"
-            echo ":: $* :: FAIL ::" | tee -a "$OUTPUTFILE"
-            if [[ -s "$log_file" ]]; then
-                rstrnt-report-result "${casename}" "FAIL" 1
-            else
-                rstrnt-report-result -o "$log_file" "${casename}" "FAIL" 1
-            fi
-        else
-            echo ":: $* :: PASS ::" | tee -a "$OUTPUTFILE"
-            rstrnt-report-result "${casename}" "PASS" 0
+    # Parse parameters
+    local casename=""
+    if [[ "$1" =~ "func/" ]]; then
+        casename=$(echo "$1" | awk -F 'func/' '{print $2}')
+    elif [[ "$1" =~ "perf/" ]]; then
+        casename=$(echo "$1" | awk -F 'perf/' '{print $2}')
+    fi
+
+    if [[ -n "$casename" ]]; then
+        echo "Got the casename '$casename' from command line '$1'."
+    else
+        echo "Failed to get casename from command line '$1'."
+        rstrnt-report-result "${casename}" "WARN" 2
+        return 2
+    fi
+
+    if [[ -z $2 ]]; then
+        echo "Failed to receive the return code from './run.sh'."
+        rstrnt-report-result "${casename}" "WARN" 2
+        return 2
+    fi
+
+    # Analyse the return code
+    if [[ $2 -ne 0 ]]; then
+        result_r="FAIL"
+        echo ":: $1 :: FAIL ::" | tee -a "$OUTPUTFILE"
+        rstrnt-report-result "${casename}" "FAIL" 1
+        return 1
+    fi
+
+    # Analyse the log files
+    echo "Analysing test '$casename'..."
+    local casestat="PASS"
+
+    if [[ ! -d ./logs ]]; then
+        echo "Directory $PWD/logs does not exist."
+        rstrnt-report-result "${casename}" "WARN" 2
+        return 2
+    fi
+
+    # Collect all the related log files
+    local log_files=$(find ./logs/ -name "*$casename*" -exec readlink -f {} \;)
+
+    if [[ -z "$log_files" ]]; then
+        echo "No log files containing '$casename' were found in $PWD/logs."
+        rstrnt-report-result "${casename}" "WARN" 2
+        return 2
+    else
+        echo "Found the log file(s) containing '$casename':"
+        for log_file in $log_files; do echo ">> $log_file"; done
+    fi
+
+    for log_file in $log_files; do
+        # Examining the log file
+        echo "Examining log file '$log_file'..."
+
+        if grep -q "Result: FAIL" "$log_file"; then
+            echo "Found 'Result: FAIL' exists in the log file."
+            casestat="FAIL"
         fi
 
-        popd || exit 1 # "logs/"
+        # Upload the log file
+        rhts-submit-log -l "$log_file"
+    done
+
+    if [[ "$casestat" = "PASS" ]]; then
+        echo ":: $1 :: PASS ::" | tee -a "$OUTPUTFILE"
+        rstrnt-report-result "${casename}" "PASS" 0
+        return 0
     else
-        result_r="FAIL"
-        echo ":: $* :: FAIL ::" | tee -a "$OUTPUTFILE"
+        echo ":: $1 :: FAIL ::" | tee -a "$OUTPUTFILE"
         rstrnt-report-result "${casename}" "FAIL" 1
+        result_r="FAIL"
+        return 1
     fi
 }
 
@@ -70,11 +124,13 @@ function runtest() {
     ./configure
 
     # default test-arguments: func, stress, perf, list
-    func_list=$(./run.sh -t list | grep "${TEST_TYPE// /\\|}")
+    func_list=$(./run.sh -t list | grep "${TEST_TYPE// /\\|}" | sed 's/^\s*//')
     while IFS= read -r case; do
-        echo "running $case"
+        echo "=== Running $case ==="
         ./run.sh -t "$case"
-        check_status "./run.sh -t $case"
+        local return_code=$?
+        echo "=== Checking $case ==="
+        check_status "./run.sh -t $case" $return_code
     done <<<"$func_list"
 
     # shellcheck disable=SC2164
