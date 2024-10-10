d106c8fde6cb42f22e785cbe0dff038b632fe162
Andrew Halaney <ahalaney@redhat.com>
2024-10-10 08:51:30 -0500

    leap_hrtimer: Make thread callback CPU unaware
    Make it so the threads have no idea about what CPU they're on,
    and leave it to the main thread to handle that.
    
    This way each thread only concerns itself with checking if it slept
    properly, and let's the main thread figure out which thread (and
    therefore CPU) failed. With this approach the thread's return value
    can be checked directly instead of having a global array of failures
    (aka "p" before), and the thread doesn't need to be passed any argument.
    
    Signed-off-by: Andrew Halaney <ahalaney@redhat.com>

--- general/time/leap-second/leap_hrtimer/check_hrtimer_expires_ontime.c
+++ general/time/leap-second/leap_hrtimer/check_hrtimer_expires_ontime.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <time.h>
 #include <errno.h>
@@ -7,6 +8,7 @@
 #include <sys/timex.h>
 #include <sys/unistd.h>
 #include <sched.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,8 +17,6 @@
 #define NSEC_PER_SEC 1000000000ULL
 #define NS_PER_MS 1000000ULL
 #define COUNT 1000
-
-int *p;
 
 struct timespec timespec_add(struct timespec ts, unsigned long long ns)
 {
@@ -42,11 +42,9 @@ static inline int in_order(struct timespec a, struct timespec b)
 }
 
 
-void test_hrtimer_failure(void * arg)
+void *test_hrtimer_failure(void *unused)
 {
 	int i = 0;
-	int cpu_number = *((int *)arg);
-
         struct timespec now, target;
 
 	while(i < COUNT){
@@ -57,16 +55,13 @@ void test_hrtimer_failure(void * arg)
 		clock_gettime(CLOCK_REALTIME, &now);
 
 		if (!in_order(target, now)) {
-			printf("Note: hrtimer early expiration failure observed in cpu %d \n",cpu_number);
-
-			*(p + cpu_number) = 1;
-
-			return;
+			return (void *)(intptr_t)false;
 		}
 
 		++i;
 	}
 
+	return (void *)(intptr_t)true;
 }
 
 
@@ -74,18 +69,11 @@ int main(int argc, const char *argv[])
 {
         int i,ret;
 	int cpu_count = sysconf(_SC_NPROCESSORS_CONF);
-	int parameter[cpu_count];
 
 	pthread_t tid[cpu_count];
 	cpu_set_t mask[cpu_count];
 
 	printf("\t * Hrtimer Expire Testing start -> ");
-
-        p = (int *)malloc(sizeof(int) * cpu_count);
-
-	for (i = 0; i < cpu_count; i++) {
-		*(p + i) = 0;
-	}
 
         for (i = 0; i < cpu_count; i++)
 		CPU_ZERO(&mask[i]);
@@ -94,9 +82,8 @@ int main(int argc, const char *argv[])
 		CPU_SET(i, &mask[i]);
 
         for (i = 0; i < cpu_count; i++) {
-                parameter[i] = i;
 
-        	ret = pthread_create(&tid[i],NULL,(void *)test_hrtimer_failure,(void *)(parameter + i));
+		ret = pthread_create(&tid[i], NULL, (void *)test_hrtimer_failure, NULL);
 		if (ret != 0) {
 			printf("pthread create thread %d error \n",i);
 
@@ -106,18 +93,20 @@ int main(int argc, const char *argv[])
         }
 
 	for (i = 0; i < cpu_count; i++) {
-		pthread_join(tid[i],NULL);
-	}
-
-	for (i = 0; i < cpu_count; i++) {
-		if (*(p + i)) {
-			printf("Fail:\n\t failture occurs on cpu %d \n",i);
+		void *retval = NULL;
+		int ret = pthread_join(tid[i], &retval);
+		if (ret) {
+			printf("pthread_join[%d] failed: %s\n", i, strerror(ret));
 
 			return 2;
 		}
 
-	}
+		if (!(bool)(intptr_t)retval) {
+			printf("Fail:\n\t failure occurs on cpu %d\n", i);
 
+			return 2;
+		}
+	}
 
 	printf("PASS");
 
