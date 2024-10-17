5b1ed6f655986b0d59e51eedf76938a5a9809a68
Yi Zhang <yi.zhang@redhat.com>
2024-10-17 08:01:49 -0400

    storage/blktests: add ublk tests
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

---
+++ storage/blktests/ublk/README.md
@@ -0,0 +1,16 @@
+# storage/blktests/ublk suite
+
+Storage blktests ublk test
+
+## How to run it
+Please refer to the top-level README.md for common dependencies.
+
+### Install dependencies
+```bash
+root# bash ../../../cki_bin/pkgs_install.sh metadata
+```
+
+### Execute the test
+```bash
+bash ./runtest.sh
+```
---
+++ storage/blktests/ublk/runtest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TNAME="storage/blktests/ublk"
+
+FILE=$(readlink -f "${BASH_SOURCE[0]}")
+CDIR=$(dirname "$FILE")
+. "$CDIR"/../include/include.sh || exit 1
+CASE_TYPE=UBLK
+
+function main
+{
+	ret=0
+	test_ws="${CDIR}"/blktests
+	testcases_default="$(get_test_cases_list $CASE_TYPE)"
+	testcases=${_DEBUG_MODE_TESTCASES:-"$testcases_default"}
+	if [ -z "$testcases" ]; then
+		echo "Skip test because $CASE_TYPE case list is empty"
+		rstrnt-report-result "$TNAME" SKIP
+		return 0
+	fi
+	if rlIsRHEL 10 || rlIsCentOS 10 || rlIsFedora; then
+		for testcase in $testcases; do
+			do_test "$test_ws" "$testcase"
+			result=$(get_test_result "$test_ws" "$testcase")
+			report_test_result "$result" "$TNAME/tests/$testcase"
+			((ret += $?))
+		done
+	else
+		echo "Skip test because $(cat /etc/redhat-release) doesn't support ublk"
+		rstrnt-report-result "$TNAME" SKIP
+		return 0
+	fi
+
+	if [[ $ret -ne 0 ]]; then
+		echo ">> There are failing tests, pls check it"
+	fi
+}
+
+# don't run it if running as part of shellspec
+# https://github.com/shellspec/shellspec#__sourced__
+if [ ! "${__SOURCED__:+x}" ]; then
+	. "$CDIR"/../include/build.sh
+	main
+fi
