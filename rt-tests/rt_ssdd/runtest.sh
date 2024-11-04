cec35bd5babb2189c7874862b2f35a37d55225d3
Shizhao Chen <shichen@redhat.com>
2024-11-04 16:09:21 +0800

    rt_ssdd: fix the result report logic and simplify the test
    log_pass / log_fail don't report the results to restraint, all they do is adding
    a LOG line in the default output file (stdout). And the command enclosed in
    `run` is trailed with a `tee` operation, which always returns true.
    
    Beside of fixing the result problem, since ssdd has a problem of not able to
    exit itself, in order to not let it abort the recipe or killed by watchdog, this
    commit also adds a `timeout` for each ssdd command so that when ssdd can't exit
    we can intentionally kill it from the test.

--- rt-tests/rt_ssdd/runtest.sh
+++ rt-tests/rt_ssdd/runtest.sh
@@ -10,32 +10,11 @@ export rhel_x
 
 if ! kernel_automotive; then
     declare pkg_name="rt-tests" && (( rhel_x >= 9 )) && pkg_name="realtime-tests"
-    which ssdd || yum install -y $pkg_name
+    run "yum install -y $pkg_name"
 fi
 
-NFORKS=${NFORKS:=10}
-NITERS=${NITERS:=10000}
+log "Test the default options of ssdd"
+oneliner "timeout --preserve-status --verbose 3m ssdd"
 
-phase_start_test "Runing ssdd $NFORKS $NITERS [default]"
-run "ssdd --forks=$NFORKS --iters=$NITERS | tee SSDD1.LOG"
-rstrnt-report-log -l SSDD1.LOG
-if grep -q "All tests PASSED" SSDD1.LOG; then
-    log_pass "ssdd default PASS"
-else
-    log_fail "ssdd default FAIL"
-fi
-phase_end
-
-NFORKS=100
-NITERS=10000
-phase_start_test "Running ssdd $NFORKS $NITERS [stress]"
-run "ssdd --forks=$NFORKS --iters=$NITERS | tee SSDD2.LOG"
-rstrnt-report-log -l SSDD2.LOG
-if grep -q "All tests PASSED" SSDD2.LOG; then
-    log_pass "ssdd stress PASS"
-else
-    log_fail "ssdd stress FAIL"
-fi
-phase_end
-
-exit 0
+log "Stress test ssdd with 100 forks and 10000 iters"
+oneliner "timeout --preserve-status --verbose 3m ssdd --forks=100 --iters=10000"
