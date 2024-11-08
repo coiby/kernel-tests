b80b4b429b6dc2075bec27321ca2c186710034e3
Ping Fang <pifang@redhat.com>
2024-11-08 21:43:40 +0800

    ltp: skip knownissue on centos
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include-ng/knownissue/rhel9_knownissue.sh
+++ distribution/ltp/include-ng/knownissue/rhel9_knownissue.sh
@@ -45,13 +45,13 @@ function rhel9_unfix_issues()
 	# https://issues.redhat.com/browse/RHELMISC-5339 ltp/swapon03.c:56: TFAIL: swapon(filename, 0) failed: EPERM (1)
 	kernel_in_range "5.14.0-441.el9" "5.14.0-999.el9" && tskip "swapon03" unfix
 	# fanotify09 failures on rhel9
-	osver_in_range "906" "907" && tskip "fanotify09" unfix
+	osver_in_range "900" "907" && tskip "fanotify09" unfix
 	# setpgid01 failures on rhel9
-	osver_in_range "906" "907" && tskip "setpgid01" unfix
+	osver_in_range "900" "907" && tskip "setpgid01" unfix
 	# RHEL-65410: ltp/lite move_pages04 test failed
-	osver_in_range "906" "907" && tskip "move_pages04" unfix
+	osver_in_range "900" "907" && tskip "move_pages04" unfix
 	# pty01, ptem01 failed with kirk
-	osver_in_range "906" "907" && tskip "pty01 ptem01" unfix
+	osver_in_range "900" "907" && tskip "pty01 ptem01" unfix
 }
 
 function rhel9_fixed_issues()
