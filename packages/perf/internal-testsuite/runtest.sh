2b865f106f5afd9e4ed1973dda3513c186fc91fa
Veronika Molnarova <vmolnaro@redhat.com>
2024-11-28 18:21:37 +0100

    internal-testsuite/runtest.sh: Fix checking the result
    The result line was changed in perf and the current regex wasn't able to
    correctly parse the result meaning that the tests were always passing
    through the else branch. Switch the if and else branches so that if
    there would be any changes in the result printing in perf and the
    wrapper wasn't able to parse the result, the tests will rather all fail
    then pass.
    
    Signed-off-by: Veronika Molnarova <vmolnaro@redhat.com>

--- packages/perf/internal-testsuite/runtest.sh
+++ packages/perf/internal-testsuite/runtest.sh
@@ -196,14 +196,14 @@ rlJournalStart
 				perf test -F -vv $TEST_NUMBER &> $TEST_NUMBER.log
 				RETVAL=$?
 				cat $TEST_NUMBER.log
-				RESULT=`grep "^$TEST_DESC" < $TEST_NUMBER.log | grep : | awk -F':' '{print $NF}' | tr -d ' ' | grep -oP "^[\s\w]+" | tr -d '\n'`
+				RESULT=`grep "$TEST_DESC" < $TEST_NUMBER.log | grep : | awk -F':' '{print $NF}' | tr -d ' ' | grep -oP "^[\s\w]+" | tr -d '\n'`
 				printf "%8s -- %s\n" $RESULT "$line" | tee -a results.log
-				echo $RESULT | grep -qi FAIL
-				if [ $RETVAL -ne 0 -o $? -eq 0 ]; then
+				echo $RESULT | grep -qi -e "Ok" -e "Skip"
+				if [ $RETVAL -eq 0 -a $? -eq 0 ]; then
+					rlPass "$TEST_NUMBER: $TEST_DESC"
+				else
 					rlFail "$TEST_NUMBER: $TEST_DESC"
 					rlFileSubmit "$TEST_NUMBER.log"
-				else
-					rlPass "$TEST_NUMBER: $TEST_DESC"
 				fi
 
 				# restore original sample rate to ensure the tests dependent on it pass
