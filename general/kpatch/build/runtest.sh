6c34a21249a22fc19f97139c2e5dee88870f078f
Yulia Kopkova <ykopkova@redhat.com>
2024-12-09 14:59:24 -0500

    kpatch: build: use functions from common kpatch/include library

--- general/kpatch/build/runtest.sh
+++ general/kpatch/build/runtest.sh
@@ -29,6 +29,7 @@
 # Include rhts environment
 . /usr/share/beakerlib/beakerlib.sh ||  exit 1
 . lib_build.sh
+. ../include/lib.sh
 
 # shellcheck disable=SC2034
 BASIC_DONE=0
@@ -41,28 +42,10 @@ TEST_PATCH_PATH="test/integration"
 rlJournalStart
     rlPhaseStartSetup
         rlRun "create_build_dir"
-        if [ ! -e "kpatch/.git" ]; then
-            if [ ! -z "${KPATCH_REV}" ]; then
-                rlRun "git clone -b ${KPATCH_REV} ${KPATCH_REPO}" || rlDie
-            else
-                rlRun "git clone ${KPATCH_REPO}" || rlDie
-                pushd kpatch
-                KPATCH_REV=$(git describe --tags --abbrev=0)
-                rlRun "git checkout ${KPATCH_REV}" || rlDie
-                popd
-            fi
-        fi
+        download_kpatch_repo ${KPATCH_REPO} ${KPATCH_REV}
         pushd kpatch
-        rlRun "git describe --tags"
-        rlRun "source test/integration/lib.sh"
-        rlRun "kpatch_dependencies"
-        rlRun "kpatch_set_ccache_max_size 10G"
-        source /etc/os-release
-        MA=$(cut -d '.' -f 1 <<< $VERSION_ID)
-        MI=$(cut -d '.' -f 2 <<< $VERSION_ID)
-        PREVIOUS_MI=$(( $MI-1 ))
-        PREVIOUS_TARGET="${MA}.${PREVIOUS_MI}"
-        [ ! -d ${TEST_PATCH_PATH}/${ID}-${VERSION_ID} ] && cp -R ${TEST_PATCH_PATH}/${ID}-${PREVIOUS_TARGET} ${TEST_PATCH_PATH}/${ID}-${VERSION_ID}
+        build_kpatch_setup
+        check_test_target
         if [ ! -z "${KPATCH_SKIP_TEST}" ]; then
             pushd test/integration/${ID}-${VERSION_ID}
             rm -rf ${KPATCH_SKIP_TEST}
@@ -88,4 +71,3 @@ rlJournalStart
     rlPhaseEnd
 rlJournalEnd
 rlJournalPrintText
-
