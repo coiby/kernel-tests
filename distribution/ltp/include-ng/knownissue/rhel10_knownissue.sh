4b44e0426f6fd27951a83214be1acfdf346b9837
Li Wang <liwang@redhat.com>
2024-12-10 10:58:09 +0800

    Revert "knownissue: move out tests from the list"
    This reverts commit 1ce7a05250ac569555c822c9cca0e78a91f8040e.

--- distribution/ltp/include-ng/knownissue/rhel10_knownissue.sh
+++ distribution/ltp/include-ng/knownissue/rhel10_knownissue.sh
@@ -7,12 +7,16 @@ function rhel10_fatal_issues()
 
 function rhel10_unfix_issues()
 {
+	# setpgid01 failed on rhel10
+	osver_in_range "1000" "1001" && tskip "setpgid01" unfix
 	# RHEL-65410: ltp/lite move_pages04 test failed
 	osver_in_range "1000" "1001" && tskip "move_pages04" unfix
 	# set_mempolicy04 failed on rhel10
 	osver_in_range "1000" "1001" && tskip "set_mempolicy04" unfix
 	# logrotate_sh failed on rhel10
 	osver_in_range "1000" "1001" && tskip "logrotate_sh" unfix
+	# pty01 and ptem01 failed with kirk
+	osver_in_range "1000" "1001" && tskip "pty01 ptem01" unfix
 	# RHEL-65409: ltp/lite fanotify22 test failed
 	osver_in_range "1000" "1001" && tskip "fanotify22" unfix
 }
