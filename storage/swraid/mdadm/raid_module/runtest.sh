dd845786230dc2fa65ed9af3d727387d726d5427
fine.fan <ffan@redhat.com>
2024-12-12 09:55:03 -0500

    Update sw raid_module to list more information about system information and raid modules.

--- storage/swraid/mdadm/raid_module/runtest.sh
+++ storage/swraid/mdadm/raid_module/runtest.sh
@@ -35,6 +35,11 @@ function clean_all_loop_md
 function startup
 {
     clean_all_loop_md
+    rlRun -l "uname -r"
+    rlRun -l "rpm -q kernel"
+    rlRun -l "rpm -q mdadm"
+    rlRun -l "lsblk"
+    rlRun -l "lsmod | grep raid" "0-255"
 }
 
 function cleanup
@@ -46,14 +51,17 @@ function runtest
 {
     typeset i
     for i in 0 1 456 10; do
+        rlRun -l "lsmod | grep raid" "0-255"
         rlRun -l "modprobe -r raid$i" "0-255"
         sleep 5
         rlRun -l "modprobe raid$i"
         sleep 5
+        rlRun -l "lsmod | grep raid" "0-255"
         rlRun -l "modprobe -r raid$i"
         sleep 5
         rlRun -l "modprobe raid$i"
         sleep 5
+        rlRun -l "lsmod | grep raid" "0-255"
         rlRun -l "modprobe -r raid$i"
     done
 }
