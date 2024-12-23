701e7505e77f7879ee95acea8a2e016c4f60ad83
Chunyu Hu <chuhu@redhat.com>
2024-12-23 16:19:57 +0800

    general/scheduler/include: remove rhts dependency
    remove the Makefile and rhts reference as rpm support could be stopped
    in the future.
    
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- general/scheduler/include/runtest.sh
+++ general/scheduler/include/runtest.sh
@@ -30,10 +30,11 @@
 export is_rhivos=0
 declare -F kernel_automotive && kernel_automotive && is_rhivos=1 || is_rhivos=0
 
-if ! (($is_rhivos)); then
-        . /usr/bin/rhts-environment.sh || exit 1
-fi
 . /usr/share/beakerlib/beakerlib.sh || exit 1
+
+if [ -z "$OUTPUTFILE" ]; then
+	export OUTPUTFILE=`mktemp /mnt/testarea/tmp.XXXXXX`
+fi
 
 SCHED_PROCESS_SRC=../include/processes
 SCHED_PROCESS_BIN=$(pwd)/tasks
@@ -69,7 +70,7 @@ function _wget_compile_stress()
         fi
 
         if [ ! "$SKIP_DOWNLOAD" = 1 ]; then
-                wget $STRESS_SRPM || { report_result "wget_stress" FAIL; rlDie "wget stress"; }
+                wget $STRESS_SRPM || { rstrnt-report-result "wget_stress" FAIL; rlDie "wget stress"; }
         fi
         rpm -ivh stress-0.18.8-1.4.el7.src.rpm
         rpm -q yum-utils || yum -y install yum-utils
@@ -83,10 +84,10 @@ function _wget_compile_stress()
                 rpmbuild -bi /root/rpmbuild/SPECS/stress.spec
         fi
         if [ ! -f $SCHED_STRESS_PATH/stress ]; then
-                report_result "compile_stress" FAIL
+                rstrnt-report-result "compile_stress" FAIL
                 return
         fi
-        report_result "compile_stress" PASS
+        rstrnt-report-result "compile_stress" PASS
 }
 
 function source_compile()
@@ -115,7 +116,7 @@ function load_avg_sum()
         echo $avg_15 > avg_15_$(uname -r)
 
         local nr_cpus=$(nproc)
-        report_result "cpu${nr_cpus}:loadavg:$avg_1_5_15"
+        rstrnt-report-result "cpu${nr_cpus}:loadavg:$avg_1_5_15"
 }
 
 function print_system_info()
