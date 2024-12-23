965c947b8ed565afdce97cf73e2a79b380adeb0c
Chunyu Hu <chuhu@redhat.com>
2024-12-23 15:52:35 +0800

    general/scheduler/1479560-syscall-nmi: remove rhts dependency
    remove Makefile and rhts references
    add metadata
    
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- general/scheduler/1479560-syscall-nmi-stackp-race/runtest.sh
+++ general/scheduler/1479560-syscall-nmi-stackp-race/runtest.sh
@@ -26,8 +26,11 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-. /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh ||  exit 1
+
+if [ -z "$OUTPUTFILE" ]; then
+	export OUTPUTFILE=`mktemp /mnt/testarea/tmp.XXXXXX`
+fi
 
 trap 'Cleanup' SIGHUP SIGINT SIGQUIT SIGTERM SIGUSR1
 
