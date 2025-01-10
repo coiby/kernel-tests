ad3c1f5fd098aabcb1d57aca816bf57266bc815d
Philip Daly <pdaly@redhat.com>
2025-01-10 11:58:13 +0000

    Del SElinux check, policy update allows unsigned

--- security/integrity/lockdown/runtest.sh
+++ security/integrity/lockdown/runtest.sh
@@ -31,6 +31,7 @@ function get_lockdown_status()
 }
 
 rlJournalStart
+	reboot=False
 	if [ "${REBOOTCOUNT}" -eq 0 ]; then
 		rlPhaseStartSetup "Check lockdown status, set to 'integrity'."
 		rlShowRunningKernel
@@ -44,95 +45,61 @@ rlJournalStart
 		fi
 		${pkg_mgr} ${pkg_mgr_inst_string} ${devel_pkg}
 		# Check current lockdown state
-		# # set to 'integrity' if not already set.
+		# set to 'integrity' if not already set.
 		lockdown_status=$(get_lockdown_status)
 		if [[ ${lockdown_status} != "integrity" ]]; then
 			rlLog "Lockdown status is set to '${lockdown_status}', setting to 'integrity'."
-			rlRun "change_cmdline 'lockdown=integrity'" || exit 1
-			rlRun "rstrnt-reboot"
+			rlRun "change_cmdline 'lockdown=integrity'" || rlFail "Couldn't set lockdown."
+			reboot=True
 		else
 			rlLog "Lockdown status is already set to 'integrity'."
 			REBOOTCOUNT=1
 		fi
 		rlPhaseEnd
+		if [ $reboot ]; then
+			rlRun "rstrnt-reboot"
+		fi
 	fi
 	if [ "${REBOOTCOUNT}" -eq 1 ]; then
 		rlPhaseStartTest "Ensure that lockdown status cannot be changed at runtime."
-		# Clone external repository with unlockdown script.
-		rlRun "git clone https://github.com/xairy/unlockdown.git"
-		rlRun "pushd unlockdown/02-evdev"
-		# Install package dependency.
-		python_pkg="python3-evdev"
-		if ! rpm -qa | grep ${python_pkg} >/dev/null; then
-			rlLog "Installing python3-evdev package."
-			${pkg_mgr} ${pkg_mgr_inst_string} ${python_pkg}
-		fi
-		# Execute script to send Alt+SysRq+x key clicks to console.
-		# Key clicks can change lockdown status at runtime.
-		rlRun "python evdev-sysrq.py" 0-255
-		if [ $? -eq 0 ]; then
-			rlLog "Alt+SysRq+x key clicks submitted, checking lockdown status."
-			if [[ $(get_lockdown_status) = "none" ]]; then
-				rlLog "Lockdown status was changed through Alt+SysRq+x."
-				exit 1
+			# Ensure input is available before continuing.
+			if [ -d /dev/input ]; then
+				# Clone external repository with unlockdown script.
+				rlRun "git clone https://github.com/xairy/unlockdown.git"
+				rlRun "pushd unlockdown/02-evdev"
+				# Install package dependency.
+				python_pkg="python3-evdev"
+				if ! rpm -qa | grep ${python_pkg} >/dev/null; then
+					rlLog "Installing python3-evdev package."
+					${pkg_mgr} ${pkg_mgr_inst_string} ${python_pkg}
+				fi
+				# Execute script to send Alt+SysRq+x key clicks to console.
+				# Key clicks can change lockdown status at runtime.
+				rlRun "python evdev-sysrq.py" 0,255
+				if [ $? -eq 0 ]; then
+					rlLog "Alt+SysRq+x key clicks submitted, checking lockdown status."
+					if [[ $(get_lockdown_status) = "none" ]]; then
+						rlFail "Lockdown status was changed through Alt+SysRq+x."
+					fi
+				else
+					rlLog "Alt+SysRq+x key clicks were not submitted. Skipping lockdown status check."
+				fi
+				rlRun "popd"
+			else
+				rlLog "No /dev/input available, skipping check."
 			fi
-		else
-			rlLog "Alt+SysRq+x key clicks were not submitted. Skipping lockdown status check."
-		fi
-		rlRun "popd"
 		rlPhaseEnd
-		rlPhaseStartTest "Ensure SELinux prevents the loading of an unsigned kernel module."
-			selinux_orig_state=$(getenforce)
-			rlRun "echo ${selinux_orig_state} > selinux_orig_state"
-			rlLog "selinux_orig_state: ${selinux_orig_state}"
+		rlPhaseStartTest "Ensure that lockdown mode prevents the loading of an unsigned kernel module."
 			rlRun "pushd src"
 			rlRun "make all"
-			if [ ${selinux_orig_state} != "Disabled" ]; then
-				# Unsigned module test
-				rlRun "make test"
-				rlAssertGrep "audit.*.avc:  denied  { module_load }.*.comm=\"insmod\".*.hw.ko" dmesg-unsigned.log -E
-				rlRun "sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config"
-				rlRun "popd"
-				rlRun "touch selinux_state_changed"
-				rlRun "rstrnt-reboot"
-			else
-				rlLog "SELinux was disabled, skipping check."
-				rlRun "popd"
-				REBOOTCOUNT=2
-			fi
-		rlPhaseEnd
-	fi
-	if [ "${REBOOTCOUNT}" -eq 2 ]; then
-		rlPhaseStartTest "Ensure that lockdown mode prevents the loading of an unsigned kernel module."
-			if [ $(getenforce) = "Disabled" ]; then
-				rlRun "pushd src"
-				# Unsigned module test
-				rlRun "make test"
-				rlAssertGrep "Lockdown: insmod: unsigned module loading is restricted" dmesg-unsigned.log
-				rlFileSubmit dmesg-unsigned.log
-				rlRun "popd"
-			else
-				rlLog "SELinux was not disabled."
-				exit 1
-			fi
+			# Unsigned module test
+			rlRun "make test"
+			rlAssertGrep "Lockdown: insmod: unsigned module loading is restricted" dmesg-unsigned.log
+			rlFileSubmit dmesg-unsigned.log
+			rlRun "popd"
 		rlPhaseEnd
 	fi
 	rlPhaseStartCleanup
-		if [ $(ls selinux_state_changed 2>/dev/null) ]; then
-			selinux_current_state=$(getenforce)
-			selinux_orig_state=$(cat selinux_orig_state)
-			rlLog "selinux_orig_state: ${selinux_orig_state} , selinux_current_state: ${selinux_current_state}"
-			if [ ${selinux_current_state} = ${selinux_orig_state} ]; then
-				rlLog "SELinux current state is the same as the original state."
-				exit 1
-			fi
-			rlRun "rm -f selinux_state_changed"
-			rlRun "rm -f selinux_orig_state"
-			regexp=$(echo ${selinux_current_state} | tr '[:upper:]' '[:lower:]')
-			replacement=$(echo ${selinux_orig_state} | tr '[:upper:]' '[:lower:]')
-			rlRun "sed -i s/SELINUX=${regexp}/SELINUX=${replacement}/g /etc/selinux/config"
-			rlRun "rstrnt-reboot"
-		fi
 	rlPhaseEnd
 rlJournalEnd
 rlJournalPrintText
