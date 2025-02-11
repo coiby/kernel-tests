dcd5ab01d64407ac9b62440a06a7b10eb4167ce2
Waylon Cude <waylon531@gmail.com>
2025-02-11 11:53:20 +0000

    Check that an inversion has occurred with pip_stress

--- rt-tests/pi_stress/runtest.sh
+++ rt-tests/pi_stress/runtest.sh
@@ -13,6 +13,9 @@
 # Source rt common functions
 . ../include/runtest.sh || exit 1
 
+: "${PIP_STRESS_RETRIES:=10}"
+: "${PIP_STRESS_USLEEP:=500}"
+
 export nrcpus rhel_x
 
 if ! kernel_automotive; then
@@ -43,7 +46,37 @@ oneliner "pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS"
 oneliner "pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS --rr"
 # Running pi_stress: set the number of inversion groups with CPU cores
 oneliner "pi_stress --quiet --groups=$(( nrcpus )) --duration=30"
+
 # Running pip_stress: used priority inheritance to handle an inversion
-oneliner "pip_stress"
+
+# Try up to N times for a successful pip_stress run
+phase_start pip_stress
+PIP_SUCCESS=0
+for attempt in $(seq "$PIP_STRESS_RETRIES"); do
+    log "Attempt $attempt"
+    RESULT=$(run "pip_stress -u $PIP_STRESS_USLEEP")
+    if [[ $(echo "$RESULT" | grep -c \
+        "Successfully used priority inheritance to handle an inversion" ) -eq 1 ]]
+    then
+        PIP_SUCCESS=1
+        break
+    fi
+    sleep 1
+done
+
+if [[ $PIP_SUCCESS -eq 1 ]] ; then
+    log "pip_stress found an inversion"
+    export PHASE_STATUS="PASS"
+else
+    log "pip_stress did not find an inversion"
+    # Only fail we are running the automotive kernel or the PIP_STRESS_STRICT
+    # environment variable is set
+    if kernel_automotive || [[ -n "$PIP_STRESS_STRICT" ]]; then
+        export PHASE_STATUS="FAIL"
+    else
+        export PHASE_STATUS="WARN"
+    fi
+fi
+phase_end
 
 exit 0
