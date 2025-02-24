01b2986c19a9a3a61ab4b79ba1388eaa6581b2ed
Hangbin Liu <haliu@redhat.com>
2025-02-24 16:19:18 +0800

    kselftests/net: submit test log via rlLog
    Current test frameworks no longer report OUTPUTFILE by default.
    Let's use rlLog to submit the test log.
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>

--- kselftests/include/net.sh
+++ kselftests/include/net.sh
@@ -364,6 +364,9 @@ do_bpf_test_progs_run()
 		echo -e "\n=== Dmesg result ===" >> "$OUTPUTFILE"
 		dmesg >> "$OUTPUTFILE"
 
+		# submit logs
+		rlLog "$(cat ${OUTPUTFILE})"
+
 		[ "$ret_1" -ne 0 ] && ret=${ret_1} || ret=${ret_2}
 		check_result $num "$total_num" "${item}:${name}" $ret
 		rlPhaseEnd
@@ -436,6 +439,10 @@ do_tc-testing_run()
 		echo "${name}" | grep -qP "tests\.json|concurrency\.json" && extra_p="-d $DEFAULT_IFACE" || extra_p=""
 		./tdc.py -f "${name}" "$extra_p" &> "$OUTPUTFILE"
 		ret=$?
+
+		# submit logs
+		rlLog "$(cat ${OUTPUTFILE})"
+
 		if grep -q "not ok" "$OUTPUTFILE"; then
 			check_result $num "$total_num" "${item}:${name}" 1
 			fail=$((fail+1))
