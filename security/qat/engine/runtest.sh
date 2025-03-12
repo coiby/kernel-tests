4ca55d2c1a1afe1dc9d32e68bd0485b9740fb8b4
Denis Aleksandrov <daleksan@redhat.com>
2025-03-12 10:32:05 -0400

    Removes random whitespace
    Signed-off-by: Denis Aleksandrov <daleksan@redhat.com>

--- security/qat/engine/runtest.sh
+++ security/qat/engine/runtest.sh
@@ -61,7 +61,7 @@ rlPhaseStartSetup
 		rlRun "systemctl start qat"
 
 		# Setup for qat_sw tests
-		rlRun "dnf install -y https://mirror.stream.centos.org/SIGs/9-stream/extras/x86_64/extras-common/Packages/c/centos-release-isa-override-9-2.el9s.noarch.rpm" 0 "Installing override package in-scrip    t since beaker metadata can't handle links"
+		rlRun "dnf install -y https://mirror.stream.centos.org/SIGs/9-stream/extras/x86_64/extras-common/Packages/c/centos-release-isa-override-9-2.el9s.noarch.rpm" 0 "Installing override package in-script since beaker metadata can't handle links"
 		rlRun "dnf install -y intel-ipsec-mb intel-ipp-crypto-mb intel-ipsec-mb-devel intel-ipp-crypto-mb-devel"
 		rlRun "git clone https://github.com/intel/QAT_Engine.git"
 		rlRun "cd QAT_Engine/"
