10f2dabc9f03e140a3cc73d585f566b53fbe8225
sbertram <sbertram@redhat.com>
2025-03-21 07:22:30 +0000

    Do not force parallel execution.

--- memory/concurrent_syscall/runtest.sh
+++ memory/concurrent_syscall/runtest.sh
@@ -9,6 +9,9 @@ rlJournalStart
     rlPhaseStartSetup
         rlShowRunningKernel
         rlRun "wget https://gitlab.com/redhat/centos-stream/tests/ltp/-/raw/${LTP_VERSION}/runtest/syscalls -O ../../distribution/ltp/generic/configs/SYSCALLS"
+        if [[ -e patches/syscalls.${LTP_VERSION}.patch ]]; then
+            rlRun "patch ../../distribution/ltp/generic/configs/SYSCALLS patches/syscalls.${LTP_VERSION}.patch"
+        fi
     rlPhaseEnd
     rlPhaseStartTest "Concurrent execution"
         rlRun "pushd ../../distribution/ltp/generic"
