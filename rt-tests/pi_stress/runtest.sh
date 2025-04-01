ac4e828c916355ba1196b344c1322be5f635c9d8
Shizhao Chen <shichen@redhat.com>
2025-04-01 11:30:52 +0800

    [rt] patch pip_stress when usleep n/a in the official binary
    When usleep is n/a, patch pip_stress and build the test from source. This test
    now is valid for all streams, so check for a successful prio inversion handling
    as a basic pass criteria. Previously the test was run at most
    $PIP_STRESS_RETRIES times to increase the probability of an inversion occur,
    with usleep available, this was removed.
    
    Resolves: RHELTEST-929
    Resolves: RHELTEST-928
    
    Signed-off-by: Shizhao Chen <shichen@redhat.com>

--- rt-tests/pi_stress/runtest.sh
+++ rt-tests/pi_stress/runtest.sh
@@ -13,87 +13,79 @@
 # Source rt common functions
 . ../include/runtest.sh || exit 1
 
-: "${PIP_STRESS_RETRIES:=10}"
-
-if [[ -z "${PIP_STRESS_USLEEP}" ]]; then
-    if uname -r | grep -qE "aarch64|rt-debug"; then
-        # aarch64 and rt-debug requires more sleep to get an inversion triggered
-        PIP_STRESS_USLEEP=10000
-    fi
-fi
-
-export nrcpus rhel_x
+export nrcpus rhel_x rhel_y
 
 if ! kernel_automotive; then
     rt_env_setup
 fi
 
-
-log "rt-tests/pi_stress Test Start"
-
-if ! kernel_automotive; then
-    declare pkg_name="rt-tests" && [ $rhel_x -ge 9 ] && pkg_name="realtime-tests"
-    which pi_stress || yum install -y $pkg_name
+if ! command -v pi_stress; then
+    log "Command pi_stress not found"
+    exit 1
 fi
 
-if (( rhel_x == 9 )); then
-    # https://issues.redhat.com/browse/RHEL-77110
-    rpmdev-vercmp "$(rpm -q --qf '%{V}-%{R}' realtime-tests)" 2.8-3.el9 > /dev/null
-elif (( rhel_x == 10 )); then
-    # https://issues.redhat.com/browse/RHEL-77111
-    rpmdev-vercmp "$(rpm -q --qf '%{V}-%{R}' realtime-tests)" 2.8-4.el10 > /dev/null
-fi
-
-# Use option when realtime-tests version is greater than the versions above
-if (( $? == 11 )); then
-    OPT_USLEEP="-u $PIP_STRESS_USLEEP"
-fi
-
-if [ -z "$PARAM_SEC" ]; then
-    # For details: https://issues.redhat.com/browse/RHEL-34758
-    # Limit the duration to 30s to avoid any rcu starvation warnings, the
-    # default timeout for rcu stall is 60s.
-    PARAM_SEC=30
-fi
-if [ -z "$PARAM_GROUPS" ]; then
-    PARAM_GROUPS=1
-fi
-
+# Limit the duration to 30s to avoid any rcu starvation warnings, the
+# default timeout for rcu stall is 60s, see RHEL-34758
+: ${PI_STRESS_DURATION:=30}
+: ${PI_STRESS_GROUPS:=1}
 # Running pi_stress: suppress running output
-oneliner "pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS"
+oneliner "pi_stress --quiet --duration=$PI_STRESS_DURATION --groups=$PI_STRESS_GROUPS"
 # Running pi_stress: use SCHED_RR for test threads
-oneliner "pi_stress --quiet --duration=$PARAM_SEC --groups=$PARAM_GROUPS --rr"
+oneliner "pi_stress --quiet --duration=$PI_STRESS_DURATION --groups=$PI_STRESS_GROUPS --rr"
 # Running pi_stress: set the number of inversion groups with CPU cores
 oneliner "pi_stress --quiet --groups=$(( nrcpus )) --duration=30"
 
-# Running pip_stress: used priority inheritance to handle an inversion
-
-# Try up to N times for a successful pip_stress run
-phase_start_test pip_stress
-PIP_SUCCESS=0
-for attempt in $(seq "$PIP_STRESS_RETRIES"); do
-    log "Attempt $attempt"
-    run "pip_stress $OPT_USLEEP" 2>&1 | tee pip_stress.log
-    if (grep -q "Successfully" pip_stress.log); then
-        PIP_SUCCESS=1
-        break
-    fi
-    sleep 1
-done
-
-if [[ $PIP_SUCCESS -eq 1 ]] ; then
-    log "pip_stress found an inversion"
-    export PHASE_STATUS="PASS"
-else
-    log "pip_stress did not find an inversion"
-    # Only fail we are running the automotive kernel or the PIP_STRESS_STRICT
-    # environment variable is set
-    if kernel_automotive || [[ -n "$PIP_STRESS_STRICT" ]]; then
-        export PHASE_STATUS="FAIL"
+if [[ -z "${PIP_STRESS_USLEEP}" ]]; then
+    # aarch64 and rt-debug requires more sleep to get an inversion triggered
+    if uname -r | grep -qE "aarch64|rt.*debug"; then
+        PIP_STRESS_USLEEP=10000
     else
-        export PHASE_STATUS="WARN"
+        # this is the default for "normal" hardwares
+        PIP_STRESS_USLEEP=500
     fi
 fi
+OPT_USLEEP="-u $PIP_STRESS_USLEEP"
+
+# Running pip_stress: used priority inheritance to handle an inversion
+phase_start_test "pip_stress $OPT_USLEEP"
+if pip_stress -h | grep -q '\--usleep'; then
+    # Use the official pip_stress when usleep option is available
+    pip_stress_cmd=pip_stress
+else
+    # Build pip_stress with usleep support when n/a in official pkg
+    build_dir=$(mktemp -d)
+    rt_tests_repo="https://gitlab.com/redhat/centos-stream/tests/kernel/core/rt-tests.git"
+    # the commit that adds the usleep option
+    usleep_hash="eea8da1647c3f7d26f3aae607475266112ab79b1"
+    # all the source files used by pip_stress
+    files="src/pi_tests/pip_stress.c src/include/pip_stress.h src/include/rt-error.h"
+    if ((rhel_x == 7)); then
+        ref="tags/v1.8"
+    elif ((rhel_x == 8)); then
+        ref="tags/v2.6"
+    elif ((rhel_x == 9 && rhel_y < 6)); then
+        ref="tags/v2.8"
+    else
+        ref="main"
+    fi
+    run "git clone $rt_tests_repo $build_dir"
+    pushd $build_dir
+    run "git checkout $ref"
+    run "git checkout $usleep_hash -- $files"
+    run "make pip_stress"
+    if run "test -x ./pip_stress"; then
+        pip_stress_cmd=$(realpath ./pip_stress)
+    else
+        log "Failed to build pip_stress with usleep support!"
+        phase_end
+        exit 1
+    fi
+    popd
+fi
+
+run "$pip_stress_cmd $OPT_USLEEP" 2>&1 | tee pip_stress.log
+# the return code doesn't reflect if an inversion was triggered
+run "grep Successfully pip_stress.log" 0 "Check if inversion was triggered"
 phase_end
 
 test_finish
