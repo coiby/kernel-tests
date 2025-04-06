ebef3065d0a049e08941c3a03df83e59295a6a62
Yi Zhang <yi.zhang@redhat.com>
2025-04-06 11:06:54 -0400

    storage/nvme_rdma: update nvmeof_rdma_luns_mapping
    Signed-off-by: Yi Zhang <yi.zhang@redhat.com>

--- storage/nvme_rdma/hardtarget/nvmeof_rdma_luns_mapping/main.sh
+++ storage/nvme_rdma/hardtarget/nvmeof_rdma_luns_mapping/main.sh
@@ -11,30 +11,35 @@ system_info_for_debug
 function runtest {
 
 	# connect to E5700 host nvme-rdma-host-1-nqn-1 which has 21 luns
-	HostNQN1=nvme-rdma-host-1-nqn-1
+	HostNQN1=nvme-rdma-host-9-nqn-1
 	HostID1=$(uuid)
+	num=0
+	while [ $num -lt 10 ]; do
+		tlog "Start to test nvme rdma luns mapping test:$num"
 
-	NVMEOF_RDMA_TARGET_CONNECT_E5700 "$IP0" "$HostNQN1" "$HostID1"
+		NVMEOF_RDMA_TARGET_CONNECT_E5700 "$IP0" "$HostNQN1" "$HostID1"
 
-	tok "sleep 1.5"
-	tok nvme list
-	tok lsblk
-	tok nvme list-subsys
-	tok nvme netapp smdevices
-	tok "grep . /sys/block/nvme0n*/nsid >ns.info"
-	local ns=1
-	while [ $ns -lt 22 ]; do
-		NSID=$((ns+20))
-		tlog "Start to check nvme disk mapping for /dev/nvme0n$ns with nsid:$NSID"
-		tok "cat ns.info | grep nvme0n$ns\/nsid:$NSID"
-		if [ $? -ne 0 ]; then
-			tlog "/dev/nvme0n$ns mapping for nsid:$NSID failed"
-			trun "cat ns.info"
-		fi
-		((ns++))
+		tok "sleep 1.5"
+		tok nvme list
+		tok lsblk
+		tok nvme list-subsys
+		tok nvme netapp smdevices
+		tok "grep . /sys/block/nvme0n*/nsid >ns.info"
+		local ns=1
+		while [ $ns -lt 22 ]; do
+			NSID=$((ns+20))
+			tlog "Start to check nvme disk mapping for /dev/nvme0n$ns with nsid:$NSID"
+			tok "cat ns.info | grep nvme0n$ns\/nsid:$NSID"
+			if [ $? -ne 0 ]; then
+				tlog "/dev/nvme0n$ns mapping for nsid:$NSID failed"
+				trun "cat ns.info"
+			fi
+			((ns++))
+		done
+
+		tok nvme disconnect-all
+		((num++))
 	done
-
-	tok nvme disconnect-all
 }
 
 tlog "running $0"
