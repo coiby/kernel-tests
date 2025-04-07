8eb23372242d69d86c0085ab7846b69f1ee11102
Oliver Gutierrez <ogutsua@gmail.com>
2025-04-07 08:09:47 +0100

    Updated NVIDIA driver and CUDA versions

--- accelerators/nvidia/openrm-build/runtest.sh
+++ accelerators/nvidia/openrm-build/runtest.sh
@@ -22,8 +22,8 @@
 # Test configuration
 # TODO: Make it customizable so we can test different driver versions
 # RHEL AI versions info: https://gitlab.com/redhat/rhel-ai/containers/nvidia-bootc/-/blob/main/argfile.conf?ref_type=heads
-DRIVER_VERSION="570.86.15"
-CUDA_VERSION='12.8.0'
+DRIVER_VERSION="570.124.06"
+CUDA_VERSION='12.8.1'
 BASE_URL='https://us.download.nvidia.com/tesla'
 SPECFILE_REPO='https://github.com/NVIDIA/yum-packaging-precompiled-kmod'
 
