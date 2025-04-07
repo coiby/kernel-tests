eb3534248953c3b50612a82c219aff439a00a852
Chunyu Hu <chuhu@redhat.com>
2025-04-07 14:54:54 +0800

    general/process/trinity_git: use forked trinity repo
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>

--- general/process/trinity_git/runtest.sh
+++ general/process/trinity_git/runtest.sh
@@ -109,7 +109,7 @@ function test_setup()
 	# rpm -q util-linux || yum -y install util-linux
 	which trinity && return
 	# Can't clone in beaker env when automation. prepare the head into lookaside
-	# rlRun "git clone https://github.com/kernelslacker/trinity.git" 0-255
+	# rlRun "git clone --depth 1 https://gitlab.com/redhat/centos-stream/tests/kernel/core/trinity" 0-255
 	test -d testversion || get_lookaside
 	patch_apply
 	rlRun "pushd $testversion"
