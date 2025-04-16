5ce7123f636553a42f532532d50e41bb71ecbd40
Bruno Goncalves <bgoncalv@redhat.com>
2025-04-16 13:28:04 +0200

    install_kselftests: don't fail if yum-builddep fails

--- kselftests/runtest.sh
+++ kselftests/runtest.sh
@@ -119,7 +119,8 @@ install_packages()
         # per https://issues.redhat.com/browse/ENGCMP-2966 this is only temporary.
         # once this is removed, this patch can also be removed.
         rlRun "sed -i 's/efiuki 1/efiuki 0/' kernel.spec"
-        rlRun "yum-builddep --downloadonly -y ./kernel.spec --downloaddir $(pwd)"
+        # I'm not sure why to run yum-builddep, but if it fails doens't seem critical, therefore ignore any error.
+        rlRun "yum-builddep --downloadonly -y ./kernel.spec --downloaddir $(pwd)" 0-255
 
         $pkg_mgr $pkg_mgr_inst_string *.rpm
         pushd ../SOURCES
