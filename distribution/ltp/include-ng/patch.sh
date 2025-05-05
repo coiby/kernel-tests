ad865a8ac1c91a62a3d29d8098d1fe490420307b
Charles Shi <schrht@gmail.com>
2025-05-05 15:53:58 -0400

    Fix rt_ltp sub-test /perf/latency

--- distribution/ltp/include-ng/patch.sh
+++ distribution/ltp/include-ng/patch.sh
@@ -136,4 +136,6 @@ patch-rtltp()
 	patch -d ${TARGET} -p1 < ${ABS_DIR}/${TESTVERSION}/0002-Ensure-prio-wake-test-runs-on-a-single-core.patch
 	patch -d ${TARGET} -p1 < ${ABS_DIR}/${TESTVERSION}/0003-Fix-the-result-parsing-bug-for-pi-tests.patch
 	patch -d ${TARGET} -p1 < ${ABS_DIR}/${TESTVERSION}/0004-Only-analyze-testpi-log-if-the-test-was-actually-exe.patch
+	patch -d ${TARGET} -p1 < ${ABS_DIR}/${TESTVERSION}/0001-Create-missing-log-directory-for-realtime-perf-laten.patch
+	patch -d ${TARGET} -p1 < ${ABS_DIR}/${TESTVERSION}/0002-Enhance-log-checking-for-realtime-perf-latency.patch
 }
