1d20ab89d3b3b4d252d596a5522fb2f8be8ed5fa
Ping Fang <pifang@redhat.com>
2025-06-06 10:09:54 +0800

    ltp: enable ksm01,03 on rhel10 s390x
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include-ng/knownissue/rhel10_knownissue.sh
+++ distribution/ltp/include-ng/knownissue/rhel10_knownissue.sh
@@ -21,8 +21,6 @@ function rhel10_unfix_issues()
 	osver_in_range "1000" "1001" && tskip "fanotify22" unfix
 	# RHELTEST-324: ltp/starvation.c:148: TFAIL: Scheduller starvation reproduced.
 	is_arch "aarch64" && osver_in_range "1000" "1001" && tskip "starvation" unfix
-	# RHEL-79771: [RHEL-10][s390x] User process fault: interruption code 0011 ilc:3 in libc.so.6[c91ee,3ffa9c00000+1e1000]
-	is_arch "s390x" && osver_in_range "1000" "1002" && tskip "ksm01 ksm03" unfix
 }
 
 function rhel10_fixed_issues()
