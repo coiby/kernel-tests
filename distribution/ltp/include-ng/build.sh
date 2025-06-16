bc9518e313da17f156ee4187d4059ea7a7f5d25c
Ping Fang <pifang@redhat.com>
2025-06-16 10:41:53 +0800

    ltp: fix shellcheck SC2053
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include-ng/build.sh
+++ distribution/ltp/include-ng/build.sh
@@ -163,7 +163,7 @@ configure()
 build_all()
 {
 	setup_testarea
-	if [[ "y" == ${USE_LTP_RPM} ]]; then
+	if [[ "y" == "${USE_LTP_RPM}" ]]; then
 		dnf copr -y enable pifang/ltp
 		dnf install -y ltp
 	elif [[ -z ${LTP_COMMIT_ID} ]]; then
@@ -195,7 +195,7 @@ build_all()
 	fi
 
 	install_kirk
-	if [[ "y" == ${USE_LTP_RPM} ]]; then
+	if [[ "y" == "${USE_LTP_RPM}" ]]; then
 		return
 	fi
 
