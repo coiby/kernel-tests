da058e55d4971d7fd4983d0db5a73c804dd975b7
Ping Fang <pifang@redhat.com>
2025-07-01 15:23:50 +0800

    ltp: install specified ltp version and add abort check
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include-ng/build.sh
+++ distribution/ltp/include-ng/build.sh
@@ -166,7 +166,17 @@ build_all()
 	if [[ "y" == "${USE_LTP_RPM}" ]]; then
 		echo "============ Install LTP-${TESTVERSION} rpm ============" | tee -a $OUTPUTFILE
 		dnf copr -y enable pifang/ltp
-		dnf install -y ltp
+		dnf install -y ltp-${TESTVERSION}
+		if [ $? -ne 0 ]; then
+			echo "Aborting current task: Couldn't install LTP rpm." | tee -a $OUTPUTFILE
+			if [[ -n $RSTRNT_TASKNAME ]]; then
+				rstrnt-report-result "${RSTRNT_TASKNAME}" WARN
+				exit 0
+			else
+				exit 1
+			fi
+		fi
+
 	elif [[ -z ${LTP_COMMIT_ID} ]]; then
 		download_ltp
 	else
