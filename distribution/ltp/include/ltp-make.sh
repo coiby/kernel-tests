89490216b799a855441437819199e22d8e31105d
Li Wang <liwang@redhat.com>
2025-08-05 21:18:51 +0800

    ltp-lite: move RHELKT1LITE.next generation to the lite
    Signed-off-by: Li Wang <liwang@redhat.com>

--- distribution/ltp/include/ltp-make.sh
+++ distribution/ltp/include/ltp-make.sh
@@ -401,20 +401,6 @@ build_all()
             # the LTP_COMMIT_ID is already the version installed
             return
         fi
-        # generate RHELKT1LITE.next
-        echo "Going to generate RHELKT1LITE.next"
-        pushd ../lite/configs
-        # restraint doesn't seem to keep the file permission
-        chmod +x ./config-maker.sh
-        LTP_VERSION=next ./config-maker.sh &> config-maker.txt
-        if [ $? -ne 0 ]; then
-            cat config-maker.txt
-            echo "Aborting current task: Couldn't generate test config." | tee -a $OUTPUTFILE
-            rstrnt-report-result "${RSTRNT_TASKNAME}" WARN
-            exit 0
-        fi
-        popd
-        echo "RHELKT1LITE.next is generated"
     fi
     configure
     echo "============ Start ${MAKE} and install ============" | tee -a $OUTPUTFILE
