25a6be047b2ecf748ea2d554bd3e02aa14a15459
root <root@netqe51.knqe.eng.rdu2.dc.redhat.com>
2025-08-05 06:00:23 -0400

    ltp/generic: Skipping mmap21 on < 9.6
    The mmap21 test case requires the 20250530 release of ltp, which
    is only available for 9.6+. This commit adds an entry to the
    knownissues framework so that the test case is skipped on those
    streams that cannot support it.
    
    Resolves: https://issues.redhat.com/browse/RHELTEST-1405

--- distribution/ltp/include-ng/knownissue/rhel9_knownissue.sh
+++ distribution/ltp/include-ng/knownissue/rhel9_knownissue.sh
@@ -16,6 +16,8 @@ function rhel9_fatal_issues()
 	is_rt && cki_is_kernel_debug && osver_in_range "906" "907" && tskip "proc01" fatal
 	# RHEL-17195 RHEL-9: RIP: 0010:memset_orig+0x33/0xb0 - intel_pt_interrupt+0x5c/0xf0
 	is_arch "x86_64" && osver_in_range "904" "906" && tskip "pt_ex_user" fatal
+	# RHELTEST-1405: distribution/ltp/generic: VMMTIER1 mmap21_*__with_dmesg_entry "mmap21: command not found"
+	osver_in_range "900" "906" && tskip "mmap21*" fatal
 }
 
 function rhel9_unfix_issues()
