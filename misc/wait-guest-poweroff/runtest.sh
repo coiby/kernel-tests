006cd56d7750ef34c4f699338a0de816c6052af5
George Jacob <gjacob@gjacob-thinkpadt14sgen2i.remote.csb>
2025-08-25 12:00:44 -0500

    misc/wait-guest-poweroff obsolete rhivos func updated

--- misc/wait-guest-poweroff/runtest.sh
+++ misc/wait-guest-poweroff/runtest.sh
@@ -26,11 +26,9 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Enable TMT testing for RHIVOS
-auto_include=../../automotive/include/rhivos.sh
-[ -f $auto_include ] && . $auto_include
-declare -F kernel_automotive && kernel_automotive && is_rhivos=1 || is_rhivos=0
+. ../../cki_lib/libcki.sh || exit 1
 
-if ! (($is_rhivos)); then
+if ! cki_is_kernel_automotive; then
     # Include rhts environment
     . /usr/bin/rhts-environment.sh
 fi
