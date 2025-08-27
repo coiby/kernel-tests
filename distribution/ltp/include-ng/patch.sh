eb8e4c3eb628bcc1a0a3dfd8fac163aa34d373c3
Ping Fang <pifang@redhat.com>
2025-08-27 11:40:15 +0800

    ltp: backport fix for splice07
    Signed-off-by: Ping Fang <pifang@redhat.com>

--- distribution/ltp/include-ng/patch.sh
+++ distribution/ltp/include-ng/patch.sh
@@ -17,6 +17,7 @@ patch_generic()
 		${PATCH} < ${ABS_DIR}/INTERNAL/0001-shmat03-ignore-EACCES.patch
 		${PATCH} < ${ABS_DIR}/INTERNAL/0001-Disable-btrfs-as-we-don-t-support-it-anymore-new.patch
 		${PATCH} < ${ABS_DIR}/INTERNAL/0001-rhel9-support-futex_waitv.patch
+		${PATCH} < ${ABS_DIR}/${TESTVERSION}/0001-splice07.c-Skip-invalid-splice-tests-involving-memfd.patch
 	fi
 	if [ "$TESTVERSION" == "20250130" ]; then
 		# Tips: this patch should be applied in single on ltp-next(version > 20180926)
